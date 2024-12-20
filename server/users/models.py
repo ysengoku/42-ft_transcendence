763cad97d5441e43d382aef2d2845c1debc5b477
emuminov <jseldardw@gmail.com>
2024-12-20 20:12:26 +0100

    current state

--- server/users/models.py
+++ server/users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class User(AbstractUser):
+    account_balance = models.DecimalField(max_digits=5, decimal_places=2, default=0)
--- server/users/views.py
+++ server/users/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpRequest, JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import User
 
 
 # Create your views here.
-def index(request: HttpRequest):
-    return JsonResponse({"hello": "world"})
+@api_view(['GET'])
+def index(request: HttpRequest) -> Response:
+    users = User.objects.all()
+    return Response(users)
