4080772f27a95a1ab6b4541aa427468050954edf
ysengoku <ysengoku.tourisme@gmail.com>
2025-01-06 22:33:14 +0100

    Fix: navbar is visible

--- front/app/js/shared_components/navbar.js
+++ front/app/js/shared_components/navbar.js
@@ -1,22 +1,21 @@
 function renderNavbar() {
 	return `
-	<div>
-		<header class="navbar navbar-expand navbar-dark bg-dark flex-column flex-md-row bd-navbar">
+		<header class="navbar navbar-expand navbar-dark bg-dark px-3">
 			<a class="navbar-brand" href="./index.html">
-				<img src="./static/img/sample-logo.svg" height="40" alt="transcendencing" class="d-inline-block align-top">
+				<img src="assets/img/sample-logo.svg" height="40" alt="transcendencing" class="d-inline-block align-top">
 			</a>
 
-			<div class="ml-auto d-flex align-items-center">
-				<ul class="navbar-nav mr-auto">
+			<div class="ms-auto d-flex align-items-center">
+				<ul class="navbar-nav">
 					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-							<img id="avatar-img" src="./static/img/default_avatar.svg" height="40" alt="user" class="d-inline-block align-top">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<img id="avatar-img" src="assets/img/default_avatar.svg" height="40" alt="user" class="d-inline-block align-top">
 						</a>
-						<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+						<div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
 							<a class="dropdown-item" href="#" id="dropdown-item-nologged1">Login</a>
 							<a class="dropdown-item" href="#" id="dropdown-item-nologged2">Sign up</a>
-							<a class="dropdown-item" href="#" id="dropdown-item-logged1">Your profil</a>
-							<a class="dropdown-item" href="#" id="dropdown-item-logged2">Setting</a>
+							<a class="dropdown-item" href="#" id="dropdown-item-logged1">Your profile</a>
+							<a class="dropdown-item" href="#" id="dropdown-item-logged2">Settings</a>
 							<div class="dropdown-divider"></div>
 							<a class="dropdown-item" href="#" id="dropdown-item-nologged3">Forgot password</a>
 							<a class="dropdown-item" href="#" id="dropdown-item-logged3">Logout</a>
@@ -25,7 +24,6 @@ function renderNavbar() {
 				</ul>
 			</div>
 		</header>
-	</div>
 	`;
 }
 
