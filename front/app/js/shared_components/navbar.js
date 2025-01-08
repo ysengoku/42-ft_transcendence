2829679fa591788385a529a9bce5d915494f7f73
yusengok <ysengoku.tourisme@gmail.com>
2025-01-08 08:02:32 +0100

    minor: Set navbar to static, only avatar and drop down menu in navbar are dynamic now

--- front/app/js/shared_components/navbar.js
+++ front/app/js/shared_components/navbar.js
@@ -3,48 +3,29 @@ import { simulateLogout } from '../mock/auth.js';
 export function renderNavbar() {
 	const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true'; // Simulation using mock
 
-	const navbarHTML = `
-		<nav class="navbar navbar-expand navbar-dark bg-dark px-3">
-			<a class="navbar-brand" href="/">
-				<img src="assets/img/sample-logo.svg" height="40" alt="transcendencing" class="d-inline-block align-top">
-			</a>
-
-			<div class="ms-auto d-flex align-items-center">
-				<ul class="navbar-nav">
-					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-							${!isLoggedIn ? `
-							<img id="avatar-img" src="assets/img/default_avatar.svg" height="40" alt="user" class="d-inline-block align-top rounded-circle">
-							` : `
-							<img id="avatar-img" src="assets/img/sample_avatar.jpg" height="40" alt="user" class="d-inline-block align-top rounded-circle">
-							`}
-						</a>
-						<div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-            				${!isLoggedIn ? `
-                			<a class="dropdown-item" href="#login" id="dropdown-item-login">Login</a>
-                			<a class="dropdown-item" href="#register" id="dropdown-item-register">Sign up</a>
-							` : `
-                			<a class="dropdown-item" href="#">Your profile</a>
-                			<a class="dropdown-item" href="#">Settings</a>
-                			<div class="dropdown-divider"></div>
-                			<a class="dropdown-item" href="#" id="dropdown-item-logout">Logout</a>
-            				`}
-						</div>
-					</li>
-				</ul>
-			</div>
-		</nav>
+	const userMenuHTML = `
+		<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			<img id="avatar-img" src="${isLoggedIn ? 'assets/img/sample_avatar.jpg' : 'assets/img/default_avatar.svg'}" height="40" alt="user" class="d-inline-block align-top rounded-circle">
+		</a>
+		<div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+			${!isLoggedIn ? `
+				<a class="dropdown-item" href="#login" id="dropdown-item-login">Login</a>
+				<a class="dropdown-item" href="#register" id="dropdown-item-register">Sign up</a>
+			` : `
+				<a class="dropdown-item" href="#">Your profile</a>
+				<a class="dropdown-item" href="#">Settings</a>
+				<div class="dropdown-divider"></div>
+				<a class="dropdown-item" href="#" id="dropdown-item-logout">Logout</a>
+			`}
+		</div>
 	`;
 
-	const app = document.getElementById('app');
-	app.querySelector('header').innerHTML = navbarHTML;
+	document.getElementById('user-menu').innerHTML = userMenuHTML;
 
 	// ------- Logout simulation using mock -----------------------------
+
 	if (isLoggedIn) {
-		const logoutBtn = document.getElementById('dropdown-item-logout');
-		if (logoutBtn) {
-			logoutBtn.addEventListener('click', simulateLogout);
-		}
+		document.getElementById('dropdown-item-logout')?.addEventListener('click', simulateLogout);
 	}
 	// -----------------------------------------------------------------
 }
