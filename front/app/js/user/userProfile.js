fe5f591fd09250eabb6819508af2a8d67d09ed51
yusengok <ysengoku.tourisme@gmail.com>
2025-01-08 13:55:33 +0100

    minor: Add a temporary button to Game

--- front/app/js/user/userProfile.js
+++ front/app/js/user/userProfile.js
@@ -4,6 +4,10 @@ export function renderUserProfile() {
 	const userProfileHTML = `
 	<div class="container d-flex flex-column justify-content-center align-items-center text-center">
 		<h1>Your profile</h1>
+
+		<div class="mb-3 pt-5">
+			<a class="btn btn-outline-primary" href="#game" role="button">Go to Game</a>
+		</div>
 	</div>
 	`;
 	contentContainer.innerHTML = userProfileHTML;
