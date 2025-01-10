4141ba882b84fe3d03d0fc85f2dd111b8eb7a390
yusengok <ysengoku.tourisme@gmail.com>
2025-01-10 16:31:58 +0100

    minor: Working on Profile class

--- front/app/js/components/pages/Profile.js
+++ front/app/js/components/pages/Profile.js
@@ -1,25 +1,33 @@
+import { router } from '../../main.js';
+
 export class UserProfile extends HTMLElement {
 	constructor() {
 		super();
-		// this.user = null;
+		this.user = null;
 	}
 
 	connectedCallback() {
 		this.render();
 	}
 
-	// set user(userData) {
-	// 	this.user = userData;
-	// 	this.render();
-	// }
+	set userData(user) {
+		this.user = user;
+		this.render();
+	}
 
 	render() {
 		// If user is not logged in, redirect to login page
+		const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';  // This is temoporay simulation
+		if (!isLoggedIn) {
+			router.navigate('/login');
+			return;
+		}
 
+		// Temporary content
 		this.innerHTML = `
 		<div class="container d-flex flex-column justify-content-center align-items-center text-center">
-			<h1>This is Profile page</h1>
-
+			<h1>This is ${this.user.name}'s Profile</h1>
+			<p>ID: ${this.user.id}</p>
 			<div class="mb-3 pt-5">
 				<a class="btn btn-outline-primary" href="#game" role="button">Go to Game</a>
 			</div>
--- front/app/js/main.js
+++ front/app/js/main.js
@@ -9,8 +9,8 @@ const router = new Router();
 router.addRoute('/', 'landing-component');
 router.addRoute('/login', 'login-form');
 router.addRoute('/register', 'register-form');
-router.addRoute('/profile', 'user-profile');  // This is temporary
-// router.addRoute('/profile/:id', 'user-profile');
+// router.addRoute('/profile', 'user-profile');  // This is temporary
+router.addRoute('/profile/:id', 'user-profile');
 // Add all routes here
 
 document.addEventListener('DOMContentLoaded', () => {
