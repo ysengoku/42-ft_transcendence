d33d984a004b60b9b2ee5ca59f699270aab254c7
Boussard Fanny <boussard.fanny@gmail.com>
2025-01-15 19:25:54 +0100

    back is up

--- back/manage.py
+++ back/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mySite.settings')
 
     try:
         from django.core.management import execute_from_command_line
