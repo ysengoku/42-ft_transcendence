f8d6dea2dc345d839580b0d0b348fccb0b970f13
faboussa <boussard.fanny@gmail.com>
2025-01-16 18:05:56 +0100

    localhost 1026 working

--- back/mySite/users/migrations/api.py
+++
@@ -1,8 +0,0 @@
-# users/api.py
-from ninja import Router
-
-router = Router()
-
-@router.get("/profile")
-def profile(request):
-    return {"message": "This is the profile endpoint"}
---
+++ back/users/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.templatetags.static import static
+
+
+class User(AbstractUser):
+    pass
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(
+        upload_to="avatars/", null=True, blank=True
+    )
+    elo = models.PositiveIntegerField(default=1000)
+    friends = models.ManyToManyField(User, related_name='friends', blank=True)
+
+    @property
+    def avatar(self):
+        if self.profile_picture:
+            return self.profile_picture.url
+        return static("images/default.svg")
+
+    def __str__(self):
+        return self.user.username
