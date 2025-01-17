92825a96eb86bcb7291958d39f56687879aa4bee
Boussard Fanny <boussard.fanny@gmail.com>
2025-01-17 08:25:11 +0100

    all dockers show no errors

--- back/mySite/asgi.py
+++ back/mySite/asgi.py
@@ -6,15 +6,19 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
-
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mySite.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mySite.settings')
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": get_asgi_application(),  # Gère les requêtes HTTP
+    # Autres protocoles peuvent être ajoutés ici
+    # "websocket": AuthMiddlewareStack(
+    #     URLRouter(
+    #         # Add routing for WebSockets here
+    #     )
+    # ),
 })
