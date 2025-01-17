46bdb1268ef9acdb74780cd144ee34dd995967c9
faboussa <boussard.fanny@gmail.com>
2025-01-17 20:34:53 +0100

    init upload picture

--- back/users/models.py
+++ back/users/models.py
@@ -1,5 +1,13 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    avatar = models.ImageField(upload_to='avatars/', default='avatars/default_avatar.png')
+    avatar = models.ImageField(
+        upload_to='avatars/',
+        default='avatars/default_avatar.png',
+        null=True,
+        blank=True
+    )
+    
+    def __str__(self):
+        return self.username
