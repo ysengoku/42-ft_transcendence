d59ece0c8705a3e007995b04a0e193fe00fa6636
Boussard Fanny <boussard.fanny@gmail.com>
2025-01-17 08:25:11 +0100

    all dockers show no errors

--- database/healthcheck.sh
+++ database/healthcheck.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+set -e
 PGPASSWORD=$POSTGRES_PASSWORD psql -h database -U $POSTGRES_USER -d $POSTGRES_DB -c "SELECT 1" > /dev/null
+if [ $? -ne 0 ]; then
+  echo "Database is not healthy"
+  exit 1
+fi
+echo "Database is healthy"
+exit 0
