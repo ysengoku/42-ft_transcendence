b7cc3f34385f7bf2800872d367b4d17678b7dbcb
faboussa <boussard.fanny@gmail.com>
2025-01-17 17:47:17 +0100

    [fixed] - acces to django admin

--- back/mySite/urls.py
+++ back/mySite/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from ninja import NinjaAPI
+from django.conf import settings
+from django.conf.urls.static import static 
 
 api = NinjaAPI()
 
@@ -29,3 +31,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', api.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
