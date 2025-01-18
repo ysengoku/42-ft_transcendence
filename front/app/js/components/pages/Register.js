6bb134410962f18cee618ef9664ba4a958deef57
ysengoku <ysengoku.tourisme@gmail.com>
2025-01-18 15:32:31 +0100

    feat: test ok for Register POST request (except path to default avatar config)

--- front/app/js/components/pages/Register.js
+++ front/app/js/components/pages/Register.js
@@ -1,10 +1,12 @@
-// import "../../api/index.js";
-import { apiRequest } from "../../api/apiRequest.js"
-import { API_ENDPOINTS } from "../../api/endpoints.js";
+import { router } from '../../main.js';
+// import '../../api/index.js';
+import { apiRequest } from '../../api/apiRequest.js'
+import { API_ENDPOINTS } from '../../api/endpoints.js';
 
 export class Register extends HTMLElement {
 	constructor() {
 		super();
+		// this.isFormValid = true;
 	}
 
 	connectedCallback() {
@@ -14,68 +16,70 @@ export class Register extends HTMLElement {
 		const inputFields = this.querySelectorAll('input');
 		inputFields.forEach(field => {
 			field.addEventListener('input', () => {
-				field.style.borderColor = '';
+				field.classList.remove('is-invalid');
+				document.querySelector('#username-feedback').textContent = '';
+				// this.isFormValid = false;
 			} )
 		})
 	}
 
 	render() {
 		this.innerHTML = `
-		<div class="container d-flex flex-column justify-content-center align-items-center">
-			<form class="w-100">
-				  <div class="mb-3">
-					<label for="username" class="form-label">Username</label>
-					   <input type="username" class="form-control" id="username" placeholder="username">
-				  </div>
-				<div class="mb-3">
-					<label for="email" class="form-label">Email</label>
-					<input type="email" class="form-control" id="email" placeholder="email">
-				  </div>
-				<div class="mb-3">
-					<label for="password" class="form-label">Password</label>
-					<div class="col-auto">
-						<input type="password" class="form-control" id="password" placeholder="password" aria-describedby="passwordHelpInline">
-					</div>
-					<div class="col-auto">
-					    <span id="passwordHelpInline" class="form-text">
-      						Add message (error etc)
-    					</span>
-					</div>
-				</div>
-				<div class="mb-3">
-					<label for="password_repeat" class="form-label">Confirm Password</label>
-					<input type="confirmPassword" class="form-control" id="password_repeat" placeholder="password">
-				  </div>
-				<div class="mb-3 py-3">
-					<button type="submit" id="registerSubmit" class="btn btn-primary btn-lg w-100 pt-50">Register</button>
-				</div>
-			</form>
-		</div>
-		`;
+    		<div class='container d-flex flex-column justify-content-center align-items-center'>
+      			<form class='w-100'>
+        			<div class='mb-3'>
+          				<label for='username' class='form-label'>Username</label>
+          				<input type='username' class='form-control' id='username' placeholder='username'>
+          				<div class='invalid-feedback' id='username-feedback'></div>
+        			</div>
+        			<div class='mb-3'>
+          				<label for='email' class='form-label'>Email</label>
+          				<input type='email' class='form-control' id='email' placeholder='email'>
+          				<div class='invalid-feedback' id='email-feedback'></div>
+        			</div>
+        			<div class='mb-3'>
+          				<label for='password' class='form-label'>Password</label>
+        				<input type='password' class='form-control' id='password' placeholder='password'>
+        				<div class='invalid-feedback' id='password-feedback'></div>
+        			</div>
+        			<div class='mb-3'>
+        				<label for='password_repeat' class='form-label'>Confirm Password</label>
+        				<input type='password' class='form-control' id='password_repeat' placeholder='password'>
+        				<div class='invalid-feedback' id='password_repeat-feedback'></div>
+        			</div>
+        			<div class='mb-3 py-3'>
+        				<button type='submit' id='registerSubmit' class='btn btn-primary btn-lg w-100 pt-50'>Register</button>
+        			</div>
+      			</form>
+    		</div>
+  		`;
 	}
 
 	setupRegisterHandler() {
-		const form = this.querySelector("form");
-		form.addEventListener("submit", (event) => {
+		const form = this.querySelector('form');
+		form.addEventListener('submit', (event) => {
 	  		event.preventDefault();  // Prevent the default behavior of browser (page reload)
 	  		this.handleRegister();
 		});
 	}
 
 	async handleRegister() {
-		const usernameField = this.querySelector("#username");
-		const emailField = this.querySelector("#email");
-		const passwordField = this.querySelector("#password");
-		const password_repeatField = this.querySelector("#password_repeat");
+		const usernameField = this.querySelector('#username');
+		const emailField = this.querySelector('#email');
+		const passwordField = this.querySelector('#password');
+		const password_repeatField = this.querySelector('#password_repeat');
 
-		if (!usernameField.value || emailField.value.trim() === "" ||
-		passwordField.value.trim() === "" || password_repeatField.value.trim() === "") {
-			if (!usernameField.value) this.querySelector("#username").style.borderColor = "red";
-			if (!emailField.value) this.querySelector("#email").style.borderColor = "red";
-			if (!passwordField.value) this.querySelector("#password").style.borderColor = "red";
-			if (!password_repeatField.value) this.querySelector("#password_repeat").style.borderColor = "red";
+		// Input validation at Front-end
+		let isFormValid = true;
+		isFormValid = this.checkInput(usernameField, '#username-feedback', 'Username is required');
+		isFormValid = this.checkInput(emailField, '#email-feedback', 'Email is required') && isFormValid;
+		isFormValid = this.checkInput(passwordField, '#password-feedback', 'Password is required') && isFormValid;
+		isFormValid = this.checkInput(password_repeatField, '#password_repeat-feedback', 'Please confirm your password') && isFormValid;
+		isFormValid = this.checkPasswordDiff(passwordField, password_repeatField) && isFormValid;
+		if (!isFormValid) {
 			return;
 		}
+
 		const userData = {
 			username: usernameField.value,
 			email: emailField.value,
@@ -85,10 +89,12 @@ export class Register extends HTMLElement {
 
     	try {
         	const response = await apiRequest('POST', API_ENDPOINTS.REGISTER, userData);
+			// const response = await apiRequest('POST', "https://run.mocky.io/v3/5567d4b4-2918-4ebc-9b1a-9fbace08419a", userData);
         	console.log('Registration successful:', response);
 			//----- Temporary ------------------------------------------
 			localStorage.setItem('isLoggedIn', 'true');
-			localStorage.setItem('user', JSON.stringify(response.user));
+			const { elo, ...filteredResponse } = response;
+			localStorage.setItem('user', JSON.stringify(filteredResponse));
 			//----------------------------------------------------------
 			const navBar = document.getElementById('navbar-container');
 			navBar.innerHTML = '<navbar-component></navbar-component>';
@@ -97,6 +103,28 @@ export class Register extends HTMLElement {
         	// Error handling
 			console.error('Error registering user:', error);
     	}
+	}
+
+	checkInput(field, feedbackSelector, errorMessage) {
+		if (!field.value.trim()) {
+			field.classList.add('is-invalid');
+			document.querySelector(feedbackSelector).textContent = errorMessage;
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	checkPasswordDiff(passwordField, password_repeatField) {
+		if (passwordField.value != password_repeatField.value) {
+			passwordField.classList.add('is-invalid');
+			password_repeatField.classList.add('is-invalid');
+			document.querySelector('#password-feedback').textContent = 'Passwords do not match';
+			document.querySelector('#password_repeat-feedback').textContent = 'Passwords do not match';
+			return false;
+		} else {
+			return true;
+		}
 	}
 }
 
--- front/doc/API_ENDPOINT.md
+++ front/doc/API_ENDPOINT.md
@@ -2,7 +2,7 @@
 
 ## User management
 
-### /api/users
+### Endpoint: /api/users/
 
 #### POST
 
@@ -20,25 +20,45 @@
 201(Created)
 ```json
 {
-  "avatar": "string",
-  "username": "string",
-  "date_joined": "2025-01-17T20:00:56.092Z",
-  "email": "string",
-  "password": "string"
+	"username": "string",
+	"avatar": "string",
+	"elo": 1000
 }
 ```
+---
+### Endpoint: /api/users/{username}
 
 #### GET
-
+##### Response
 200 (OK)
 ```json
 {
+	"username": "string",
 	"avatar": "string",
-    "username": "string",
-    "date_joined": "2025-01-17T20:00:06.810Z",
-    "email": "string",
-    "password": "string"
+	"date_joined": "2025-01-18T12:47:27.588Z",
+	"winrate": 0,
+ 	"worst_enemy": {
+		"username": "string",
+		"avatar": "string",
+		"elo": 1000
+	},
+	 "best_enemy": {
+		"username": "string",
+		"avatar": "string",
+		"elo": 1000
+	},
+	"scored_balls": 0,
+	"elo_history": [
+		{
+			"date": "2025-01-18T12:47:27.588Z",
+			"elo_change_signed": 0,
+			"elo_result": 0
+		}
+	],
+	"is_online": true,
+	"elo": 1000,
+	"friends": [
+		0
+	]
 }
 ```
-
-### /api/users/{username}
