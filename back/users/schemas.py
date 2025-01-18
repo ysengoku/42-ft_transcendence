f330f6d0aab907dedc96cd67851a31e99639697f
faboussa <boussard.fanny@gmail.com>
2025-01-18 19:32:33 +0100

    gunicorn instead of daphne to be able to connect to localhost:8000

--- back/users/schemas.py
+++ back/users/schemas.py
@@ -3,3 +3,8 @@ from typing import Optional
 
 class UserAvatarSchema(BaseModel):
     avatar: Optional[str] = None  # Champ pour l'upload d'avatar
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    avatar: Optional[str] = None  # L'avatar peut être vide ou avoir une URL
