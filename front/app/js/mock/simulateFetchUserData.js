a78ee88ece6d5b853eef2514250200d47e7feb57
faboussa <boussard.fanny@gmail.com>
2025-01-18 23:31:24 +0100

    [enhanced] we created volumes for database media and static files on the project so they are persistent

--- front/app/js/mock/simulateFetchUserData.js
+++ front/app/js/mock/simulateFetchUserData.js
@@ -1,17 +1,17 @@
 export async function simulateFetchUserData(userId) {
 	const mockUserData = [
-		{ userid: '12345', name: 'JohnDoe', avatar: '/assets/img/sample_avatar.jpg', online: true },
-		{ userid: '123', name: 'Alice', avatar: '/assets/img/sample_avatar2.jpg', online: true },
-		{ userid: '234', name: 'Jane', avatar: '/assets/img/sample_avatar3.jpg', online: false },
-		{ userid: '345', name: 'George', avatar: '/assets/img/sample_avatar.jpg', online: true },
-		{ userid: '456', name: 'Lucas', avatar: '/assets/img/sample_avatar.jpg', online: false },
-		{ userid: '789', name: 'Emily', avatar: '/assets/img/sample_avatar.jpg', online: true },
-	  ];
+		{ userid: '12345', name: 'JohnDoe', avatar: 'app/media/default_avatar.png', online: true },
+		{ userid: '123', name: 'Alice', avatar: '/media/default_avatar.png', online: true },
+		{ userid: '234', name: 'Jane', avatar: '/media/default_avatar.png', online: false },
+		{ userid: '345', name: 'George', avatar: '/media/default_avatar.png', online: true },
+		{ userid: '456', name: 'Lucas', avatar: '/media/default_avatar.png', online: false },
+		{ userid: '789', name: 'Emily', avatar: '/media/default_avatar.png', online: true },
+	];
 
 	const user = mockUserData.find((user) => user.userid === userId);
 
 	if (!user) {
-		return { userid: userId, name: 'Unknown User', avatar: '/assets/img/sample_avatar.jpg', online: false };
+		return { userid: userId, name: 'Unknown User', avatar: '/media/default_avatar.png', online: false };
 	}
 	return user;
 }
