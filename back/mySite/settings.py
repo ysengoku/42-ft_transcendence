0e4d515db09432d97e17194b9535e360256c97a7
faboussa <boussard.fanny@gmail.com>
2025-01-18 21:18:43 +0100

    media volume shared in nginx

--- back/mySite/settings.py
+++ back/mySite/settings.py
@@ -157,18 +157,13 @@ SECURE_SSL_REDIRECT = False
 # Configuration for picture
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR / 'static' # / is to concatenate the path
-
-
-# Le dossier où les fichiers statiques seront collectés
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 # # Si vous avez des fichiers statiques supplémentaires (par exemple, dans un dossier statique pour l'admin), ajoutez-les ici :
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Pour tout fichier statique supplémentaire que vous avez
