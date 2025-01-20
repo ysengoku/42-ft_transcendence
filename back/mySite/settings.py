c2e04db3ba10165ff6f48fa40ed86a3493d5f1c2
faboussa <boussard.fanny@gmail.com>
2025-01-20 18:53:02 +0100

    static files are in static front

--- back/mySite/settings.py
+++ back/mySite/settings.py
@@ -108,7 +108,6 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-STATIC_URL = '/static/'
 
 SITE_ID = 1
 
@@ -157,15 +156,10 @@ SECURE_SSL_REDIRECT = False
 # Configuration for picture
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# for dynamic images (uploaded by user)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_URL = '/static/'
-# 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# # Si vous avez des fichiers statiques supplémentaires (par exemple, dans un dossier statique pour l'admin), ajoutez-les ici :
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Pour tout fichier statique supplémentaire que vous avez
-]
+STATIC_URL = '/static/back/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'back')
--- front/app/js/mock/simulateFetchUserData.js
+++ front/app/js/mock/simulateFetchUserData.js
@@ -1,11 +1,11 @@
 export async function simulateFetchUserData(userId) {
 	const mockUserData = [
-		{ userid: '12345', name: 'JohnDoe', avatar: 'app/media/default_avatar.png', online: true },
-		{ userid: '123', name: 'Alice', avatar: '/media/default_avatar.png', online: true },
-		{ userid: '234', name: 'Jane', avatar: '/media/default_avatar.png', online: false },
-		{ userid: '345', name: 'George', avatar: '/media/default_avatar.png', online: true },
-		{ userid: '456', name: 'Lucas', avatar: '/media/default_avatar.png', online: false },
-		{ userid: '789', name: 'Emily', avatar: '/media/default_avatar.png', online: true },
+		{ userid: '12345', name: 'JohnDoe', avatar: 'app/media/avatars/default_avatar.png', online: true },
+		{ userid: '123', name: 'Alice', avatar: '/media/avatars/default_avatar.png', online: true },
+		{ userid: '234', name: 'Jane', avatar: '/media/avatars/default_avatar.png', online: false },
+		{ userid: '345', name: 'George', avatar: '/media/avatars/default_avatar.png', online: true },
+		{ userid: '456', name: 'Lucas', avatar: '/media/avatars/default_avatar.png', online: false },
+		{ userid: '789', name: 'Emily', avatar: '/media/avatars/default_avatar.png', online: true },
 	];
 
 	const user = mockUserData.find((user) => user.userid === userId);
