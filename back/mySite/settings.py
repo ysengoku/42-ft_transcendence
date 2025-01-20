76705eb9b11247364a1255465a2ab3968b70c132
faboussa <boussard.fanny@gmail.com>
2025-01-20 17:32:03 +0100

    creating back and front static folder in nginx docker

--- back/mySite/settings.py
+++ back/mySite/settings.py
@@ -163,6 +163,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = '/static/'
+# 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # # Si vous avez des fichiers statiques supplémentaires (par exemple, dans un dossier statique pour l'admin), ajoutez-les ici :
 STATICFILES_DIRS = [
