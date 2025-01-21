ab900f3fb1de993fb59fc621386cd42cfadc37bc
yusengok <ysengoku.tourisme@gmail.com>
2025-01-21 14:29:57 +0100

    minor: Add 'delete account button' and make avatar upload module (Need to add

--- front/app/mock/functions/simulateFetchUserData.js
+++ front/app/mock/functions/simulateFetchUserData.js
@@ -1,31 +1,36 @@
 export async function simulateFetchUserData(username) {
 	const user1 = {
 		username: 'JohnDoe', 
-		avatar: '/media/default_avatar.png', 
+		avatar: '/media/default_avatar.png',
+		hasOwnAvatar: true,
 		email: 'johndoe@example.com'
 	  };
 	  
 	const user2 = {
 		username: 'Alice', 
-		avatar: '/mock/img/avatars/sample_avatar2.jpg', 
+		avatar: '/mock/img/avatars/sample_avatar2.jpg',
+		hasOwnAvatar: true,
 		email: 'alice@example.com'
 	};
 
 	const user3 = {
 		username: 'Jane', 
-		avatar: '/mock/img/avatars/sample_avatar3.jpg', 
+		avatar: '/mock/img/avatars/sample_avatar3.jpg',
+		hasOwnAvatar: false,
 		email: 'jane@example.com'
 	  };
 	  
 	const user4 = {
 		username: 'George', 
-		avatar: '/mock/img/avatars/sample_avatar.jpg', 
+		avatar: '/mock/img/avatars/sample_avatar.jpg',
+		hasOwnAvatar: false,
 		email: 'george@example.com'
 	  };
 	  
 	const user5 = {
 		username: 'Lucas', 
-		avatar: '/mock/img/avatars/sample_avatar4.jpg', 
+		avatar: '/mock/img/avatars/sample_avatar4.jpg',
+		hasOwnAvatar: true,
 		email: 'lucas@example.com'
 	  };
 	  
