fcf57b5221713d2f431633a0111e0954fb4a7af0
yusengok <ysengoku.tourisme@gmail.com>
2025-01-21 14:29:57 +0100

    minor: Add 'delete account button' and make avatar upload module (Need to add

--- front/app/js/components/pages/settings/Settings.js
+++ front/app/js/components/pages/settings/Settings.js
@@ -1,4 +1,5 @@
 import { simulateFetchUserData } from '../../../../mock/functions/simulateFetchUserData.js'
+import './components/index.js';
 
 export class Settings extends HTMLElement {
 	constructor() {
@@ -22,60 +23,52 @@ export class Settings extends HTMLElement {
   	}
 
 	render() {
-		// Temporay to test
-		this.user.hasOwnAvatar = true;
-
-		const avatarUploadMessage = this.user.hasOwnAvatar ? 'Change Avatar' : 'Upload Avatar';
-
 		this.innerHTML = `
 		<div class="container">
 			<form>
 				<legend class="mb-5 border-bottom">Settings</legend>
-
-				<div class="d-flex align-items-start mb-3 pb-5 border-bottom">
-					<div class="col-6 profile-avatar-container">
-						<img src="${this.user.avatar}" alt="User Avatar" class="rounded-circle">
-					</div>
-					<div class="pl-sm-4 pl-2">
-						<b>Avatar</b>
-						<button class="btn btn-primary" id="avatar-upload-button">${avatarUploadMessage}</button>
-					</div>
+				<div>
+					<avatar-upload></avatar-upload>
 				</div>
 
-				<div class="mb-3 pb-5 border-bottom">
+				<div class="mb-3 pb-3">
 					<label for="username" class="form-label">Username</label>
 					<input type="username" class="form-control" id="username" placeholder="${this.user.username}">
-					<div class="invalid-feedback" id="username-feedback"></div>
 				</div>
-				<div class="mb-3 pb-5 border-bottom">
+				<div class="mb-3 pb-3">
 					<label for="email" class="form-label">Email</label>
 					<input type="email" class="form-control" id="email" placeholder="${this.user.email}">
-					<div class="invalid-feedback" id="email-feedback"></div>
 				</div>
-				<div class="mb-3 pb-5 border-bottom">
+				<div class="mb-3 pb-3">
 					<label for="password" class="form-label">Password</label>
 					<input type="password" class="form-control" id="password" placeholder="new password">
 					<div class="invalid-feedback" id="password-feedback"></div>
 				</div>
-				<div class="mb-3 pb-5">
+				<div class="mb-3 pb-3">
 					<label for="password_repeat" class="form-label">Confirm Password</label>
 					<input type="password" class="form-control" id="password_repeat" placeholder="new password">
 					<div class="invalid-feedback" id="password_repeat-feedback"></div>
 				</div>
 
-				<div class="mb-3 p-3 border-bottom">
-					<a class="btn btn-outline-primary mx-2" href="/profile/${this.user.username}" role="button">Cancel</a>
-					<button type="submit" id="settingsSubmit" class="btn btn-primary btn mx-2">Save changes</button>
+				<div class="mb-5 py-5 border-bottom">
+					<a class="btn btn-outline-primary" href="/profile/${this.user.username}" role="button">Cancel</a>
+					<button type="submit" id="settingsSubmit" class="btn btn-primary mx-2">Save changes</button>
 				</div>
 
-				<div class="mb-3 p-3">
-					<a class="btn btn-danger" href="/home" role="button">Delete account</a>
-				</div>
+				<delete-account-button></delete-account-button>
 			</form>
 	
 			<avatar-upload-modal></avatar-upload-modal>
 		</div>
 		`;
+
+		const avatarUploadButton = this.querySelector('avatar-upload');
+		avatarUploadButton.setAvatar(this.user);
+
+		const deleteAccountButton = this.querySelector('delete-account-button');
+		deleteAccountButton.setUsername(this.user.username);
+
+		this.setupSubmitHandler();
 	}
 
 	setupSubmitHandler() {
@@ -84,8 +77,21 @@ export class Settings extends HTMLElement {
 	  		event.preventDefault();  // Prevent the default behavior of browser (page reload)
 	  		this.handleSubmit();
 		});
+	}
 
-		
+	async handleSubmit() {
+		const usernameField = this.querySelector('#username');
+		const emailField = this.querySelector('#email');
+		const passwordField = this.querySelector('#password');
+		const password_repeatField = this.querySelector('#password_repeat');
+
+		const userData = {
+			username: usernameField.value,
+			email: emailField.value,
+			password: passwordField.value,
+			password_repeat: password_repeatField.value
+		};	
+
 	}
 }
 
