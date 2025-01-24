4d2010193382bfb57a1b5161e32abf194f504443
emuminov <jseldardw@gmail.com>
2025-01-24 16:51:42 +0100

    refactor: changed validation

--- server/users/management/commands/orm_scratch.py
+++ server/users/management/commands/orm_scratch.py
@@ -1,19 +1,14 @@
 from django.core.management.base import BaseCommand
 from users.models import Match, Profile
-from django.db.models import Q, Sum, When, Case
+from django.db.models import Q, Sum, When, Case, Count
 
 
 class Command(BaseCommand):
     help = 'Creates application data'
 
     def handle(self, **kwargs):
-        p = Profile.objects.get(user__username="Celia")
-        scored_balls_stats = p.matches.aggregate(
-            scored_balls=Sum(Case(
-                When(loser=p, then="losers_score"),
-                When(winner=p, then="winners_score"),
-            )),
-            # lscore=Sum('losers_score', filter=Q(loser=self)),
-            # wscore=Sum('winners_score', filter=Q(winner=self)),
+        p = Profile.objects.filter(user__username="celiastral")
+        res = p.annotate(
+            winss=Count('won_matches', distinct=True),
+            losess=Count('lost_matches', distinct=True)
         )
-        print(scored_balls_stats)
