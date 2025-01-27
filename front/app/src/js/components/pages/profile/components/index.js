46b9892df3d82d3a1059d205e2484f265c8a5876
yusengok <ysengoku.tourisme@gmail.com>
2025-01-27 14:53:26 +0100

    feat: Add user actions component module to Profile (Need to add blocked case handling)

--- front/app/src/js/components/pages/profile/components/index.js
+++ front/app/src/js/components/pages/profile/components/index.js
@@ -1,6 +1,7 @@
 import { OnlineStatusIndicator } from './OnlineStatusIndicator.js';
 import { ProfileAvatar } from './Avatar.js';
 import { ProfileUserInfo } from './UserInfo.js';
+import { ProfileUserActions } from './UserActions.js';
 import { ProfileEnemy } from './Enemy.js';
 
-export { OnlineStatusIndicator, ProfileAvatar, ProfileUserInfo, ProfileEnemy };
+export { OnlineStatusIndicator, ProfileAvatar, ProfileUserInfo, ProfileUserActions, ProfileEnemy };
