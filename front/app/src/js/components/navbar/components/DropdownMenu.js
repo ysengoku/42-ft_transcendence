52ff055f8f33c71faef31091570c6aa6a1cb1467
yusengok <ysengoku.tourisme@gmail.com>
2025-01-30 14:03:04 +0100

    minor: Add endpoint Login

--- front/app/src/js/components/navbar/components/DropdownMenu.js
+++ front/app/src/js/components/navbar/components/DropdownMenu.js
@@ -16,14 +16,6 @@ export class DropdownMenu extends HTMLElement {
     // Temporary solution with localStorage
     const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
     const isDarkMode = ThemeController.getTheme() === 'dark';
-    // const storedUser = localStorage.getItem('user');
-    // let username = null;
-    // let avatarSrc = `${anonymousavatar}`;
-    // if (user) {
-    // 	const user = JSON.parse(storedUser);
-    // 	username = user.username;
-    // 	avatarSrc = `${user.avatar}`;
-    // }
     let username = null;
     let avatarSrc = `${anonymousavatar}`;
     const storedUser = localStorage.getItem('user');
