d566047a17dd2e68dfb0d56e37ab2c4a90426955
ysengoku <ysengoku.tourisme@gmail.com>
2025-01-31 18:07:43 +0100

    feat: Login request and response handling, Add csrf token clear in hanleLogout

--- front/app/src/js/utils/handleLogout.js
+++ front/app/src/js/utils/handleLogout.js
@@ -1,10 +1,11 @@
-import {router} from '@router';
+import { router } from '@router';
 import '@components/navbar/components/DropdownMenu.js';
 
 export function handleLogout(event) {
   event.preventDefault();
   localStorage.removeItem('isLoggedIn'); // Need to delete later
   localStorage.removeItem('user');
+  document.cookie = `csrftoken=; Max-Age=0; path=/;`;
 
   const navBar = document.getElementById('navbar-container');
   navBar.innerHTML = '<navbar-component></navbar-component>';
