0aa4391f22483aa5f810fae803655b6515fc21af
ysengoku <ysengoku.tourisme@gmail.com>
2025-01-31 18:07:43 +0100

    feat: Login request and response handling, Add csrf token clear in hanleLogout

--- front/app/src/js/api/apiRequest.js
+++ front/app/src/js/api/apiRequest.js
@@ -1,3 +1,30 @@
+/**
+ * Makes an API request with the specified method and endpoint.
+ *
+ * @async
+ * @function
+ * @param {string} method - The HTTP method (GET, POST, DELETE, etc.).
+ * @param {string} endpoint - The API endpoint to make the request to.
+ * @param {object|null} [data=null] - The data to be sent with the request, for POST or PUT requests. Defaults to null.
+ * @param {boolean} [isFileUpload=false] - Whether the request involves file uploading. Defaults to false.
+ * @param {boolean} [needToken=true] - Whether a CSRF token is needed for the request. Defaults to true.
+ * @returns {Promise<Response>} The response object from the fetch request if successful.
+ * @throws {Error} Throws an error with the status and response data if the request fails.
+ *
+ * @example
+ * // Example usage: Sending a GET request to fetch user data
+ * try {
+ *   const response = await apiRequest('GET', '/api/user/data');
+ *   const data = await response.json();
+ *   console.log('User Data:', data);
+ * } catch (error) {
+ *   console.error('Error fetching user data:', error.message);
+ *   if (error.response) {
+ *     console.error('Response Data:', error.response);
+ *   }
+ * }
+ */
+
 export async function apiRequest(method, endpoint, data = null, isFileUpload = false, needToken = true) {
   function getCSRFTokenfromCookies() {
     const name = 'csrftoken';
@@ -5,7 +32,7 @@ export async function apiRequest(method, endpoint, data = null, isFileUpload = f
     if (document.cookie && document.cookie !== '') {
       const cookies = document.cookie.split(';');
       for (let i = 0; i < cookies.length; i++) {
-        let cookie = cookies[i].trim();
+        const cookie = cookies[i].trim();
         if (cookie.startsWith(name)) {
           token = decodeURIComponent(cookie.substring(name.length + 1));
           break;
@@ -41,31 +68,22 @@ export async function apiRequest(method, endpoint, data = null, isFileUpload = f
   try {
     const response = await fetch(url, options);
     if (response.ok) {
-      return await response.json();
+      console.log('Request successful:', response);
+      return response;
     }
-    console.error('Request failed:', response);
-    const error = new Error();
+    const error = new Error('Request failed');
     error.status = response.status;
     let errorData = null;
-    try {
-      console.log('Error response:', response);
+    const contentType = response.headers.get('Content-Type');
+    if (contentType && contentType.includes('application/json')) {
       errorData = await response.json();
-      error.response = errorData;
-    } catch (jsonError) {
-      console.error('Failed to parse JSON:', jsonError);
+    } else if (contentType && contentType.includes('text/html')) {
+      errorData = await response.text();
     }
+    console.log('Error Data: ', errorData);
+    error.response = errorData;
     throw error;
   } catch (error) {
     throw error;
   }
 }
-
-// Example usage
-// async function getUserData(username) {
-//     try {
-//         const data = await apiRequest('GET', API_ENDPOINTS.GET_USER_DATA(username));
-//         console.log(data);
-//     } catch (error) {
-//         // Error handling
-//     }
-// }
