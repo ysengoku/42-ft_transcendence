85cb1fab1e9ac8abc36639e553cf8d70c5348a76
emuminov <jseldardw@gmail.com>
2025-02-01 19:24:49 +0100

    style: fix of the ruff issues for the ORM scratch

--- server/users/management/commands/orm_scratch.py
+++ server/users/management/commands/orm_scratch.py
@@ -1,21 +1,15 @@
 from django.core.management.base import BaseCommand
-from django.db.models import Count
 
-from users.jwt import create_jwt, verify_jwt
-from users.models import Profile, User
+from users.models import User
 
 
 class Command(BaseCommand):
     help = "Creates application data"
 
     def handle(self, **kwargs) -> None:
-        u1 = User.objects.create_user(
-            username="Pedro", password="123", email="pedro1@gmail.com", connection_type=User.REGULAR
+        User.objects.create_user(
+            username="Pedro", password="123", email="pedro1@gmail.com", connection_type=User.REGULAR  # noqa: S106
         )
-        print(u1.connection_type)
-        u2 = User.objects.create_user(
-            username="Pedro", password="123", email="pedro2@gmail.com", connection_type=User.FT
+        User.objects.create_user(
+            username="Pedro", password="123", email="pedro2@gmail.com", connection_type=User.FT  # noqa: S106
         )
-        print(u2.connection_type)
-        print(u1.slug)
-        print(u2.slug)
