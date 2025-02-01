69c13fd6c065ed89dc3c9a9733f21d10be140475
Boussard Fanny <boussard.fanny@gmail.com>
2025-02-01 11:33:13 +0100

    added meta to 2fa class

--- server/users/models.py
+++ server/users/models.py
@@ -380,5 +380,13 @@ class Match(models.Model):
 
 class TwoFactorAuth(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    secret = models.charField(max_length=32) # secret key for the user
+    secret = models.CharField(max_length=32) # secret key for the user
     is_enabled = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "user_2fa"
+
+    def __str__(self):
+        return f"2FA for {self.user.slug_id}"
