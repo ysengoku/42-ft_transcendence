afbdb81e1a774c0bda07fe71fe7f2151bb336311
pdlmn <jseldardw@gmail.com>
2025-02-02 15:03:17 +0100

    perf: optimized the number of queries for the get full profile endpoint refactor: changed some of the function names, added profile manager, changed endpoints functions

--- server/users/api/endpoints/users.py
+++ server/users/api/endpoints/users.py
@@ -1,11 +1,12 @@
 from django.core.exceptions import PermissionDenied, RequestDataTooBig
-from django.db.models import Exists, Q
+from django.db.models import Q
 from django.http import HttpRequest
 from ninja import File, Form, Router
 from ninja.errors import AuthenticationError, HttpError
 from ninja.files import UploadedFile
 from ninja.pagination import paginate
 
+from users.api.common import get_profile_queryset_by_username_or_404
 from users.models import Profile
 from users.schemas import (
     Message,
@@ -46,13 +47,8 @@ def get_user(request: HttpRequest, username: str):
     Gets a specific user by username.
     """
     curr_user = request.auth
-    return (
-        Profile.objects.filter(user__username=username)
-        .annotate(is_friend=Exists(curr_user.profile.friends.filter(user__username=username)))
-        .annotate(is_blocked_user=Exists(curr_user.profile.blocked_users.filter(user__username=username)))
-        .annotate(is_blocked_by_user=Exists(curr_user.profile.blocked_users_of.filter(user__username=username)))
-        .first()
-    )
+    user_profile = get_profile_queryset_by_username_or_404(username)
+    return user_profile.with_full_profile(curr_user, username).first()
 
 
 # TODO: add authorization to settings change
