88d81fdc3033e27dd75ca627fb4cdc0784e0ab56
pdlmn <jseldardw@gmail.com>
2025-02-02 21:09:39 +0100

    refactor: split models.py into it's own directory

--- server/users/models/profile.py
+++ server/users/models/profile.py
@@ -1,158 +1,22 @@
 from pathlib import Path
 
 import magic
-from django.contrib.auth.hashers import make_password
-from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.models import UserManager as BaseUserManager
-from django.contrib.auth.validators import UnicodeUsernameValidator
-from django.core.exceptions import PermissionDenied, RequestDataTooBig, ValidationError
+from django.core.exceptions import RequestDataTooBig, ValidationError
 from django.db import models
 from django.db.models import Case, Count, Exists, ExpressionWrapper, F, Func, IntegerField, Q, Sum, Value, When
 from django.db.models.lookups import Exact
 from ninja.files import UploadedFile
 
-from .stats_calc import calculate_elo_change, calculate_winrate
-from .utils import merge_err_dicts
+from users.utils import merge_err_dicts
+
+from .user import User
 
 
-class UserManager(BaseUserManager):
-    def for_username_or_email(self, identifier: str):
-        return self.filter(Q(username__iexact=identifier) | Q(email=identifier))
-
-    def for_username(self, username: str):
-        return self.filter(username__iexact=username)
-
-    def fill_user_data(self, username: str, connection_type: str, **extra_fields):
-        username = AbstractUser.normalize_username(username)
-        if connection_type != self.model.REGULAR:
-            username = self._generate_unique_username(username)
-        if extra_fields.get("password"):
-            extra_fields["password"] = make_password(extra_fields.get("password"))
-        if extra_fields.get("email"):
-            extra_fields["email"] = BaseUserManager.normalize_email(extra_fields.get("email"))
-        extra_fields.setdefault("nickname", username)
-        return self.model(username=username, connection_type=connection_type, **extra_fields)
-
-    def create_user(self, username: str, connection_type: str, **extra_fields):
-        extra_fields["is_superuser"] = False
-        user = self.fill_user_data(username, connection_type, **extra_fields)
-        user.full_clean()
-        user.save()
-        return user
-
-    def validate_and_create_user(self, username: str, connection_type: str, **extra_fields):
-        extra_fields["is_superuser"] = False
-        user = self.fill_user_data(username, connection_type, **extra_fields)
-        user.full_clean()
-        user.save()
-        return user
-
-    def _generate_unique_username(self, username: str):
-        base_username = username
-        counter = self.filter(username__iexact=username).count()
-        if counter > 0:
-            return f"{base_username}-{counter}"
-        return base_username
-
-
-class User(AbstractUser):
-    """
-    Contains information that is related to authentication and authorization of one single user.
-    """
-
-    FT = "42"
-    GITHUB = "github"
-    REGULAR = "regular"
-    CONNECTION_TYPES_CHOICES = (
-        (FT, "42 School API"),
-        (GITHUB, "Github API"),
-        (REGULAR, "Our Own Auth"),
-    )
-
-    REQUIRED_FIELDS = ()
-
-    username_validator = UnicodeUsernameValidator()
-
-    username = models.CharField(max_length=50, validators=[username_validator], unique=True)
-    nickname = models.CharField(max_length=50, validators=[username_validator])
-    email = models.EmailField(default="")
-    connection_type = models.CharField(max_length=10, choices=CONNECTION_TYPES_CHOICES, default="regular")
-    password = models.CharField(max_length=128, default="")
-
-    objects = UserManager()
-
-    def validate_unique(self, *args: list, **kwargs: dict) -> None:
-        if "username" not in kwargs["exclude"] and User.objects.filter(username__iexact=self.username).exists():
-            raise ValidationError({"username": ["A user with that username already exists."]})
-        if (
-            "email" not in kwargs["exclude"]
-            and self.connection_type == User.REGULAR
-            and User.objects.filter(email=self.email).exists()
-        ):
-            raise ValidationError({"email": ["A user with that email already exists."]})
-        kwargs["exclude"] |= {"username", "email"}
-        super().validate_unique(*args, **kwargs)
-
-    def clean(self):
-        if not self.password and self.connection_type == User.REGULAR:
-            raise ValidationError({"password": ["This connection type requires a password."]})
-        if not self.email and self.connection_type == User.REGULAR:
-            raise ValidationError({"password": ["Email is required."]})
-
-    def update_user(self, data, new_profile_picture: UploadedFile | None):
-        err_dict = {}
-
-        if new_profile_picture:
-            try:
-                self.profile.update_avatar(new_profile_picture)
-            except ValidationError as exc:
-                err_dict = merge_err_dicts(err_dict, exc.error_dict)
-
-        if data.old_password and not data.password:
-            err_dict = merge_err_dicts(err_dict, {"password": ["Please enter your new password."]})
-
-        if data.password and data.password_repeat:
-            is_old_password_valid = self.check_password(password=data.old_password)
-            if not is_old_password_valid:
-                raise PermissionDenied
-            if data.old_password == data.password:
-                err_dict = merge_err_dicts(
-                    err_dict,
-                    {"password": ["New password cannot be the same as the old password."]},
-                )
-            self.set_password(data.password)
-            data.password = ""
-
-        if data.username == self.username:
-            err_dict = merge_err_dicts(err_dict, {"username": ["New username cannot be the same as the old username."]})
-
-        if data.email == self.email:
-            err_dict = merge_err_dicts(err_dict, {"email": ["New email cannot be the same as the old email."]})
-
-        for key, val in data:
-            if val and hasattr(self, key):
-                setattr(self, key, val)
-
-        exclude = set()
-        if not data.email:
-            exclude.add("email")
-        if not data.username:
-            exclude.add("username")
-
-        try:
-            self.validate_unique(exclude=exclude)
-            self.full_clean(validate_unique=False)
-        except ValidationError as exc:
-            err_dict = merge_err_dicts(err_dict, exc.error_dict)
-
-        if err_dict:
-            raise ValidationError(err_dict)
-        self.save()
-        self.profile.save()
-        return self
-
-    def __str__(self):
-        return f"{self.username} - {self.connection_type}"
+def calculate_winrate(wins: int, loses: int) -> int | None:
+    total = wins + loses
+    if total == 0:
+        return None
+    return round(wins / (total) * 100)
 
 
 class ProfileQuerySet(models.QuerySet):
@@ -358,61 +222,9 @@ class Friendship(models.Model):
         constraints = [
             # Block self-friendship
             models.CheckConstraint(check=~models.Q(from_profile=models.F("to_profile")), name="no_self_friendship"),
-            # Prevent duplicate entries (A→B can only exist once)
+            # Prevent duplicate entries (A->B can only exist once)
             models.UniqueConstraint(fields=["from_profile", "to_profile"], name="unique_friendship"),
         ]
 
     def __str__(self):
         return f"{self.from_profile.user.username} likes {self.to_profile.user.username}"
-
-
-class MatchManager(models.Manager):
-    MINUMUM_ELO = 100
-    K_FACTOR = 32
-    WIN = 1
-    DRAW = 0.5
-    LOSS = 0
-
-    def resolve(self, winner: Profile, loser: Profile, winners_score: int, losers_score: int):
-        """
-        Resolves all elo calculations, updates profiles of players,
-        creates a new match record and saves everything into the database.
-        """
-        elo_change = calculate_elo_change(winner.elo, loser.elo, MatchManager.WIN, MatchManager.K_FACTOR)
-        if (loser.elo - elo_change) < MatchManager.MINUMUM_ELO:
-            elo_change = loser.elo - MatchManager.MINUMUM_ELO
-        winner.elo += elo_change
-        loser.elo -= elo_change
-        resolved_match = Match(
-            winner=winner,
-            loser=loser,
-            winners_score=winners_score,
-            losers_score=losers_score,
-            elo_change=elo_change,
-            winners_elo=winner.elo,
-            losers_elo=loser.elo,
-        )
-        resolved_match.save()
-        winner.save()
-        loser.save()
-        return resolved_match
-
-
-class Match(models.Model):
-    winner = models.ForeignKey(Profile, related_name="won_matches", on_delete=models.SET_NULL, null=True)
-    loser = models.ForeignKey(Profile, related_name="lost_matches", on_delete=models.SET_NULL, null=True)
-    winners_score = models.IntegerField()
-    losers_score = models.IntegerField()
-    elo_change = models.IntegerField()
-    winners_elo = models.IntegerField(default=1000)
-    losers_elo = models.IntegerField(default=1000)
-    date = models.DateTimeField(auto_now_add=True)
-
-    objects = MatchManager()
-
-    class Meta:
-        verbose_name_plural = "matches"
-        ordering = ["-date"]
-
-    def __str__(self) -> str:
-        return f"{self.winner.user.username} - {self.loser.user.username}"
