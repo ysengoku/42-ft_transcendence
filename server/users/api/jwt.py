adb71efa27d05a43fd43d355a5704d4d7c5bb554
emuminov <jseldardw@gmail.com>
2025-02-03 14:16:09 +0100

    feat: added function for creating and verifying refresh token refactor: changed the variable names for jwt secret key

--- server/users/api/jwt.py
+++ server/users/api/jwt.py
@@ -4,15 +4,27 @@ import jwt
 from django.conf import settings
 
 
-def create_jwt(username: str) -> str:
+def create_jwt(username: str, key: str, td: timedelta) -> str:
     now = datetime.now(timezone.utc)
     payload = {
         "sub": username,
         "iat": now,
-        "exp": now + timedelta(minutes=30),
+        "exp": now + td,
     }
-    return jwt.encode(payload, settings.JWT_SECRET_KEY, algorithm="HS256")
+    return jwt.encode(payload, key, algorithm="HS256")
 
 
-def verify_jwt(token: str) -> dict | None:
-    return jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=["HS256"])
+def verify_jwt(token: str, key: str) -> dict | None:
+    return jwt.decode(token, key, algorithms=["HS256"])
+
+def create_access_token(username: str):
+    return create_jwt(username, settings.ACCESS_TOKEN_SECRET_KEY, timedelta(minutes=30))
+
+def verify_access_token(token: str):
+    return verify_jwt(token, settings.ACCESS_TOKEN_SECRET_KEY)
+
+def create_refresh_token(username: str):
+    return create_jwt(username, settings.REFRESH_TOKEN_REFRESH_SECRET_KEY, timedelta(days=182))
+
+def verify_refresh_token(token: str):
+     return verify_jwt(token, settings.REFRESH_TOKEN_REFRESH_SECRET_KEY)
