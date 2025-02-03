788aa73a8b8786053c64a738be688aa9681cae89
Boussard Fanny <boussard.fanny@gmail.com>
2025-02-03 18:32:50 +0100

    enhanced: adapted oauth2 and twofa to endpoints

--- server/users/models/twofa.py
+++ server/users/models/twofa.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class TwoFactorAuth(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    secret = models.CharField(max_length=32) # secret key for the user
+    secret = models.CharField(max_length=32)  # secret key for the user
     is_enabled = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -13,4 +13,4 @@ class TwoFactorAuth(models.Model):
         db_table = "user_2fa"
 
     def __str__(self):
-        return f"2FA for {self.user.slug_id}"
+        return f"2FA for {self.user.username}"
