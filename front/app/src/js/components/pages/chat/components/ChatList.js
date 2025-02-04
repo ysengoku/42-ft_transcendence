823d6c51df00c669e20688bca4371235b161edd2
yusengok <ysengoku.tourisme@gmail.com>
2025-02-04 15:32:11 +0100

    feat: Add chat message header

--- front/app/src/js/components/pages/chat/components/ChatList.js
+++ front/app/src/js/components/pages/chat/components/ChatList.js
@@ -19,7 +19,7 @@ export class ChatList extends HTMLElement {
     const listContainer = this.querySelector('#chat-list');
     listContainer.innerHTML = '';
     this.chatListData.forEach((chat) => {
-      const listItem = document.createElement('chat-list-item');
+      const listItem = document.createElement('chat-list-item-component');
       listItem.setData(chat);
       listContainer.appendChild(listItem);
     });
@@ -30,11 +30,11 @@ export class ChatList extends HTMLElement {
 	  <div class="border-end d-flex flex-column h-100 overflow-auto">
       <h5 class="py-3">Conversations</h5>
       <ul class="list-group flex-grow-1 overflow-auto" id="chat-list">
-          <chat-list-item></chat-list-item>
-          <chat-list-item></chat-list-item>
-          <chat-list-item></chat-list-item>
-          <chat-list-item></chat-list-item>
-          <chat-list-item></chat-list-item>
+          <chat-list-item-component></chat-list-item-component>
+          <chat-list-item-component></chat-list-item-component>
+          <chat-list-item-component></chat-list-item-component>
+          <chat-list-item-component></chat-list-item-component>
+          <chat-list-item-component></chat-list-item-component>
         </ul>
       </div>
 	  `;
