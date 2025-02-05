a61364e19f0d480d1bf2068422cd2a96d1f688ca
yusengok <ysengoku.tourisme@gmail.com>
2025-02-05 13:52:55 +0100

    feat: Add websocket and tested with chat using online echo websocket tool

--- front/app/vite.config.js
+++ front/app/vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
     alias: {
       '@main': path.resolve(__dirname, 'src/main.js'),
       '@router': path.resolve(__dirname, 'src/js/router.js'),
+      '@socket': path.resolve(__dirname, 'src/js/socket.js'),
       '@api': path.resolve(__dirname, 'src/js/api/'),
       '@components': path.resolve(__dirname, 'src/js/components/'),
       '@mock': path.resolve(__dirname, 'src/js/mock/'),
