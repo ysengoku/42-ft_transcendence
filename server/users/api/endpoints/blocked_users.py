57123b3f29fb294407a324c9fb9fe38acb5a0331
emuminov <jseldardw@gmail.com>
2025-02-06 18:21:18 +0100

    refactor: removed the decorator and replaced it with a regular function fix: fixed comparison of the uuid of the user with payloaded id in the refresh token logic

--- server/users/api/endpoints/blocked_users.py
+++ server/users/api/endpoints/blocked_users.py
@@ -17,28 +17,29 @@ blocked_users_router = Router()
     "{username}/blocked_users", response={200: list[ProfileMinimalSchema], frozenset({401, 403, 404}): Message},
 )
 @paginate
-@allow_only_for_self
 def get_blocked_users(request: HttpRequest, username: str):
     """
     Gets blocked users of specific user.
     Paginated by the `limit` and `offset` settings.
     For example, `/users/{username}/blocked_users?limit=10&offset=0` will get 10 blocked users from the very first one.
     """
-    return request.auth.profile.blocked_users.all()
+    user = allow_only_for_self(request, username)
+    return user.profile.blocked_users.all()
 
 
 @blocked_users_router.post(
     "{username}/blocked_users",
     response={201: ProfileMinimalSchema, frozenset({401, 403, 404, 422}): Message},
 )
-@allow_only_for_self
 def add_to_blocked_users(request: HttpRequest, username: str, user_to_add: UsernameSchema):
     """
     Adds user to the blocklist.
     """
+    user = allow_only_for_self(request, username)
+
     blocked_user = get_user_queryset_by_username_or_404(user_to_add.username).first()
 
-    err_msg = request.auth.profile.block_user(blocked_user.profile)
+    err_msg = user.profile.block_user(blocked_user.profile)
     if err_msg:
         raise HttpError(422, err_msg)
     return 201, blocked_user.profile
@@ -48,14 +49,15 @@ def add_to_blocked_users(request: HttpRequest, username: str, user_to_add: Usern
     "{username}/blocked_users/{blocked_user_to_remove}",
     response={204: None, frozenset({401, 403, 404, 422}): Message},
 )
-@allow_only_for_self
 def remove_from_blocked_users(request: HttpRequest, username: str, blocked_user_to_remove: str):
     """
     Deletes user from a blocklist.
     """
+    user = allow_only_for_self(request, username)
+
     blocked_user = get_user_queryset_by_username_or_404(blocked_user_to_remove).first()
 
-    err_msg = request.auth.profile.unblock_user(blocked_user.profile)
+    err_msg = user.profile.unblock_user(blocked_user.profile)
     if err_msg:
         raise HttpError(422, err_msg)
     return 204, None
