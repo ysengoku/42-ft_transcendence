10cea00511617f7305c407414579ed60487cad99
faboussa <boussard.fanny@gmail.com>
2025-02-06 17:19:00 +0100

    feat: user managed in database by oauth connection

--- server/users/api/endpoints/oauth2.py
+++ server/users/api/endpoints/oauth2.py
@@ -5,11 +5,13 @@ from urllib.parse import urlencode
 import requests
 from django.conf import settings
 from django.contrib.auth import login
-from django.http import JsonResponse
+from django.http import HttpRequest, JsonResponse
 from django.shortcuts import redirect
 from django.core.exceptions import ValidationError
 from ninja import Router
-
+from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
+from users.api.endpoints.auth import _create_json_response_with_tokens
+from users.schemas import Message, ProfileMinimalSchema, SignUpSchema
 from users.models import User
 
 oauth2_router = Router()
@@ -53,19 +55,23 @@ def oauth_authorize(request, platform: str):
         return JsonResponse({"error": str(e)}, status=500)
 
 
-# Backend: oauth2_router.py
-
-
-# TODO: refactor in two functions
-@oauth2_router.get("/callback/{platform}", auth=None)
-def oauth_callback(request, platform: str, code: str, state: str):
+def create_user_oauth(user_info: dict, connection_type: str) -> User:
     """
-    this is the callback endpoint that the auth server will redirect to after the user has authenticated.
-    the code and state will be checked and then the access token will be retrieved
+    Creates a new user from OAuth data.
     """
-    print(f"print IN CALLBACK : Generated state: {request.session.get('oauth_state')}")
+    user = User.objects.validate_and_create_user(
+        username=user_info.get("login"),
+        connection_type=connection_type,
+        email=user_info.get("email", ""),  # email might be optional for some platforms
+        oauth_id=user_info.get("id"),
+    )
+    user.save()
+    return user
 
-    # Verify the state parameter for security
+
+@oauth2_router.get("/callback/{platform}", response={200: ProfileMinimalSchema, 401: Message}, auth=None)
+@csrf_exempt
+def oauth_callback(request: HttpRequest, platform: str, code: str, state: str):
     if not state or state != request.session.get("oauth_state"):
         return JsonResponse({"status": "error", "error": "Invalid state parameter"}, status=400)
 
@@ -75,83 +81,47 @@ def oauth_callback(request, platform: str, code: str, state: str):
 
     try:
         config = get_oauth_config(platform)
-
-        # Exchange the authorization code for an access token, all through the backend, nothing is exposed to the frontend
-        token_response = requests.post(  # request to the auth server to get the access token
+        token_response = requests.post(
             config["token_uri"],
             data={
                 "client_id": config["client_id"],
                 "client_secret": config["client_secret"],
                 "code": code,
                 "redirect_uri": config["redirect_uris"][0],
-                "grant_type": "authorization_code",  # this is the type of oauth2 grant
+                "grant_type": "authorization_code",
             },
             headers={"Accept": "application/json"},
         )
 
         token_data = token_response.json()
-
         if "access_token" not in token_data:
             return JsonResponse({"status": "error", "error": "Failed to get access token"}, status=500)
 
-        # Fetch user information using the access token
         user_info_response = requests.get(
             config["user_info_uri"],
             headers={"Authorization": f"Bearer {token_data['access_token']}"},
         )
-
-        # function to create user in the database
-
         user_info = user_info_response.json()
 
         connection_type_map = {
             "42": User.FT,
             "github": User.GITHUB,
         }
+        connection_type = connection_type_map.get(platform)
+        if not connection_type:
+            return JsonResponse({"status": "error", "error": "Invalid platform"}, status=400)
 
-        # TODO: create a function to create user in the database
+        # Check if user already exists
+        user = User.objects.filter(oauth_id=user_info["id"]).first()
+        if user:
+            print(f"print : USER EXISTS: {user.username}, {connection_type}, {user_info['id']}")
+        else:
+            user = create_user_oauth(user_info, connection_type)
+            print(f"print : USER CREATED: {user.username}, {connection_type}, {user_info['id']}")
 
-        # connection_type = connection_type_map.get(platform)
-
-        # if not connection_type:
-        #     return JsonResponse({"status": "error", "error": "Invalid platform"}, status=400)
-
-        # login_name = user_info.get("login")
-        # if not login_name:
-        #     return JsonResponse({"status": "error", "error": "Failed to get user info"}, status=500)
-
-        # user = User.objects.find_by_identifier(login_name, connection_type)
-
-        # if not user:
-        #     try:
-        #         email = user_info.get("email", "")  # email is optional because github api does not return email
-        #         user = User.objects.create_user(
-        #             username=login_name,
-        #             connection_type=connection_type,
-        #             email=email,
-        #         )
-        #     except ValidationError as e:
-        #         return JsonResponse(
-        #             {"status": "error", "error": "Validation error", "details": e.message_dict}, status=400
-        #         )
-        #     except Exception as e:
-        #         return JsonResponse({"status": "error", "error": str(e)}, status=400)
-
-        # # Create session or token for authenticated user
-        # login(
-        #     request, user
-        # )  # this is to authenticate the user, maintain the session and create the token. if use of JWT, this line has to be removed
-
-        return JsonResponse(
-            {"status": "success", "message": "Authentication successful", "user_info": {"username": user_info["login"]}}
-        )
+        # Return JSON response with tokens
+        response = _create_json_response_with_tokens(user, user.profile.to_profile_minimal_schema())
+        return response
 
     except Exception as e:
-        print(f"Error during OAuth callback: {e}")
-        return JsonResponse({"status": "error", "error": "Internal server error"}, status=500)
-
-
-##### END OAuth #####
-
-# TODO: # secure the callback endpoint. check that the callback is coming from the same domain and is the same one that was sent to the auth server
-# TODO: # store the access token in the database in an encrypted form, that way it avoids multiple calls to the api , where there is always a risk of the token being stolen
+        return JsonResponse({"msg": str(e)}, status=500)
