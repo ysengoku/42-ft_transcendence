8560b44eb63d8c93553f5dc9ec70ac1b781d9091
yusengok <ysengoku.tourisme@gmail.com>
2025-02-07 09:04:15 +0100

    fix: Update logout handling to avoid json parsing error for 204 no content, refactor related functions

--- front/app/src/js/auth/authManager.js
+++ front/app/src/js/auth/authManager.js
@@ -33,6 +33,22 @@ const auth = (() => {
       // TODO: Add API request
       return false;
     }
+
+    getCSRFTokenfromCookies() {
+      const name = 'csrftoken';
+      let token = null;
+      if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+          const cookie = cookies[i].trim();
+          if (cookie.startsWith(name)) {
+            token = decodeURIComponent(cookie.substring(name.length + 1));
+            break;
+          }
+        }
+      }
+      return token;
+    }
   }
   return new AuthManager();
 })();
