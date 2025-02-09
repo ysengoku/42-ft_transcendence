89907229f05e6b4a0d1eacda83ded1a18a165c61
ysengoku <ysengoku.tourisme@gmail.com>
2025-02-09 17:03:52 +0100

    refactor: Remove 'requiredAuth' param from route, Add clearCSRFToken() func,

--- front/app/src/js/auth/index.js
+++ front/app/src/js/auth/index.js
@@ -1,6 +1,12 @@
 import { auth } from './authManager.js';
 import { handleLogout } from './handleLogout.js';
-import { getCSRFTokenfromCookies } from './csrfToken.js';
+import { getCSRFTokenfromCookies, clearCSRFToken } from './csrfToken.js';
 import { refreshAccessToken } from './refreshToken.js';
 
-export { auth, handleLogout, getCSRFTokenfromCookies, refreshAccessToken };
+export {
+  auth,
+  handleLogout,
+  getCSRFTokenfromCookies,
+  clearCSRFToken,
+  refreshAccessToken,
+};
