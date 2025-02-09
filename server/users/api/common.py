4e39238772f363076ae398dcb6436aed67dd2c0e
emuminov <jseldardw@gmail.com>
2025-02-09 18:40:23 +0100

    refactor: added typing to the common.py

--- server/users/api/common.py
+++ server/users/api/common.py
@@ -1,16 +1,18 @@
+from django.db import models
+from django.http import HttpRequest
 from ninja.errors import HttpError
 
 from users.models import Profile, User
 
 
-def allow_only_for_self(request, username: str):
+def allow_only_for_self(request: HttpRequest, username: str):
     user: User = request.auth
     if user.username != username:
         raise HttpError(403, "Forbidden.")
     return request.auth
 
 
-def get_queryset_by_username_or_404(model, username: str):
+def get_queryset_by_username_or_404(model: models.Model, username: str):
     user = model.objects.for_username(username)
     if not user.exists():
         raise HttpError(404, f"User {username} not found.")
