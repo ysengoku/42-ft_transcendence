b7d4e803f93cbedcc83eafbdf8d5e80c6eb009b2
yusengok <ysengoku.tourisme@gmail.com>
2025-02-10 11:50:13 +0100

    fix: Update alias in jest test

--- front/app/jest.config.js
+++ front/app/jest.config.js
@@ -5,8 +5,10 @@ export default {
     '^src/(.*)$': '<rootDir>/src/$1',
     '@main': '<rootDir>/src/main.js',
     '@router': '<rootDir>/src/js/router.js',
-    '@api': '<rootDir>/src/js/api/',
-    '@components': '<rootDir>/src/js/components/',
-    '@utils': '<rootDir>/src/js/utils/',
+    '@auth': '<rootDir>/src/js/auth/index.js',
+    '@api': '<rootDir>/src/js/api/index.js',
+    '@components/(.*)$': '<rootDir>/src/js/components/$1',
+    '@utils': '<rootDir>/src/js/utils/index.js',
+    '\\.(svg)(\\?url)?$': '<rootDir>/src/js/mock/svgMock.js',
   },
 };
