79092bd93a6691b8f2e6b509b1656f256b127a09
yusengok <ysengoku.tourisme@gmail.com>
2025-02-11 17:22:53 +0100

    feat: Add nickname to Profile page, working on error handling for add/remove friend and blocl/unblock user

--- front/app/src/js/utils/index.js
+++ front/app/src/js/utils/index.js
@@ -1,12 +1,18 @@
-import { ERROR_MESSAGES, showErrorMessage, removeAlert, addDissmissAlertListener } from './errorMessage';
 import { INPUT_FEEDBACK } from './inputFeedback';
 import { ThemeController } from './ThemeController';
+import { ERROR_MESSAGES,
+  showErrorMessage,
+  removeAlert,
+  showErrorMessageForDuration,
+  addDissmissAlertListener,
+} from './errorMessage';
 
 export {
+  INPUT_FEEDBACK,
+  ThemeController,
   ERROR_MESSAGES,
   showErrorMessage,
   removeAlert,
+  showErrorMessageForDuration,
   addDissmissAlertListener,
-  INPUT_FEEDBACK,
-  ThemeController,
 };
