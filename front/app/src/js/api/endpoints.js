12ff8166bfc9ac7035045c7d887a543b389d12df
yusengok <ysengoku.tourisme@gmail.com>
2025-02-11 11:10:02 +0100

    fix: apiRequest return value if status is 204, fix typo in endpoint,

--- front/app/src/js/api/endpoints.js
+++ front/app/src/js/api/endpoints.js
@@ -8,7 +8,7 @@ export const API_ENDPOINTS = {
   SELF: `${API_BASE_URL}self`,
   USER_PROFILE: (username) => `${API_BASE_URL}users/${username}`,
   USER_SETTINGS: (username) => `${API_BASE_URL}users/${username}/settings`,
-  USER_FRIENDS: (username) => `${API_BASE_URL}users/${username}friends`,
+  USER_FRIENDS: (username) => `${API_BASE_URL}users/${username}/friends`,
   USER_REMOVE_FRIEND: (username, friend) => `${API_BASE_URL}users/${username}/friends/${friend}`,
   USER_BLOCKED_USERS: (username) => `${API_BASE_URL}users/${username}/blocked_users`,
   USER_UNBLOCK_USER: (username, blockedUser) => `${API_BASE_URL}users/${username}/blocked_users/${blockedUser}`,
