b2465b2578505b874dd90ce7d07f599508f28f13
yusengok <ysengoku.tourisme@gmail.com>
2025-02-13 10:13:10 +0100

    feat: Add components to Home (temporary style), style: Replace h1 to legend on Login and Register

---
+++ front/app/src/js/components/pages/home/components/index.js
@@ -0,0 +1,13 @@
+import { ProfileButton } from './ProfileButton';
+import { SettingsButton } from './SettingsButton';
+import { DualButton } from './DualButton';
+import { TournamentButton } from './TournamentButton';
+import { LogoutButton } from './LogoutButton';
+
+export {
+  ProfileButton,
+  SettingsButton,
+  DualButton,
+  TournamentButton,
+  LogoutButton,
+};
