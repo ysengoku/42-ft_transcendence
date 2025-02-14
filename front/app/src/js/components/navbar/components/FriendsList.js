6aee0d2a81409cd28e36d1bee335ee0999312b2c
ysengoku <ysengoku.tourisme@gmail.com>
2025-02-14 22:50:47 +0100

    enhanced: Transform Friends list from modal to  dropdown

---
+++ front/app/src/js/components/navbar/components/FriendsList.js
@@ -0,0 +1,142 @@
+import { auth } from '@auth';
+import { apiRequest, API_ENDPOINTS } from '@api';
+
+export class FriendsList extends HTMLElement {
+  constructor() {
+    super();
+    this.modal = null;
+    this.friendsList = [];
+    this.username = '';
+    this.totalFriendsCount = 0;
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  async fetchFriendsData() {
+    // this.friendsList = await simulateFetchFriendsList();
+    this.username = auth.getStoredUser().username;
+    const listLength = this.friendsList.length;
+    const response = await apiRequest(
+        'GET',
+        /* eslint-disable-next-line new-cap */
+        API_ENDPOINTS.USER_FRIENDS_LIST(this.username, 10, listLength),
+        null, false, true);
+    if (response.success) {
+      if (response.data) {
+        this.totalFriendsCount = response.data.count;
+        this.friendsList.push(...response.data.items);
+      }
+      this.renderFriendsList();
+    } else {
+      this.hideModal();
+      if (response.status === 401) {
+        showErrorMessageForDuration(ERROR_MESSAGES.SESSION_EXPIRED, 5000);
+        router.navigate('/login');
+      } else {
+        showErrorMessageForDuration(ERROR_MESSAGES.UNKNOWN_ERROR, 5000);
+        router.navigate('/');
+      }
+    }
+  }
+
+  renderNoFriendsFound(listContainer) {
+    const noFriends = document.createElement('li');
+    noFriends.innerHTML = `
+      <style>
+        .list-group-item {
+          border: none;
+          position: relative;
+        }
+        li {
+          list-style-type: none;
+        }
+      </style>
+      <div class="list-group-item p-3">
+        <p class="text-center m-0">No friends found</p>
+      </div>
+    `;
+    listContainer.appendChild(noFriends);
+  }
+
+  renderShowMoreButton(listContainer) {
+    const showMoreButton = document.createElement('li');
+    showMoreButton.innerHTML = `
+      <style>
+        #show-more-friends {
+          border: none;
+          position: relative;
+          border-top: 1px solid var(--bs-border-color);
+        }
+        li {
+          list-style-type: none;
+        }
+      </style>
+      <div class="list-group-item mt-4 p-3" id="show-more-friends">
+        <p class="text-center m-0">Show more friends</p>
+      </div>
+    `;
+    listContainer.appendChild(showMoreButton);
+
+    const button = showMoreButton.querySelector('#show-more-friends');
+    if (button) {
+      button.addEventListener('click', (event) => {
+        event.stopPropagation();
+        this.fetchFriendsData();
+      });
+    }
+  }
+
+  renderFriendsList() {
+    const listContainer = this.querySelector('#friends-list');
+    listContainer.innerHTML = '';
+    if (this.friendsList.length === 0) {
+      this.renderNoFriendsFound(listContainer);
+      return;
+    }
+    this.friendsList.forEach((friend) => {
+      // console.log(`Rendering friend:`, friend);
+      const listItem = document.createElement('friends-list-item');
+      listItem.setAttribute('username', friend.username);
+      listItem.setAttribute('nickname', friend.nickname);
+      listItem.setAttribute('avatar', friend.avatar);
+      listItem.setAttribute('online', friend.is_online);
+      listContainer.appendChild(listItem);
+    });
+    if (this.friendsList.length < this.totalFriendsCount) {
+      this.renderShowMoreButton(listContainer);
+    }
+  }
+
+  render() {
+    this.innerHTML = `
+      <style>
+        #friends-list-header {
+          border-bottom: 1px solid var(--bs-border-color);
+		  position: sticky;
+		  top: 0;
+		  background-color: var(--bs-body-bg);
+		  z-index: 1;
+        }
+      </style>
+      <div class="ps-3 pe-4">
+        <h6 class="pt-2 pb-4" id="friends-list-header" sticky>Friends</h6>
+        <ul class="list-group mb-2" id="friends-list"></ul>
+      </div>
+    `;
+
+    const button = document.getElementById('navbarFriendsButton');
+    button.addEventListener('shown.bs.dropdown', async () => {
+      this.fetchFriendsData();
+    });
+    button.addEventListener('hidden.bs.dropdown', () => {
+      this.friendsList = [];
+      this.totalFriendsCount = 0;
+      const listContainer = this.querySelector('#friends-list');
+      listContainer.innerHTML = '';
+    });
+  }
+}
+
+customElements.define('friends-list', FriendsList);
---
+++ front/app/src/js/components/navbar/components/FriendsListItem.js
@@ -0,0 +1,94 @@
+import { router } from '@router';
+
+export class FriendsListItem extends HTMLElement {
+  constructor() {
+    super();
+    this.username = '';
+    this.nickname = '';
+    this.avatar = '';
+    this.online = false;
+  }
+
+  connectedCallback() {
+    this.setAttributes();
+    this.render();
+    this.addEventListener('click', () => {
+      router.navigate(`/profile/${this.username}`);
+    });
+  }
+
+  setAttributes() {
+    this.username = this.getAttribute('username');
+    this.nickname = this.getAttribute('nickname');
+    this.avatar = this.getAttribute('avatar');
+    this.online = this.getAttribute('online') === 'true';
+  }
+
+  static get observedAttributes() {
+    return ['online'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'online') {
+      this.online = newValue === 'true';
+      this.updateOnlineStatus();
+    }
+  }
+
+  updateOnlineStatus() {
+  }
+
+  render() {
+    this.innerHTML = `
+    <style>
+      .friends-list-item {
+        border: none;
+        padding: 16px 32px;
+        border-bottom: 1px solid var(--bs-border-color);
+        position: relative;
+      }
+      friends-list-item:last-of-type .friends-list-item {
+        border-bottom: none;
+        padding-bottom: 8px;
+      }
+      .avatar-container {
+        position: relative;
+        display: inline-block;
+        margin-right: 10px;
+      }
+      .friends-list-avatar {
+        width: 56px;
+        height: 56px;
+        object-fit: cover;
+      }
+      .friends-list-status-indicator {
+        position: absolute;
+        width: 16px;
+        height: 16px;
+        bottom: 0;
+        right: 16%;
+        border-radius: 50%;
+        background-color: gray;
+        border: 2px solid var(--bs-body-bg);
+      }
+      .friends-list-status-indicator.online {
+        background-color: green;
+      }
+    </style>
+		<li class="list-group-item friends-list-item">
+      <div class="d-flex flex-row align-items-center">
+			  <div class="avatar-container">
+				  <img src="${this.avatar}" alt="Avatar" class="rounded-circle me-3 friends-list-avatar">
+				  <span class="friends-list-status-indicator ${this.online ? 'online' : ''} ms-3"></span>
+			  </div>
+        <div class="d-flex flex-column justify-content-center">
+          <p class="m-0 fs-5">${this.nickname}</P>
+			    <p class="m-0 fw-light">@${this.username}</p>
+        </div>
+      <div>
+		</li>
+	`;
+  }
+}
+
+customElements.define('friends-list-item', FriendsListItem);
