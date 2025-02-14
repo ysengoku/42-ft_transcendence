efcdfe34d7a4decc93512204eccddcdf2fa9118d
faboussa <boussard.fanny@gmail.com>
2025-02-14 15:21:40 +0100

    refactored: added docstrings and error management feat: picture is cartonized

--- front/app/src/js/api/endpoints.js
+++ front/app/src/js/api/endpoints.js
@@ -12,10 +12,7 @@ export const API_ENDPOINTS = {
   USER_REMOVE_FRIEND: (username, friend) => `${API_BASE_URL}users/${username}/friends/${friend}`,
   USER_BLOCKED_USERS: (username) => `${API_BASE_URL}users/${username}/blocked_users`,
   USER_UNBLOCK_USER: (username, blockedUser) => `${API_BASE_URL}users/${username}/blocked_users/${blockedUser}`,
-  // LOGOUT: ,
   OAUTH_AUTHORIZE: (platform) => `${API_BASE_URL}oauth/authorize/${platform}`,
-  OAUTH_CALLBACK: (platform) => `${API_BASE_URL}oauth/callback/${platform}`,
   MFA_SETUP: `${API_BASE_URL}2fa/setup`,
   MFA_VERIFY: `${API_BASE_URL}2fa/verify`,
-  MFA_SEND: `${API_BASE_URL}2fa/send`,
 };
