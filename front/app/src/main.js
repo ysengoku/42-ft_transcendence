9774bcedb1fdea06c8f97c15fa32535fc09df636
ysengoku <ysengoku.tourisme@gmail.com>
2025-02-14 22:50:47 +0100

    enhanced: Transform Friends list from modal to  dropdown

--- front/app/src/main.js
+++ front/app/src/main.js
@@ -1,6 +1,6 @@
 import '@components/navbar/index.js';
 import '@components/pages/index.js';
-import '@components/modals/index.js';
+// import '@components/modals/index.js';
 import '@css/style.css';
 
 const savedTheme = localStorage.getItem('theme') || 'light';
