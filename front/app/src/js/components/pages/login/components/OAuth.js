94db47a43d9385e24f668b6ca65c8b36db47ddd4
faboussa <boussard.fanny@gmail.com>
2025-02-14 15:36:10 +0100

    enhanced: hover for 42 button

--- front/app/src/js/components/pages/login/components/OAuth.js
+++ front/app/src/js/components/pages/login/components/OAuth.js
@@ -64,7 +64,7 @@ export class OAuth extends HTMLElement {
                     font-size: 1rem;
                 }
                 .btn-42:hover {
-                    background-color: #f8f9fa;
+                    background-color:rgb(160, 165, 171);
                     color: #000000;
                 }
                 .me-2 {
