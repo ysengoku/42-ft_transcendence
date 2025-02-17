789befae4ded22c0614dd6bd18aefb1d3e04cf54
emuminov <jseldardw@gmail.com>
2025-02-17 19:27:27 +0100

    feat: configured the server for the asgi and websockets, added new chat app, added models for the chat docs: updated TODO.md

--- server/server/asgi.py
+++ server/server/asgi.py
@@ -9,22 +9,18 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
-
 django_asgi_app = get_asgi_application()
+
+from chat.routing import websocket_urlpatterns  # noqa: E402
 
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-
-        # "websocket": AuthMiddlewareStack(
-        #     URLRouter(
-        #         # Add routing for WebSockets here
-        #     )
-        # ),
+        "websocket": AllowedHostsOriginValidator(URLRouter(websocket_urlpatterns)),
     },
 )
