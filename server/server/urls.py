3eadf7f27f49dc2efe1b69dbeef6268c38361ace
emuminov <jseldardw@gmail.com>
2025-02-17 19:27:27 +0100

    feat: configured the server for the asgi and websockets, added new chat app, added models for the chat docs: updated TODO.md

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,14 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
 from django.urls import include, path
 
 from users.api import api
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("api/", api.urls),
     path("silk/", include("silk.urls", namespace="silk")),
+    path("chat/", include("chat.urls")),
 ]
 
 if settings.DEBUG:
