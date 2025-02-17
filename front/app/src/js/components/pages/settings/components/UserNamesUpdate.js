4e15a689aca78c3557134d99140fc93e778ddbe5
ysengoku <ysengoku.tourisme@gmail.com>
2025-02-17 22:47:14 +0100

    minor: Update input feedback

--- front/app/src/js/components/pages/settings/components/UserNamesUpdate.js
+++ front/app/src/js/components/pages/settings/components/UserNamesUpdate.js
@@ -17,27 +17,16 @@ export class UserNamesUpdate extends HTMLElement {
     this._user.username = user.username;
     this._user.nickname = user.nickname;
     this.render();
+    this.setEventListeners();
   }
 
-  render() {
-    this.innerHTML = `
-	  <div class="mt-3">
-		<label for="settings-username" class="form-label">Username</label>
-		<input type="text" class="form-control" id="settings-username" value="${this._user.username}" autocomplete="off">
-		<div class="invalid-feedback" id="settings-username-feedback"></div>
-	  </div>
-	  <div class="mt-3">
-		<label for="settings-nickname" class="form-label">Nickname</label>
-		<input type="text" class="form-control" id="settings-nickname" value="${this._user.nickname}" autocomplete="off">
-		<div class="invalid-feedback" id="settings-nickname-feedback"></div>
-	  </div>
-	 `;
-
+  setEventListeners() {
     const usernameInput = this.querySelector('#settings-username');
+
     usernameInput.addEventListener('input', (event) => {
       const feedback = this.querySelector('#settings-username-feedback');
       if (event.target.value.length < 1) {
-        feedback.textContent = INPUT_FEEDBACK.EMPTY_USERNAME;
+        feedback.textContent = INPUT_FEEDBACK.CANNOT_DELETE_USERNAME;
         usernameInput.classList.add('is-invalid');
       } else {
         feedback.textContent = '';
@@ -48,11 +37,20 @@ export class UserNamesUpdate extends HTMLElement {
       }
     });
 
+    usernameInput.addEventListener('blur', (event) => {
+      if (event.target.value.length < 1) {
+        event.target.value = this._user.username;
+        usernameInput.classList.remove('is-invalid');
+        this.querySelector('#settings-username-feedback').textContent = '';
+      }
+    });
+
     const nicknameInput = this.querySelector('#settings-nickname');
+
     nicknameInput.addEventListener('input', (event) => {
       const feedback = this.querySelector('#settings-nickname-feedback');
       if (event.target.value.length < 1) {
-        feedback.textContent = INPUT_FEEDBACK.EMPTY_NICKNAME;
+        feedback.textContent = INPUT_FEEDBACK.CANNOT_DELETE_NICKNAME;
         nicknameInput.classList.add('is-invalid');
       } else {
         feedback.textContent = '';
@@ -62,6 +60,29 @@ export class UserNamesUpdate extends HTMLElement {
         this.newUserInfo.nickname = event.target.value;
       }
     });
+
+    nicknameInput.addEventListener('blur', (event) => {
+      if (event.target.value.length < 1) {
+        event.target.value = this._user.nickname;
+        nicknameInput.classList.remove('is-invalid');
+        this.querySelector('#settings-nickname-feedback').textContent = '';
+      }
+    });
+  }
+
+  render() {
+    this.innerHTML = `
+	  <div class="mt-3">
+		  <label for="settings-username" class="form-label">Username</label>
+		  <input type="text" class="form-control" id="settings-username" value="${this._user.username}" autocomplete="off">
+		  <div class="invalid-feedback" id="settings-username-feedback"></div>
+	  </div>
+	  <div class="mt-3">
+		  <label for="settings-nickname" class="form-label">Nickname</label>
+		  <input type="text" class="form-control" id="settings-nickname" value="${this._user.nickname}" autocomplete="off">
+		  <div class="invalid-feedback" id="settings-nickname-feedback"></div>
+	  </div>
+	 `;
   }
 }
 
