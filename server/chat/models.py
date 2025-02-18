55df56fa58d46c85b1301fc1fa0f0041e4cd3a82
emuminov <jseldardw@gmail.com>
2025-02-18 17:45:44 +0100

    refactor: changed the chat models feat: added ordering of the chats by the last message and filtering of chats by specific participants

--- server/chat/models.py
+++ server/chat/models.py
@@ -1,29 +1,67 @@
 import uuid
 
 from django.db import models
+from django.db.models import Max, OuterRef, Q, Subquery
 
 from users.models import Profile
 
 
-class Dialogue(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user1 = models.ForeignKey(Profile, related_name="dialogues_as_user1", on_delete=models.CASCADE)
-    user2 = models.ForeignKey(Profile, related_name="dialogues_as_user2", on_delete=models.CASCADE)
+class ChatQuerySet(models.QuerySet):
+    def for_participants(self, *participants: Profile):
+        """
+        Returns all the chats where at least one of the specified users participate.
+        """
+        return self.filter(participants__in=participants).distinct()
 
-    class Meta:
-        unique_together = ("user1", "user2")
+    def for_exact_participants(self, *participants: Profile):
+        """
+        Returns all the chats with specified users as participants.
+        """
+        qs = self.all()
+        for participant in participants:
+            qs = qs.filter(participants=participant)
+        return qs.distinct()
+
+    def order_by_last_message(self):
+        """
+        Attaches last message date and content to Chats and sorts them by the latest message date.
+        Messages are sorted by the date by default, so there is no need to call 'order_by'.
+        """
+        latest_message_subquery = Message.objects.filter(
+            chat=OuterRef("pk"),
+        ).values("content")[:1]
+
+        return self.annotate(
+            last_message=Subquery(latest_message_subquery),
+            last_message_date=Max("messages__date"),
+        ).order_by("-last_message_date")
+
+
+class Chat(models.Model):
+    """
+    Represents the chat between unspecified amount of users.
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    participants = models.ManyToManyField(Profile, related_name="dialogues")
+
+    objects = ChatQuerySet.as_manager()
 
     def __str__(self):
-        return ""
+        max_participants_to_display = 20
+        participants_list = [p.user.username for p in self.participants.all()[:max_participants_to_display]]
+        res = ", ".join(participants_list)
+        if self.participants.count() > max_participants_to_display:
+            res + " ..."
+        return res
 
 
-class PrivateMessage(models.Model):
+class Message(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     content = models.CharField(max_length=256)
     date = models.DateTimeField(auto_now_add=True)
     sender = models.ForeignKey(Profile, related_name="sent_messages", on_delete=models.CASCADE)
-    receiver = models.ForeignKey(Profile, related_name="received_messages", on_delete=models.CASCADE)
-    dialogue = models.ForeignKey(Dialogue, related_name="messages", on_delete=models.CASCADE)
+    chat = models.ForeignKey(Chat, related_name="messages", on_delete=models.CASCADE)
     is_read = models.BooleanField(default=False)
     is_liked = models.BooleanField(default=False)
 
@@ -33,6 +71,6 @@ class PrivateMessage(models.Model):
     def __str__(self):
         max_msg_len = 15
         return (
-            f"{self.date} {self.sender.user.username} to {self.receiver.user.username}: "
+            f"{self.date} {self.sender.user.username}: "
             f"{self.content[:max_msg_len] + ' ...' if len(self.content) > max_msg_len else self.content}"
         )
