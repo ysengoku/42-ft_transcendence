ab0708ac1f0f901538eae6049df2769af049941c
faboussa <boussard.fanny@gmail.com>
2025-02-21 19:13:13 +0100

    feat: 2fa with email from gmail application is working. Endpoints send code and very code created and tested. gmail new account with app password created. Documentation created.

--- server/users/management/commands/populate_db.py
+++ server/users/management/commands/populate_db.py
@@ -58,85 +58,86 @@ class Command(BaseCommand):
                 user.add_friend(friend)
             user.save()
 
-        Match.objects.resolve(celia, yuko, 2, 1)
-        Match.objects.resolve(celia, yuko, 3, 1)
-        Match.objects.resolve(celia, yuko, 4, 2)
-        Match.objects.resolve(celia, yuko, 5, 2)
-        Match.objects.resolve(celia, yuko, 1, 0)
-        Match.objects.resolve(celia, yuko, 2, 1)
+        # Match.objects.resolve(celia, yuko, 2, 1)
+        # Match.objects.resolve(celia, yuko, 3, 1)
+        # Match.objects.resolve(celia, yuko, 4, 2)
+        # Match.objects.resolve(celia, yuko, 5, 2)
+        # Match.objects.resolve(celia, yuko, 1, 0)
+        # Match.objects.resolve(celia, yuko, 2, 1)
 
-        Match.objects.resolve(celia, eldar, 2, 1)
-        Match.objects.resolve(celia, eldar, 3, 1)
-        Match.objects.resolve(celia, eldar, 4, 2)
-        Match.objects.resolve(celia, eldar, 2, 1)
-        Match.objects.resolve(eldar, celia, 5, 2)
-        Match.objects.resolve(eldar, celia, 1, 0)
+        # Match.objects.resolve(celia, eldar, 2, 1)
+        # Match.objects.resolve(celia, eldar, 3, 1)
+        # Match.objects.resolve(celia, eldar, 4, 2)
+        # Match.objects.resolve(celia, eldar, 2, 1)
+        # Match.objects.resolve(eldar, celia, 5, 2)
+        # Match.objects.resolve(eldar, celia, 1, 0)
 
-        Match.objects.resolve(celia, fanny, 3, 2)
-        Match.objects.resolve(celia, fanny, 3, 0)
-        Match.objects.resolve(celia, fanny, 5, 1)
-        Match.objects.resolve(celia, fanny, 4, 1)
-        Match.objects.resolve(celia, fanny, 5, 4)
-        Match.objects.resolve(celia, fanny, 2, 1)
+        # Match.objects.resolve(celia, fanny, 3, 2)
+        # Match.objects.resolve(celia, fanny, 3, 0)
+        # Match.objects.resolve(celia, fanny, 5, 1)
+        # Match.objects.resolve(celia, fanny, 4, 1)
+        # Match.objects.resolve(celia, fanny, 5, 4)
+        # Match.objects.resolve(celia, fanny, 2, 1)
 
-        Match.objects.resolve(yuko, fanny, 3, 2)
-        Match.objects.resolve(yuko, fanny, 3, 0)
-        Match.objects.resolve(yuko, fanny, 5, 1)
-        Match.objects.resolve(yuko, fanny, 4, 1)
-        Match.objects.resolve(fanny, yuko, 5, 4)
-        Match.objects.resolve(fanny, yuko, 2, 1)
+        # Match.objects.resolve(yuko, fanny, 3, 2)
+        # Match.objects.resolve(yuko, fanny, 3, 0)
+        # Match.objects.resolve(yuko, fanny, 5, 1)
+        # Match.objects.resolve(yuko, fanny, 4, 1)
+        # Match.objects.resolve(fanny, yuko, 5, 4)
+        # Match.objects.resolve(fanny, yuko, 2, 1)
 
-        Match.objects.resolve(eldar, fanny, 3, 2)
-        Match.objects.resolve(eldar, fanny, 3, 0)
-        Match.objects.resolve(eldar, fanny, 5, 1)
-        Match.objects.resolve(eldar, fanny, 4, 1)
-        Match.objects.resolve(fanny, eldar, 5, 4)
-        Match.objects.resolve(fanny, eldar, 2, 1)
+        # Match.objects.resolve(eldar, fanny, 3, 2)
+        # Match.objects.resolve(eldar, fanny, 3, 0)
+        # Match.objects.resolve(eldar, fanny, 5, 1)
+        # Match.objects.resolve(eldar, fanny, 4, 1)
+        # Match.objects.resolve(fanny, eldar, 5, 4)
+        # Match.objects.resolve(fanny, eldar, 2, 1)
 
-        Match.objects.resolve(eldar, yuko, 3, 2)
-        Match.objects.resolve(eldar, yuko, 3, 0)
-        Match.objects.resolve(eldar, yuko, 5, 1)
-        Match.objects.resolve(eldar, yuko, 4, 1)
-        Match.objects.resolve(yuko, eldar, 5, 4)
-        Match.objects.resolve(yuko, eldar, 2, 1)
+        # Match.objects.resolve(eldar, yuko, 3, 2)
+        # Match.objects.resolve(eldar, yuko, 3, 0)
+        # Match.objects.resolve(eldar, yuko, 5, 1)
+        # Match.objects.resolve(eldar, yuko, 4, 1)
+        # Match.objects.resolve(yuko, eldar, 5, 4)
+        # Match.objects.resolve(yuko, eldar, 2, 1)
 
-        for _i in range(10):
-            Match.objects.resolve(yuko, sad_hampter, 5, 1)
-            Match.objects.resolve(eldar, sad_hampter, 6, 1)
-            Match.objects.resolve(celia, sad_hampter, 11, 1)
-            Match.objects.resolve(fanny, sad_hampter, 5, 1)
+        # for _i in range(10):
+        #     Match.objects.resolve(yuko, sad_hampter, 5, 1)
+        #     Match.objects.resolve(eldar, sad_hampter, 6, 1)
+        #     Match.objects.resolve(celia, sad_hampter, 11, 1)
+        #     Match.objects.resolve(fanny, sad_hampter, 5, 1)
 
-        for _i in range(5):
-            for user in regular_users:
-                opponents = regular_users.copy()
-                opponents.remove(user)
-                opponent = choice(opponents)  # noqa: S311
-                players = [user, opponent]
-                winner = choice(players)  # noqa: S311
-                players.remove(winner)
-                loser = players[0]
-                Match.objects.resolve(winner.profile, loser.profile, choice(range(3, 6)), choice(range(3)))  # noqa: S311
-                Match.objects.resolve(life_enjoyer, loser.profile, choice(range(3, 6)), choice(range(3)))  # noqa: S311
-                Match.objects.resolve(life_enjoyer, winner.profile, choice(range(3, 6)), choice(range(3)))  # noqa: S311
-                if randint(0, 10) > 6:  # noqa: S311,PLR2004
-                    Match.objects.resolve(winner.profile, life_enjoyer, choice(range(3, 6)), choice(range(3)))  # noqa: S311
-                if randint(0, 10) > 7:  # noqa: S311,PLR2004
-                    Match.objects.resolve(loser.profile, life_enjoyer, choice(range(3, 6)), choice(range(3)))  # noqa: S311
-        
+        # for _i in range(5):
+        #     for user in regular_users:
+        #         opponents = regular_users.copy()
+        #         opponents.remove(user)
+        #         opponent = choice(opponents)  # noqa: S311
+        #         players = [user, opponent]
+        #         winner = choice(players)  # noqa: S311
+        #         players.remove(winner)
+        #         loser = players[0]
+        #         Match.objects.resolve(winner.profile, loser.profile, choice(range(3, 6)), choice(range(3)))  # noqa: S311
+        #         Match.objects.resolve(life_enjoyer, loser.profile, choice(range(3, 6)), choice(range(3)))  # noqa: S311
+        #         Match.objects.resolve(life_enjoyer, winner.profile, choice(range(3, 6)), choice(range(3)))  # noqa: S311
+        #         if randint(0, 10) > 6:  # noqa: S311,PLR2004
+        #             Match.objects.resolve(winner.profile, life_enjoyer, choice(range(3, 6)), choice(range(3)))  # noqa: S311
+        #         if randint(0, 10) > 7:  # noqa: S311,PLR2004
+        #             Match.objects.resolve(loser.profile, life_enjoyer, choice(range(3, 6)), choice(range(3)))  # noqa: S311
+
         # Groupe 1: Utilisateurs avec MFA
-        
+
         mfa_users = [
             ("secure_bob", "secure_bob@gmail.com"),
             ("safe_alice", "safe_alice@gmail.com"),
             ("careful_charlie", "careful_charlie@gmail.com"),
             ("prudent_paula", "prudent_paula@gmail.com"),
+            ("fanny", "boussard.fanny@gmail.com"),
         ]
         for username, email in mfa_users:
             user = User.objects.create_user(username, email=email, password="123")
             user.mfa_enabled = True
             user.save()
 
-# 42 OAuth Users
+        # 42 OAuth Users
         ft_users = [
             ("ft_user1", "ft1@student.42.fr"),
             ("ft_user2", "ft2@student.42.fr"),
@@ -145,9 +146,7 @@ class Command(BaseCommand):
         ]
         for i, (username, email) in enumerate(ft_users, start=1):
             oauth_connection = OauthConnection.objects.create(
-                status=OauthConnection.CONNECTED,
-                connection_type=OauthConnection.FT,
-                oauth_id=420000 + i
+                status=OauthConnection.CONNECTED, connection_type=OauthConnection.FT, oauth_id=420000 + i
             )
             User.objects.create_user(
                 username=username,
@@ -163,9 +162,7 @@ class Command(BaseCommand):
         ]
         for i, (username, email) in enumerate(github_users, start=1):
             oauth_connection = OauthConnection.objects.create(
-                status=OauthConnection.CONNECTED,
-                connection_type=OauthConnection.GITHUB,
-                oauth_id=430000 + i
+                status=OauthConnection.CONNECTED, connection_type=OauthConnection.GITHUB, oauth_id=430000 + i
             )
             User.objects.create_user(
                 username=username,
