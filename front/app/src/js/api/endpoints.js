a4c9fe8d201b1c9b3e0fc6ac9d5b503c2a02891c
yusengok <ysengoku.tourisme@gmail.com>
2025-02-24 09:00:05 +0100

    feat: Add mfa_enabled update handler in Settings, Add mfa endpoints

--- front/app/src/js/api/endpoints.js
+++ front/app/src/js/api/endpoints.js
@@ -4,8 +4,8 @@ export const API_ENDPOINTS = {
   SIGNUP: `${API_BASE_URL}signup`,
   LOGIN: `${API_BASE_URL}login`,
   OAUTH_AUTHORIZE: (platform) => `${API_BASE_URL}oauth/authorize/${platform}`,
-  MFA_VERIFICATION: `${API_BASE_URL}mfa/verify-mfa`,
-  MFA_RESEND_CODE: `${API_BASE_URL}mfa/send-code`,
+  MFA_VERIFICATION: (username) => `${API_BASE_URL}mfa/verify-mfa?username=${username}`,
+  MFA_RESEND_CODE: (username) => `${API_BASE_URL}mfa/send-code?username=${username}`,
   REFRESH: `${API_BASE_URL}refresh`,
   LOGOUT: `${API_BASE_URL}logout`,
   SELF: `${API_BASE_URL}self`,
@@ -18,3 +18,20 @@ export const API_ENDPOINTS = {
   USER_SEARCH: (query, limit, offset) => `${API_BASE_URL}users?search=${query}&limit=${limit}&offset=${offset}`,
   USER_FRIENDS_LIST: (username, limit, offset) => `${API_BASE_URL}users/${username}/friends?limit=${limit}&offset=${offset}`,
 };
+
+
+
+// curl -X 'POST' \
+//   'http://localhost:8000/api/mfa/verify-mfa?username=john' \
+//   -H 'accept: application/json' \
+//   -H 'Content-Type: application/json' \
+//   -H 'X-CSRFToken: Aj4mvulfzyamfhiOdUYNxlNDQKlIbjPyFkHFsk7mJul1LyQA9qJ6Bm8Gvec8q87i' \
+//   -d '{
+//   "token": "string"
+// }'
+
+// curl -X 'POST' \
+//   'http://localhost:8000/api/mfa/send-code?username=john' \
+//   -H 'accept: application/json' \
+//   -H 'X-CSRFToken: Aj4mvulfzyamfhiOdUYNxlNDQKlIbjPyFkHFsk7mJul1LyQA9qJ6Bm8Gvec8q87i' \
+//   -d ''
