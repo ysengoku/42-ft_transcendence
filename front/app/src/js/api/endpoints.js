75258ce23b78c52e5c8f4ee3591cda4a277c5547
yusengok <ysengoku.tourisme@gmail.com>
2025-02-25 12:46:06 +0100

    fix: Patched from dev

--- front/app/src/js/api/endpoints.js
+++ front/app/src/js/api/endpoints.js
@@ -3,6 +3,10 @@ const API_BASE_URL = 'https://localhost:1026/api/'; // For Test environment
 export const API_ENDPOINTS = {
   SIGNUP: `${API_BASE_URL}signup`,
   LOGIN: `${API_BASE_URL}login`,
+  OAUTH_AUTHORIZE: (platform) => `${API_BASE_URL}oauth/authorize/${platform}`,
+  OAUTH_CALLBACK: (platform) => `${API_BASE_URL}oauth/callback/${platform}`,
+  MFA_VERIFICATION: (username) => `${API_BASE_URL}mfa/verify-mfa?username=${username}`,
+  MFA_RESEND_CODE: (username) => `${API_BASE_URL}mfa/send-code?username=${username}`,
   REFRESH: `${API_BASE_URL}refresh`,
   LOGOUT: `${API_BASE_URL}logout`,
   SELF: `${API_BASE_URL}self`,
@@ -12,12 +16,6 @@ export const API_ENDPOINTS = {
   USER_REMOVE_FRIEND: (username, friend) => `${API_BASE_URL}users/${username}/friends/${friend}`,
   USER_BLOCKED_USERS: (username) => `${API_BASE_URL}users/${username}/blocked_users`,
   USER_UNBLOCK_USER: (username, blockedUser) => `${API_BASE_URL}users/${username}/blocked_users/${blockedUser}`,
-  OAUTH_AUTHORIZE: (platform) => `${API_BASE_URL}oauth/authorize/${platform}`,
-  OAUTH_CALLBACK: (platform) => `${API_BASE_URL}oauth/callback/${platform}`,
-  MFA_VERIFY_LOGIN: `${API_BASE_URL}mfa/verify_login`,
-  MFA_SEND_CODE: `${API_BASE_URL}mfa/send_code`,
-  MFA_SETUP: `${API_BASE_URL}2fa/setup`,
-  MFA_VERIFY: `${API_BASE_URL}2fa/verify`,
   USER_SEARCH: (query, limit, offset) => `${API_BASE_URL}users?search=${query}&limit=${limit}&offset=${offset}`,
   USER_FRIENDS_LIST: (username, limit, offset) => `${API_BASE_URL}users/${username}/friends?limit=${limit}&offset=${offset}`,
 };
