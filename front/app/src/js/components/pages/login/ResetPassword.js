14926b0385ee6c5911d474cdbfffa524dd975006
yusengok <ysengoku.tourisme@gmail.com>
2025-02-26 11:17:15 +0100

    enchance: Add event listeners to remove feedback, Refactor Register component and input feedback functions

--- front/app/src/js/components/pages/login/ResetPassword.js
+++ front/app/src/js/components/pages/login/ResetPassword.js
@@ -2,85 +2,116 @@ import { router } from '@router';
 import { auth } from '@auth';
 import { apiRequest, API_ENDPOINTS } from '@api';
 import { passwordFeedback } from '@utils';
+import { showAlertMessageForDuration, ALERT_TYPE } from '@utils';
 
 export class ResetPassword extends HTMLElement {
+  #state = {
+    token: '',
+    newPassword: '',
+    newPasswordRepeat: '',
+  };
+
   constructor() {
     super();
-    this.token = '';
-    this.password = '';
-    this.passwordRepeat = '';
   }
 
   async setParam(param) {
-    this.token = param.token;
-	const authStatus = await auth.fetchAuthStatus();
+    this.#state.token = param.token;
+    const authStatus = await auth.fetchAuthStatus();
     if (authStatus.success) {
       router.navigate('/home');
-	  return;
+    } else {
+      this.render();
     }
-    this.render();
-	this.setEventListener();
-  }
-
-  setEventListener() {
-	const submitButton = this.querySelector('#reset-password-submit');
-	submitButton.addEventListener('click', async (event) => {
-	  event.preventDefault();
-	  await this.handleResetPassword();
-	});
-  }
-
-  async handleResetPassword() {
-	const passwordField = this.querySelector('#reset-password');
-	const passwordRepeatField = this.querySelector('#reset-password-repeat');
-	if (!passwordFeedback(passwordField, passwordRepeatField, '#reset-password-feedback', '#reset-password-repeat-feedback')) {
-		return;
-	}
-	this.password = passwordField.value;
-	this.passwordRepeat = passwordRepeatField.value;
-	const response = await apiRequest(
-		'POST',
-	    API_ENDPOINTS.RESET_PASSWORD(this.token),
-		{ password: this.password, password_repeat: this.passwordRepeat },
-		false,
-		true,
-	);
-	if (response.success) {
-	  router.navigate('/login');
-	} else {
-	  const feedback = this.querySelector('#reset-password-failed-feedback');
-	  feedback.innerHTML = `
-	    <div class="alert alert-danger" role="alert">
-	      ${response.message}
-	    </div>
-	  `;
-	}
   }
 
   render() {
-	this.innerHTML = `
-	  <div class="container my-3">
-        <div class="row justify-content-center py-4">
-          <div class="col-12 col-md-4">
-            <div id="reset-password-failed-feedback"></div>
-      		  <form class="w-100">
-                <legend class="my-4 border-bottom">Reset password</legend>
-                <div class="mt-5 mb-3">
-                  <label for="reset-password" class="form-label">New password</label>
-                  <input type="password" class="form-control" id="reset-password" placeholder="new password" autocomplete="off">
-				  <div class="invalid-feedback" id="reset-password-feedback"></div>
-                  <label for="reset-password-repeat" class="form-label mt-3">Confirm new password</label>
-                  <input type="password" class='form-control' id='reset-password-repeat' placeholder="confirm new password" autocomplete="off">
-                  <div class="invalid-feedback" id="reset-password-repeat-feedback"></div>
-                </div>
-                <button class="btn btn-primary w-100 mt-3" type="submit" id="reset-password-submit">
-                  Reset password
-                </button>
-            </form>
-          </div>
-      </div>
-	`;
+    this.innerHTML = this.template() + this.style();
+
+    this.passwordField = this.querySelector('#reset-password');
+    this.passwordRepeatField = this.querySelector('#reset-password-repeat');
+    this.passwordFeedback = this.querySelector('#reset-password-feedback');
+    this.passwordRepeatFeedback = this.querySelector('#reset-password-repeat-feedback');
+    const submitButton = this.querySelector('#reset-password-submit');
+
+    submitButton.addEventListener('click', async (event) => {
+      event.preventDefault();
+      await this.handleResetPassword();
+    });
+    this.passwordField.addEventListener('input', this.removeInputFeedback);
+    this.passwordRepeatField.addEventListener('input', this.removeInputFeedback);
   }
+
+  disconnectedCallback() {
+    this.passwordField.removeEventListener('input', this.removeInputFeedback);
+    this.passwordRepeatField.removeEventListener('input', this.removeInputFeedback);
+  }
+
+  async handleResetPassword() {
+    if (!passwordFeedback(this.passwordField, this.passwordRepeatField,
+        this.passwordFeedback, this.passwordRepeatFeedback)) {
+      return;
+    }
+    this.#state.newPassword = this.passwordField.value;
+    this.#state.newPasswordRepeat = this.passwordRepeatField.value;
+    const response = await apiRequest(
+        'POST',
+        /* eslint-disable-next-line new-cap */
+        API_ENDPOINTS.RESET_PASSWORD(this.#state.token),
+        { password: this.#state.newPassword, password_repeat: this.#state.newPasswordRepeat },
+        false,
+        true,
+    );
+    if (response.success) {
+      const successMessage = 'Password reset successful. You can now login with your new password.';
+      showAlertMessageForDuration(ALERT_TYPE.SUCCESS, successMessage, 3000);
+      router.navigate('/login');
+    } else {
+      const errorMessage = response.message || 'Password reset failed. Please try again.';
+      showAlertMessageForDuration(ALERT_TYPE.ERROR, errorMessage, 3000);
+    }
+  }
+
+  removeInputFeedback(event, feedbackField) {
+    event.target.classList.remove('is-invalid');
+    feedbackField.innerHTML = '';
+  }
+
+  template() {
+    return `
+    <div class="container my-4">
+      <div class="row justify-content-center py-4">
+        <div class="form-container col-12 col-md-4 p-4">
+          <form class="w-100">
+            <legend class="my-4 border-bottom">Reset password</legend>
+            <div class="mt-5 mb-3">
+              <label for="reset-password" class="form-label">New password</label>
+              <input type="password" class="form-control" id="reset-password" placeholder="new password" autocomplete="off">
+              <div class="invalid-feedback" id="reset-password-feedback"></div>
+
+              <label for="reset-password-repeat" class="form-label mt-4">Confirm new password</label>
+              <input type="password" class='form-control' id='reset-password-repeat' placeholder="confirm new password" autocomplete="off">
+              <div class="invalid-feedback" id="reset-password-repeat-feedback"></div>
+            </div>
+            <button class="btn btn-primary w-100 mt-4" type="submit" id="reset-password-submit">
+              Reset password
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+    `;
+  }
+
+//   style() {
+//     return `
+// 	<style>
+// 	  .form-container {
+// 		background-color: rgba(128,128,128, 0.1);
+// 	  }
+// 	</style>
+// 	`;
+//   }
 }
 
 customElements.define('reset-password', ResetPassword);
