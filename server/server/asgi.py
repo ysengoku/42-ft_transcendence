94905fcd711586ced6eb829e4beb42d57e357711
emuminov <jseldardw@gmail.com>
2025-02-28 00:36:21 +0100

    nginx: added websocket handling to the conf feat: established ws connection from the game temp: removed the content of OnlineStatusManager for now

--- server/server/asgi.py
+++ server/server/asgi.py
@@ -13,22 +13,18 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from users.middleware import JWTAuthMiddleware
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 django_asgi_app = get_asgi_application()
 
 from chat.routing import websocket_urlpatterns as chat_websocket_urlpatterns
-from pong.routing import websocket_urlpatterns as users_websocket_urlpatterns
-from users.routing import websocket_urlpatterns as pong_websocket_urlpatterns
+from pong.routing import websocket_urlpatterns as pong_websocket_urlpatterns
+from users.routing import websocket_urlpatterns as users_websocket_urlpatterns
 
 combined_patterns = chat_websocket_urlpatterns + users_websocket_urlpatterns + pong_websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": JWTAuthMiddleware(
-            AllowedHostsOriginValidator(URLRouter(combined_patterns)),
-        ),
+        "websocket": AllowedHostsOriginValidator(URLRouter(combined_patterns)),
     },
 )
