311a215fb3a6fe21c8c7740cfa756cdcccf59155
emuminov <jseldardw@gmail.com>
2025-02-27 19:17:51 +0100

    refactor: updated the model of the game consumer

--- server/server/asgi.py
+++ server/server/asgi.py
@@ -16,11 +16,12 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 django_asgi_app = get_asgi_application()
 
-from chat.routing import websocket_urlpatterns  # noqa: E402
+import chat.routing as chat  # noqa: E402
+import pong.routing as pong  # noqa: E402
 
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(URLRouter(websocket_urlpatterns)),
+        "websocket": AllowedHostsOriginValidator(URLRouter(chat.websocket_urlpatterns + pong.websocket_urlpatterns)),
     },
 )
