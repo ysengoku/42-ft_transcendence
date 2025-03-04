454f89d150330330c8acfe074cfe48749741d714
Melodie Bernard (from school) <melodie.bernard123@yahoo.fr>
2025-03-04 16:02:26 +0100

    changed inMemory for redis, other changes not tested yet

--- server/server/settings.py
+++ server/server/settings.py
@@ -156,7 +156,11 @@ SOCIALACCOUNT_PROVIDERS = {
 # Configuration Django Channels
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
+        # "BACKEND": "channels.layers.InMemoryChannelLayer",
+          'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
     },
 }
 
