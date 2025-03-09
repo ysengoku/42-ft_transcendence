e42548948fdb042f86bad94722dbc94ca7e3af6d
ysengoku <ysengoku.tourisme@gmail.com>
2025-03-09 23:24:42 +0100

    style: Add test version background update according theme (dark/light)

--- front/app/src/main.js
+++ front/app/src/main.js
@@ -5,6 +5,13 @@ import '@css/style.css';
 const savedTheme = localStorage.getItem('theme') || 'light';
 document.documentElement.setAttribute('data-bs-theme', savedTheme);
 
+const observer = new MutationObserver(() => {
+  document.documentElement.getAttribute('data-bs-theme') === 'light' ? 
+    document.body.style.backgroundImage = `linear-gradient(180deg,rgb(243, 204, 163), #d47a3e,  #d47a3e)` :
+    document.body.style.backgroundImage = `linear-gradient(0deg, #115d89,rgb(13, 66, 97), #080f1c)`;
+});
+observer.observe(document.documentElement, { attributes: true, attributeFilter: ['data-bs-theme'] });
+
 document.addEventListener('mousedown', () => {
   document.body.style.cursor = 'url(\'/img/sample-cursor-active.png\') 8 8, auto';
 });
