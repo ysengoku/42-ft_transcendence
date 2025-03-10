6aecfecc30a6ad0e8f5a49504e599a5e1c6db3a1
yusengok <ysengoku.tourisme@gmail.com>
2025-03-10 11:37:54 +0100

    enhance: Add functions to create background animations

--- front/app/src/main.js
+++ front/app/src/main.js
@@ -5,12 +5,13 @@ import '@css/style.css';
 const savedTheme = localStorage.getItem('theme') || 'light';
 document.documentElement.setAttribute('data-bs-theme', savedTheme);
 
-const observer = new MutationObserver(() => {
-  document.documentElement.getAttribute('data-bs-theme') === 'light' ?
-    document.body.style.backgroundImage = `linear-gradient(180deg,rgb(243, 204, 163), #d47a3e,  #d47a3e)` :
-    document.body.style.backgroundImage = `linear-gradient(0deg, #115d89,rgb(13, 66, 97), #080f1c)`;
-});
-observer.observe(document.documentElement, { attributes: true, attributeFilter: ['data-bs-theme'] });
+// const observer = new MutationObserver(() => {
+//   document.documentElement.getAttribute('data-bs-theme') === 'light' ? (
+//     document.body.style.backgroundImage = `linear-gradient( #f3cca3, #d47a3e, #d47a3e)`
+//   ) : (
+//     document.body.style.backgroundImage = `linear-gradient( #080f1c 0%, #0d4261 32%,  #1473ab 100%)`);
+// });
+// observer.observe(document.documentElement, { attributes: true, attributeFilter: ['data-bs-theme'] });
 
 document.addEventListener('mousedown', () => {
   document.body.style.cursor = 'url(\'/img/sample-cursor-active.png\') 8 8, auto';
