9768c53ef4e657702a2c4a778cf92218cee09f47
emuminov <jseldardw@gmail.com>
2025-03-17 18:29:17 +0100

    feat: configured redis for the django-channels

--- server/server/settings.py
+++ server/server/settings.py
@@ -152,14 +152,17 @@ SOCIALACCOUNT_PROVIDERS = {
     },
 }
 
-# Configuration Django Channels
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
-    },
-}
 REDIS_HOST = os.environ.get("REDIS_HOST", "redis")
 REDIS_PORT = int(os.environ.get("REDIS_PORT", 6379))
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [(REDIS_HOST, REDIS_PORT)],
+        },
+    },
+ }
+
 
 
 # Configuration for proxy
