2365c97d7101f96ed64906e64ac41b2461bd9f54
ysengoku <ysengoku.tourisme@gmail.com>
2025-03-17 22:32:59 +0100

    style: Add some test custom colors

--- front/app/src/js/components/pages/duel/DuelResult.js
+++ front/app/src/js/components/pages/duel/DuelResult.js
@@ -17,8 +17,8 @@ export class DuelResult extends HTMLElement {
 			<h1>Duel Result</h1>
       <div class="my-5">--- Show detailed Duel result here ---</div>
 			<div class="btn-container d-flex flex-row justify-content-center align-items-center my-5 gap-3">
-        <a class="btn btn-primary" href="/home" role="button">Go to Home</a>
-        <a class="btn btn-primary" role="button">Go to Profile</a>
+        <a class="btn btn-wood" href="/home" role="button">Go to Home</a>
+        <a class="btn btn-wood" role="button">Go to Profile</a>
 			</div>
 		</div>
 		`;
