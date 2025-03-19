a14fcd231f780205b6c484c5a61bbc40b66f8ac7
yusengok <ysengoku.tourisme@gmail.com>
2025-03-19 11:40:35 +0100

    feat: Update socketManager to open socket to our server

--- front/app/src/js/auth/handleLogout.js
+++ front/app/src/js/auth/handleLogout.js
@@ -1,6 +1,7 @@
 import { router } from '@router';
 import { auth } from '@auth';
 import { apiRequest, API_ENDPOINTS } from '@api';
+import { socketManager } from '@socket';
 
 export async function handleLogout() {
   const response = await apiRequest('DELETE', API_ENDPOINTS.LOGOUT, null, false, true);
@@ -10,5 +11,6 @@ export async function handleLogout() {
     console.error('A problem occurred while logging out:', response);
   }
   auth.clearStoredUser();
+  socketManager.close();
   router.navigate('/');
 }
