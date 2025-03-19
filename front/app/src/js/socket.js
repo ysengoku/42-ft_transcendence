553492ec5defe914fc0ccade73cd1b3b5e1084a6
yusengok <ysengoku.tourisme@gmail.com>
2025-03-19 11:40:35 +0100

    feat: Update socketManager to open socket to our server

--- front/app/src/js/socket.js
+++ front/app/src/js/socket.js
@@ -1,10 +1,19 @@
 const socketManager = (() => {
   class WebSocketManager {
-    constructor(url) {
-      if (!url) {
-        throw new Error('URL is required to create a WebSocketManager');
+    constructor() {
+      this.url = 'wss://' + window.location.host + '/ws/events/';
+      console.log('WebSocket URL:', this.url);
+      this.socket = null;
+      this.listeners = null;
+      this.socketOpen = false;
+    }
+
+    init() {
+      if (this.socketOpen) {
+        return;
       }
-      this.socket = new WebSocket(url);
+      this.socket = new WebSocket(this.url);
+      this.socketOpen = true;
       this.listeners = new Map();
 
       this.socket.onmessage = (event) => this.handleMessage(event);
@@ -12,6 +21,11 @@ const socketManager = (() => {
       this.socket.onopen = (event) => console.log('WebSocket opened:', event);
       this.socket.onclose = (event) => console.log('WebSocket closed:', event);
       this.socket.onerror = (event) => console.error('WebSocket error:', event);
+    }
+
+    close() {
+      this.socket.close();
+      this.socketOpen = false;
     }
 
     handleMessage(event) {
@@ -23,26 +37,26 @@ const socketManager = (() => {
         // console.error('Invalid JSON:', event.data);
         return;
       }
-      if (!message.type) {
-        console.error('Message missing type field:', message);
+      if (!message.action) {
+        console.error('Message missing action field:', message);
+        return;
       }
-      const matchedListeners = this.listeners.get(message.type);
+      const matchedListeners = this.listeners.get(message.action);
       if (!matchedListeners) {
-        console.error('No listeners set for message type:', message.type);
+        console.error('No listeners set for this action:', message.action);
         return;
       }
       matchedListeners.forEach((callback) => callback(message.data));
     }
 
-    addListener(type, callback) {
-      if (!this.listeners.has(type)) {
-        this.listeners.set(type, []);
+    addListener(action, callback) {
+      if (!this.listeners.has(action)) {
+        this.listeners.set(action, []);
       }
-      this.listeners.get(type).push(callback);
+      this.listeners.get(action).push(callback);
     }
   }
-  // TODO: Change this URL to our WebSocket server
-  return new WebSocketManager('https://echo.websocket.org/');
+  return new WebSocketManager();
 })();
 
 export { socketManager };
