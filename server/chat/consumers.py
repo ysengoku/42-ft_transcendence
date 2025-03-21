e2e0dbb8bb167c6656649249dce5789cbdc19e97
Melodie Bernard (from school) <melodie.bernard123@yahoo.fr>
2025-03-21 11:25:22 +0100

    feat : added game_invite + friend_invite functions in consumers.py and added GameInvitation and FriendInvitation in models.py

--- server/chat/consumers.py
+++ server/chat/consumers.py
@@ -12,6 +12,9 @@ class UserEventsConsumer(WebsocketConsumer):
         self.user = self.scope.get("user")
         if not self.user:
             self.close()
+    # Add user's channel to personal group to receive answers to invitations sent
+        async_to_sync(self.channel_layer.group_add)(
+            f"user_{self.user.id}", self.channel_name)
 
         self.user_profile = self.user.profile
         self.chats = Chat.objects.for_participants(self.user_profile)
@@ -50,13 +53,21 @@ class UserEventsConsumer(WebsocketConsumer):
             case "read_message":
                 self.handle_read_message(text_data_json)
             case "game_invite":
-                self.handle_game_invite(text_data_json)
-            case "reply_game_invite":
-                self.handle_reply_game_invite(text_data_json)
+                self.send_game_invite(text_data_json)
+            case "accept_game_invite":
+                self.accept_game_invite(text_data_json)
+            case "decline_game_invite":
+                self.decline_game_invite(text_data_json)
             case "new_tournament":
                 self.handle_new_tournament(text_data_json)
-            case "new_friend":
-                self.handle_new_friend(text_data_json)
+            case "send_friend_request":
+                self.send_friend_request(text_data_json)
+            case "accept_friend_request":
+                self.accept_friend_request(text_data_json)
+            case "decline_friend_request":
+                self.decline_friend_request(text_data_json)
+            case _:
+                print(f"Unknown action : {action}")
 
     def handle_message(self, data):
         message, chat_id = data["message"], data["chat_id"]
@@ -156,27 +167,29 @@ class UserEventsConsumer(WebsocketConsumer):
     def chat_message(self, event):
         message = event["message"]
         # Send message to WebSocket
-        self.send(text_data=message)
+        try:
+            json.loads(message)
+            # message already in JSON -> send
+            self.send(text_data=message)
+        except json.JSONDecodeError:
+            # message no in JSON -> restructure and send
+            self.send(text_data=json.dumps({"message": message}))
+        # self.send(text_data=message)
         # message is created in handle_message :
         # -> it is already structured in json before being send
         # -> this function seems useless / reworking for nothing
         # self.send(text_data=json.dumps({"message": message}))
-        # else, can treat if with a try/except :
-        # try:
-        #     json.loads(message)
-        #     # Le message est déjà en JSON
-        #     self.send(text_data=message)
-        # except json.JSONDecodeError:
-        #     # Le message n'est pas en JSON, il faut le structurer
-        #     self.send(text_data=json.dumps({"message": message}))
+        # let the try / except just to be sure
 
     def handle_notification(self, data):
         notification_data = data["notification"]
+        notification_type = data["type"]
         notification_id = data.get("notification_id")
 
+        # Create the notification in the db
         if notification_id is None:
             Notification.objects.create(
-                user=self.user, message=notification_data)
+                user=self.user, message=notification_data, type=notification_type)
         else:
             try:
                 notification = Notification.objects.get(id=notification_id)
@@ -187,17 +200,126 @@ class UserEventsConsumer(WebsocketConsumer):
 
         self.send(text_data=json.dumps({
             "type": "notification",
-            "data": notification_data
+            "data": notification_data,
+            "type_notification": notification_type
         }))
 
-    def handle_game_invite(self, event):
-        pass
+    def accept_game_invite(self, data):
+        invitation_id = data["invitation_id"]
+        try:
+            invitation = GameInvitation.objects.get(id=invitation_id)
+            invitation.status = "accepted"
+            invitation.save()
+            # send notif to sender of the game invitation
+            async_to_sync(self.channel_layer.group_send)(f"user_{invitation.sender.id}", {
+                "type": "game_invite",
+                "data": {"id": invitation_id, "status": "accepted"}
+            })
 
-    def handle_reply_game_invite(self, event):
-        pass
+            self.send(text_data=json.dumps({
+                "type": "game_invite",
+                "data": {"id": invitation_id, "status": "accepted"}
+            }))
+        except GameInvitation.DoesNotExist:
+            print(f"Invitation {invitation_id} does not exist.")
+            self.send(text_data=json.dumps({
+                "type": "error",
+                        "message": "Invitation not found."
+            }))
+
+    def decline_game_invite(self, data):
+        invitation_id = data["invitation_id"]
+        try:
+            invitation = GameInvitation.objects.get(id=invitation_id)
+            invitation.status = "declined"
+            invitation.save()
+            # send notif to sender of the game invitation
+            async_to_sync(self.channel_layer.group_send)(f"user_{invitation.sender.id}", {
+                "type": "game_invite",
+                "data": {"id": invitation_id, "status": "declined"}
+            })
+            self.send(text_data=json.dumps({
+                "type": "game_invite",
+                "data": {"id": invitation_id, "status": "declined"}
+            }))
+        except GameInvitation.DoesNotExist:
+            print(f"Invitation {invitation_id} does not exist.")
+            self.send(text_data=json.dumps({
+                "type": "error",
+                        "message": "Invitation not found."
+            }))
+
+    def send_game_invite(self, event):
+        sender_id = data["sender_id"]
+        receiver_id = data["receiver_id"]
+
+        GameInvitation.objects.create(
+            sender_id=sender_id, receiver_id=receiver_id)
+
+        self.send(text_data=json.dumps({
+            "type": "game_invite",
+            "data": {"sender_id": sender_id, "receiver_id": receiver_id}
+        }))
 
     def handle_new_tournament(self, event):
-        pass
+        tournament_data = data["tournament_data"]
+        # send notification to concerned users
+        self.send(text_data=json.dumps({
+            "type": "new_tournament",
+            "data": tournament_data
+        }))
 
-    def handle_new_friend(self, event):
-        pass
+    def send_friend_request(self, data):
+        sender_id = data["sender_id"]
+        receiver_id = data["receiver_id"]
+        # Add friendship request in db
+        FriendRequest.objects.create(
+            sender_id=sender_id, receiver_id=receiver_id)
+        self.send(text_data=json.dumps({
+            "type": "new_friend",
+            "data": {"sender_id": sender_id, "receiver_id": receiver_id}
+        }))
+
+    def accept_friend_request(self, data):
+        request_id = data["request_id"]
+        try:
+            request = FriendRequest.objects.get(id=request_id)
+            request.status = "accepted"
+            request.save()
+            # send notif to the request sender
+            async_to_sync(self.channel_layer.group_send)(f"user_{request.sender_id}", {
+                "type": "friend_request_accepted",
+                "data": {"id": request_id}
+            })
+            self.send(text_data=json.dumps({
+                "type": "friend_request_accepted",
+                "data": {"id": request_id}
+            }))
+        except FriendRequest.DoesNotExist:
+            print(f"Request {request_id} does not exist.")
+            self.send(text_data=json.dumps({
+                "type": "error",
+                        "friend_request": "Friend request not found."
+            }))
+
+    def decline_friend_request(self, data):
+        request_id = data["request_id"]
+        try:
+            request = FriendRequest.objects.get(id=request_id)
+            request.status = "declined"
+            request.save()
+            # send notif to the request sender
+            async_to_sync(self.channel_layer.group_send)(f"user_{request.sender_id}", {
+                "type": "friend_request_declined",
+                "data": {"id": request_id}
+            })
+            self.send(text_data=json.dumps({
+                "type": "friend_request_declined",
+                "data": {"id": request_id}
+            }))
+        except FriendRequest.DoesNotExist:
+            print(f"Request {request_id} does not exist.")
+            self.send(text_data=json.dumps({
+                "type": "error",
+                        "friend_request": "Friend request not found."
+            }))
