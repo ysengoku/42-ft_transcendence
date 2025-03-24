a13cb8a90d1be554eace94ba2b5b7a50338fc958
Melodie Bernard (from school) <melodie.bernard123@yahoo.fr>
2025-03-24 07:54:01 +0100

    test: added tests

--- server/chat/urls.py
+++ server/chat/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     # JUST FORT TESTING
-    # path("<str:room_name>/", views.room, name="room"),
+    path("<str:room_name>/", views.room, name="room"),
     # JUST FORT TESTING
-    path("/", views.room, name="room"),
+    # path("/", views.room, name="room"),
+    path("notifications/test", views.notifications_view, name="notifications"),
 ]
