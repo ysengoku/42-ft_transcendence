769eb4df131cc15dc8d1258bf33f91e2770d8efc
Melodie Bernard (from school) <melodie.bernard123@yahoo.fr>
2025-03-25 10:38:29 +0100

    norm: removed commented code

--- server/chat/urls.py
+++ server/chat/urls.py
@@ -4,9 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    # JUST FORT TESTING
     path("<str:room_name>/", views.room, name="room"),
-    # JUST FORT TESTING
-    # path("/", views.room, name="room"),
     path("notifications/test", views.notifications_view, name="notifications"),
 ]
