7e46d5fdd0a4f7e459129a354d113862305a1f21
melodie <melodie.bernard123@yahoo.fr>
2025-03-27 14:24:06 +0100

    test: added test_chat.py, configured ruff to ignore the test files since they always contain a fake user with a fake password

--- server/chat/tests/test_notifications.py
+++ server/chat/tests/test_notifications.py
@@ -1,22 +1,21 @@
-
-from http import HTTPStatus
-
 from django.contrib.auth import get_user_model
 from django.test import RequestFactory, TestCase
 
 from chat.views import notifications_view
 
 User = get_user_model()
+HTTP_OK = 200
 
 
 class TestNotificationsViewTestCase(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
         self.user = User.objects.create_user(
-            username="testuser", password="12345") # noqa: S106
+            username="testuser", password="12345",
+        )  # noqa: S106
 
     def test_notifications_view(self):
         request = self.factory.get("/chat/notifications/test/")
-        request.user = self.user  # Simuler un utilisateur connecté
+        request.user = self.user
         response = notifications_view(request)
-        assert response.status_code == HTTPStatus.OK # noqa: S101
+        assert response.status_code == HTTP_OK  # noqa: S101
