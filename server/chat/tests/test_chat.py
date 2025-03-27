46f07f899e49ac45790eadceef3987d40f0689fa
melodie <melodie.bernard123@yahoo.fr>
2025-03-27 14:24:06 +0100

    test: added test_chat.py, configured ruff to ignore the test files since they always contain a fake user with a fake password

--- server/chat/tests/test_chat.py
+++ server/chat/tests/test_chat.py
@@ -1,6 +1,5 @@
-import pytest
 from django.contrib.auth import get_user_model
-from django.db.models.signals import post_save
+from django.test import TestCase
 
 from chat.models import Chat, ChatMessage
 from users.models import Profile
@@ -8,25 +7,22 @@ from users.models import Profile
 User = get_user_model()
 
 
-post_save.disconnect(Profile.objects.create, sender=User)
+class ChatTestCase(TestCase):
 
+    def setUp(self):
+        self.user1 = User.objects.create(
+            username="user1", password="password1")
+        self.user2 = User.objects.create(
+            username="user2", password="password2")
+        self.profile1, _ = Profile.objects.get_or_create(user=self.user1)
+        self.profile2, _ = Profile.objects.get_or_create(user=self.user2)
 
-@pytest.mark.django_db
-def test_chat_id_consistency():
-    # Setup : Créer deux utilisateurs et leurs profils
-    user1 = User.objects.create(username="user1", password="password1")
-    user2 = User.objects.create(username="user2", password="password2")
-    profile1 = Profile.objects.create(user=user1)
-    profile2 = Profile.objects.create(user=user2)
+    def test_chat_id_consistency(self):
+        chat = Chat.objects.create()
+        chat.participants.add(self.profile1, self.profile2)
 
-    # Créer un chat entre les deux profils
-    chat = Chat.objects.create()
-    chat.participants.add(profile1, profile2)
+        message = ChatMessage.objects.create(
+            sender=self.profile1, content="Test message", chat=chat,
+        )
 
-    # Envoyer un message
-    message = ChatMessage.objects.create(
-        sender=profile1, content="Test message", chat=chat
-    )
-
-    # Vérifier que le chat_id du message correspond au chat créé
-    assert message.chat.id == chat.id
+        assert message.chat.id == chat.id
