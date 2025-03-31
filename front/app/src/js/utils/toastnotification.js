000ba1f8e733510ccffc1a099426c473eecc498b
yusengok <ysengoku.tourisme@gmail.com>
2025-03-31 09:36:34 +0200

    merge dev-front into dev

--- front/app/src/js/utils/toastnotification.js
+++ front/app/src/js/utils/toastnotification.js
@@ -4,11 +4,25 @@ export function showToastNotification(message) {
     return;
   }
   toastContainer.innerHTML = `
-    <div class="toast-container position-fixed bottom-0 end-0 p-3">
+    <style>
+    .toast-container {
+      top: 56px;
+    }
+    .toast {
+      background-color: var(--pm-primary-500);
+    }
+    .toast-body-wrapper {
+      background-color: var(--pm-primary-500);
+      border: none;
+      border-radius: 0.5rem;
+      color: var(--pm-gray-100);
+    }
+    </style>
+    <div class="toast-container position-fixed end-0 p-3">
       <div class="toast align-items-center border-0" role="alert" aria-live="assertive" aria-atomic="true">
-	    <div class="d-flex">
-          <div class="toast-body ps-2"></div>
-		  <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+	    <div class="toast-body-wrapper d-flex">
+        <div class="toast-body ps-4"></div>
+		    <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
         </div>
   	  </div>
     </div>`;
