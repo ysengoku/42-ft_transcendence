6ae3b29416563f67ee5a59e1cf350a22caef0b71
yusengok <ysengoku.tourisme@gmail.com>
2025-03-31 11:53:18 +0200

    style: Replace console.log by devLog

--- front/app/src/js/auth/handleLogout.js
+++ front/app/src/js/auth/handleLogout.js
@@ -1,14 +1,13 @@
 import { router } from '@router';
 import { auth } from '@auth';
 import { apiRequest, API_ENDPOINTS } from '@api';
-import { socketManager } from '@socket';
 
 export async function handleLogout() {
   const response = await apiRequest('DELETE', API_ENDPOINTS.LOGOUT, null, false, true);
   if (response.success) {
-    console.log('Logout successful');
+    devLog('Logout successful');
   } else {
-    console.error('A problem occurred while logging out:', response);
+    devErrorLog('A problem occurred while logging out:', response);
   }
   auth.clearStoredUser();
   router.navigate('/');
