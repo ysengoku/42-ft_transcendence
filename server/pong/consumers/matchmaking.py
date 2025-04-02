af91e66588d5f9c1e3164d6787189d157a496538
emuminov <jseldardw@gmail.com>
2025-04-02 17:10:12 +0200

    refactor: removed worker, simplified matchmaking protocol, chaned PendingMatch model to MatchmakingTicket

--- server/pong/consumers/matchmaking.py
+++ server/pong/consumers/matchmaking.py
@@ -1,59 +1,40 @@
 import json
 
-from channels.generic.websocket import AsyncConsumer, AsyncWebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer
+
+from pong.models import MatchmakingTicket
 
 """
-example protocol
-flowchart TD
-    A[Player clicks "Find Match"] --> B[HTTP POST to /matchmaking]
-    B --> C[Create Matchmaking Ticket]
-    C --> D[Matchmaking Worker scans tickets]
-    D --> E{Match Found?}
-    E -- Yes --> F[Update tickets & create match record]
-    F --> G[Send match found event via WebSocket]
-    G --> H[Players receive notification & respond]
-    H -- Accept --> I[Finalize match & notify clients]
-    H -- Decline --> J[Remove ticket & re-queue accepted players]
-"""
-
-"""
-user connects and creates ticket
+flowchart
+    A[User clicks "Find match"] --> B[Establishes websocket connection to /ws/matchmaking]
+    B --> C[Checks database or creates matchmaking ticket]
+    C --> D[Waits for another player]
+    D -->
+          E[Disconnects] --> F[The ticket is removed from the queue]
+          G[Player is found] --> H[Players are redirected to the game route/room]
 """
 
 
-class MatchmakingConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
+class MatchmakingConsumer(WebsocketConsumer):
+    def connect(self):
         self.user = self.scope.get("user")
         if not self.user:
-            await self.close()
+            self.close()
             return
 
-        await self.channel_layer.send(
-            "matchmaking",
-            {
-                "type": "matchmaking.find",
-                "heh": "hehehe",
-            },
-        )
-        # user connects
-        #     -> search for matchmaking ticket
-        #           -> if found, startgame
-        #           -> if not found, create a ticket with one pending player
-        await self.accept()
+        self.accept()
+        self.match = MatchmakingTicket.objects.filter(status=MatchmakingTicket.PENDING).first()
+        if self.match:
+            raise Exception("not implemented yet")
+        self.match = MatchmakingTicket.objects.create(player=self.user.profile)
 
-    async def disconnect(self, code: int):
-        pass
+    def disconnect(self, code: int):
+        self.match.close()
 
-    async def receive(self, text_data):
-        pass
-
-    async def search_match(self, message: dict):
-        await self.send(text_data=json.dumps({"id": self.user.id}))
-
-
-class MatchmakingWorkerConsumer(AsyncConsumer):
-    async def matchmaking_find(self, message):
-        print("As shrimple as that. Message: ", message)
-
-    async def search_match(self, message: dict):
-        await self.send(text_data=json.dumps({"id": self.user.id}))
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        action = text_data_json["action"]
+        match action:
+            case "cancel":
+                self.match.close()
+                self.close()
