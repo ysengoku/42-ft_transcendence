7ec27717c360e72230e54e7e92ff086152649cc7
emuminov <jseldardw@gmail.com>
2025-04-06 18:58:34 +0200

    feat: added a working endpoint for notifications, updated the model to match the required schema

--- server/chat/router/endpoints/notifications.py
+++ server/chat/router/endpoints/notifications.py
@@ -1,15 +1,9 @@
-import logging
-
-from asgiref.sync import async_to_sync
-from channels.layers import get_channel_layer
 from django.http import HttpRequest
 from ninja import Router
-from ninja.errors import HttpError
 from ninja.pagination import paginate
 
 from chat.models import Notification
 from chat.schemas import NotificationSchema
-from common.routers import get_profile_queryset_by_username_or_404
 from common.schemas import MessageSchema
 
 notifications_router = Router()
@@ -19,7 +13,8 @@ notifications_router = Router()
 @paginate
 def get_notifications(request: HttpRequest):
     """
-    Gets notifications.
+    Gets notifications of the user who is currently logged in.
     Paginated by the `limit` and `offset` settings.
     For example, `/notifications?&limit=10&offset=0` will get 10 notifications from the very first one.
     """
+    return Notification.objects.filter(receiver=request.auth.profile.id)
