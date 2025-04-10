81969eb5a5bbacc4564fbaee037de3c895cef5a7
melodie <melodie.bernard123@yahoo.fr>
2025-04-10 21:09:04 +0200

    feat: working on putting a docker containing crontab on alpine, for the moment, crontab exits with an error, after saying setpgid operation not permitted

--- server/chat/urls.py
+++ server/chat/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from . import views
+from .views import check_inactive_users_view
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:room_name>/", views.room, name="room"),
     path("notifications/test", views.notifications_view, name="notifications"),
+    path('api/check-inactive-users/', check_inactive_users_view, name='check_inactive_users'),
 ]
