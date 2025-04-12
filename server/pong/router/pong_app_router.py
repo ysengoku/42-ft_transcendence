c1d69c17c3163a502f10d995f7bd074feeae11d1
emuminov <jseldardw@gmail.com>
2025-04-12 20:58:43 +0200

    refactor: change the name of the game_stats endpoint

--- server/pong/router/pong_app_router.py
+++ server/pong/router/pong_app_router.py
@@ -4,4 +4,4 @@ from .endpoints.game_stats import game_stats_router
 
 pong_app_router = Router()
 
-pong_app_router.add_router("game_stats", game_stats_router)
+pong_app_router.add_router("game-stats", game_stats_router)
