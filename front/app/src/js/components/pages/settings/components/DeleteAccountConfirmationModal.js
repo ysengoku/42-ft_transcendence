b44c822a6ebf1ebab1e05be534f66bc947098a4d
ysengoku <ysengoku.tourisme@gmail.com>
2025-04-13 00:12:46 +0200

    feat: Add confirmation modal in case user tries to quite/reload the page during matchmaking/starting status (still on working)

--- front/app/src/js/components/pages/settings/components/DeleteAccountConfirmationModal.js
+++ front/app/src/js/components/pages/settings/components/DeleteAccountConfirmationModal.js
@@ -36,18 +36,18 @@ export class DeleteAccountConfirmationModal extends HTMLElement {
   template() {
     return `
     <div class="modal fade" id="delete-account-confirm-modal" tabindex="-1" aria-hidden="true">
-	  <div class="modal-dialog">
-	    <div class="modal-content">
-		  <div class="modal-body py-4">
-		    Are you sure you want to delete your account?
-		  </div>
-		  <div class="modal-footer">
-		    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-			<button type="button" class="btn btn-danger" id="delete-account-confirm">Delete account</button>
-		  </div>
-		</div>
-	  <div>
-	</div>
+	    <div class="modal-dialog">
+	      <div class="modal-content">
+		      <div class="modal-body py-4">
+		        Are you sure you want to delete your account?
+		      </div>
+		      <div class="modal-footer">
+		        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+			      <button type="button" class="btn btn-danger" id="delete-account-confirm">Delete account</button>
+		      </div>
+		    </div>
+	    <div>
+	  </div>
 	`;
   }
 }
