a2ccaa87c1afe0d845919a0ca8c16d76ece539fb
Melodie Bernard (from school) <melodie.bernard123@yahoo.fr>
2025-04-14 14:53:15 +0200

    minor: put back 5 minutes to wait before disconnexion

--- server/users/consumers.py
+++ server/users/consumers.py
@@ -94,7 +94,7 @@ redis_status_manager = RedisUserStatusManager()
 
 def check_inactive_users():
     logger.info("Checking for inactive users")
-    threshold = timezone.now() - timedelta(minutes=1)
+    threshold = timezone.now() - timedelta(minutes=5)
 
     inactive_users = Profile.objects.filter(
         last_activity__lt=threshold,
