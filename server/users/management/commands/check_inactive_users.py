089b0f1e59b57ed5a96c263a603291082453a867
Melodie Bernard (from school) <melodie.bernard123@yahoo.fr>
2025-04-14 14:08:06 +0200

    fix: online status when user goes offline after the crontab disconnected them and refresh the page now acts normally

--- server/users/management/commands/check_inactive_users.py
+++ server/users/management/commands/check_inactive_users.py
@@ -1,9 +1,13 @@
 from django.core.management.base import BaseCommand
 from chat.consumers import check_inactive_users
 
+import logging
+
+logger = logging.getLogger("server")
+
 class Command(BaseCommand):
     help = 'Vérifie les utilisateurs inactifs toutes les 5 minutes'
 
     def handle(self, *args, **options):
         check_inactive_users()
-        self.stdout.write("Vérification des utilisateurs inactifs terminée.")
+        logger.info("Vérification des utilisateurs inactifs terminée.")
