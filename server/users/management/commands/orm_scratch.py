26cab76d77ed97a2beb43fbd0be8547bdda032fb
emuminov <jseldardw@gmail.com>
2025-04-14 19:25:21 +0200

    feat: added endpoint for getting match history, as well as a specific match

--- server/users/management/commands/orm_scratch.py
+++ server/users/management/commands/orm_scratch.py
@@ -1,14 +1,8 @@
 from django.core.management.base import BaseCommand
 
-from chat.models import Notification
-from users.models import Profile
-
 
 class Command(BaseCommand):
     help = "Creates application data"
 
     def handle(self, **kwargs) -> None:
-        u1 = Profile.objects.for_username("celiastral").first()
-        u2 = Profile.objects.for_username("emuminov").first()
-
-        Notification.objects.action_new_friend(receiver=u1, sender=u2)
+        pass
