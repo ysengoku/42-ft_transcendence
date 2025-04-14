33e5c6ac87737ac5a4fc6c09818e19e3240ead8a
yusengok <ysengoku.tourisme@gmail.com>
2025-04-14 08:45:18 +0200

    merge dev into dev-front

--- front/app/__mock__/functions/mockDuelData.js
+++ front/app/__mock__/functions/mockDuelData.js
@@ -1,6 +1,21 @@
-export function mockDuelBeforeData() {
+export function mockDuelData(status) {
+  switch (status) {
+    case 'waiting_opponent':
+      return withoutResult('waiting_opponent');
+    case 'starting':
+      return withoutResult('starting');
+    case 'canceled':
+      return withoutResult('canceled');
+    case 'finished':
+      return finished;
+    default:
+      return withoutResult('starting');
+  }
+}
+
+function withoutResult(status) {
   const data = {
-    status: 'start',
+    status: status,
     gameId: '1234567890',
     player1: {
       username: 'Pedro',
@@ -23,27 +38,24 @@ export function mockDuelBeforeData() {
   return data;
 }
 
-export function mockDuelEndData() {
-  const data = {
-    status: 'end',
-    gameId: '1234567890',
-    player1: {
-      username: 'Pedro',
-      nickname: 'Pedro',
-      avatar: '/img/default-avatar.png',
-      elo: 1480,
-      is_online: true,
-    },
-    player2: {
-      username: 'Alice',
-      nickname: 'Alice',
-      avatar: '/img/default-avatar.png',
-      elo: 1500,
-      is_online: true,
-    },
-    winner_score: 5,
-    looser_score: 3,
-    date: '',
-  };
-  return data;
-}
+const finished = {
+  status: 'finished',
+  gameId: '1234567890',
+  player1: {
+    username: 'Pedro',
+    nickname: 'Pedro',
+    avatar: '/img/default-avatar.png',
+    elo: 1480,
+    is_online: true,
+  },
+  player2: {
+    username: 'Alice',
+    nickname: 'Alice',
+    avatar: '/img/default-avatar.png',
+    elo: 1500,
+    is_online: true,
+  },
+  winner_score: 5,
+  looser_score: 3,
+  date: '2025-04-02T09:00:00Z',
+};
--- front/app/src/js/components/pages/duel/components/ConfirmationModal.js
+++ front/app/src/js/components/pages/duel/components/ConfirmationModal.js
@@ -5,34 +5,33 @@ export class ConfirmationModal extends HTMLElement {
     super();
     this.modal = null;
 
-	this.handleConfirm = null;
-	this.handleDancel = null;
+    this.handleConfirm = null;
+    this.handleDancel = null;
   }
 
   set handleConfirm(callback) {
-	this._handleConfirm = callback;
-	this.confirmButton?.addEventListener('click', this._handleConfirm);
+    this._handleConfirm = callback;
+    this.confirmButton?.addEventListener('click', this._handleConfirm);
   }
 
   set handleCancel(callback) {
-	this._handleCancel = callback;
-	this.cancelButton?.addEventListener('click', this._handleCancel);
+    this._handleCancel = callback;
+    this.cancelButton?.addEventListener('click', this._handleCancel);
   }
-  
-  disconnectedCallback() {
-	this.confirmButton.removeEventListener('click', this._handleConfirm);
-	this.cancelButton.removeEventListener('click', this._handleCancel);
-  }
-  
-  render() {
-	this.innerHTML = this.template();
-	this.modal = new Modal(this.querySelector('.modal'));
-	  
-	this.confirmButton = this.querySelector('.confirm-button');
-	this.cancelButton = this.querySelector('.cancel-button');
-	this.confirmButton.addEventListener('click', this._handleConfirm);
-	this.cancelButton.addEventListener('click', this._handleCancel);
 
+  disconnectedCallback() {
+    this.confirmButton.removeEventListener('click', this._handleConfirm);
+    this.cancelButton.removeEventListener('click', this._handleCancel);
+  }
+
+  render() {
+    this.innerHTML = this.template();
+    this.modal = new Modal(this.querySelector('.modal'));
+
+    this.confirmButton = this.querySelector('.confirm-button');
+    this.cancelButton = this.querySelector('.cancel-button');
+    this.confirmButton.addEventListener('click', this._handleConfirm);
+    this.cancelButton.addEventListener('click', this._handleCancel);
   }
 
   showModal() {
@@ -49,7 +48,7 @@ export class ConfirmationModal extends HTMLElement {
               <div class="confirmation-message modal-body py-4"></div>
               <div class="modal-footer">
                 <button type="button" class="cancel-button btn" data-bs-dismiss="modal"></button>
-                <button type="button" class="confirm-button btn btn-danger data-bs-dismiss="modal"></button>
+                <button type="button" class="confirm-button btn btn-danger" data-bs-dismiss="modal"></button>
               </div>
             </div>
         <div>
