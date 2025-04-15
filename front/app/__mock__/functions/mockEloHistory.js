63dd635375e1b03f83566897b134e73266fb77f2
yusengok <ysengoku.tourisme@gmail.com>
2025-04-15 12:28:31 +0200

    Feat: Add duel detail modal on Profile

--- front/app/__mock__/functions/mockEloHistory.js
+++ front/app/__mock__/functions/mockEloHistory.js
@@ -1,57 +1,57 @@
 export function mockEloHistory() {
   return {
-    "items": [
+    items: [
       {
-        "day": "2025-03-31",
-        "daily_elo_change": -2,
-        "elo_result": 1164
+        day: '2025-03-31',
+        daily_elo_change: -2,
+        elo_result: 1164,
       },
       {
-        "day": "2025-03-30",
-        "daily_elo_change": -21,
-        "elo_result": 1166
+        day: '2025-03-30',
+        daily_elo_change: -21,
+        elo_result: 1166,
       },
       {
-        "day": "2025-03-29",
-        "daily_elo_change": -47,
-        "elo_result": 1187
+        day: '2025-03-29',
+        daily_elo_change: -47,
+        elo_result: 1187,
       },
       {
-        "day": "2025-03-28",
-        "daily_elo_change": 48,
-        "elo_result": 1234
+        day: '2025-03-28',
+        daily_elo_change: 48,
+        elo_result: 1234,
       },
       {
-        "day": "2025-03-27",
-        "daily_elo_change": -40,
-        "elo_result": 1186
+        day: '2025-03-27',
+        daily_elo_change: -40,
+        elo_result: 1186,
       },
       {
-        "day": "2025-03-26",
-        "daily_elo_change": 24,
-        "elo_result": 1226
+        day: '2025-03-26',
+        daily_elo_change: 24,
+        elo_result: 1226,
       },
       {
-        "day": "2025-03-25",
-        "daily_elo_change": -51,
-        "elo_result": 1202
+        day: '2025-03-25',
+        daily_elo_change: -51,
+        elo_result: 1202,
       },
       {
-        "day": "2025-03-24",
-        "daily_elo_change": 61,
-        "elo_result": 1253
+        day: '2025-03-24',
+        daily_elo_change: 61,
+        elo_result: 1253,
       },
       {
-        "day": "2025-03-23",
-        "daily_elo_change": 43,
-        "elo_result": 1192
+        day: '2025-03-23',
+        daily_elo_change: 43,
+        elo_result: 1192,
       },
       {
-        "day": "2025-03-22",
-        "daily_elo_change": 149,
-        "elo_result": 1149
-      }
+        day: '2025-03-22',
+        daily_elo_change: 149,
+        elo_result: 1149,
+      },
     ],
-    "count": 10
-  }
+    count: 10,
+  };
 }
