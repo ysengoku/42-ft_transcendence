84f18e3fdca4a94b6d8e566aa5b18e2cc0fedc1e
ysengoku <ysengoku.tourisme@gmail.com>
2025-04-23 22:28:14 +0200

    feat: add confrim button handling to game options in Duel Menu, add game options form to Local Game Menu

--- front/app/src/js/components/pages/match/LocalGameMenu.js
+++ front/app/src/js/components/pages/match/LocalGameMenu.js
@@ -16,6 +16,9 @@ export class LocalGameMenu extends HTMLElement {
 
   render() {
     this.innerHTML = this.template() + this.style();
+
+    this.querySelector('.modal-title')?.classList.add('d-none');
+    this.querySelector('#is-ranked-selector')?.classList.add('d-none');
   }
 
   template() {
@@ -24,12 +27,13 @@ export class LocalGameMenu extends HTMLElement {
       <div class="row justify-content-center py-4">
         <div class="form-container col-12 col-sm-10 col-md-8 col-lg-6 col-xl-5 p-4">
           <div class="d-flex flex-column justify-content-center align-items-center w-100">
-            <h2 class="text-start m-0pt-2 w-75">Local Game</h2>
-            <form class="w-75">
-              <p class="fs-5 fw-bolder m-0 mb-3">Options</p>
-              <button type="submit" id="local-game-classic" class="btn btn-wood btn-lg my-2 w-100">Classic local game</button>
-              <button type="submit" id="local-game-ai" class="btn btn-wood btn-lg my-2 w-100">Play against AI</button>
-            </form>
+            <div class="w-75">
+              <h2 class="text-start m-0 py-2 w-75">Local Game</h2>
+              <p class="fs-5 fw-bolder m-0 my-3">Game Options</p>
+              <game-options></game-options>
+              <button type="submit" id="local-game-classic" class="btn btn-wood btn-lg mt-5 w-100">Local player battle</button>
+              <button type="submit" id="local-game-ai" class="btn btn-wood btn-lg my-4 w-100">AI Challenge</button>
+            </div>
           </div>
         </div>
       </div>
