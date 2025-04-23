27feb07766544b415058300d7baa2ddbbe1b62de
yusengok <ysengoku.tourisme@gmail.com>
2025-04-23 16:18:17 +0200

    refactor: move game options modal into DuelMenu, create GameOptions (on working)

--- front/app/src/js/components/pages/match/components/index.js
+++ front/app/src/js/components/pages/match/components/index.js
@@ -1,9 +1,11 @@
 import { GameOptionsModal } from './GameOptionsModal';
+import { GameOptions } from './GameOptions';
 import { DuelPreview } from './DuelPreview';
 import { ConfirmationModal } from './ConfirmationModal';
 
 export {
   GameOptionsModal,
+  GameOptions,
   DuelPreview,
   ConfirmationModal,
 };
