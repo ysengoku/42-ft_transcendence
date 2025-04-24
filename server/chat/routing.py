a3ee91fadac0aca008ad1f3266cdc31f4c9ea073
Melodie Bernard (from school) <melodie.bernard123@yahoo.fr>
2025-04-24 09:55:52 +0200

    norm: fixed norm mistakes, going to put online_status settings.py file to be able to have every password and link in the env properly

--- server/chat/routing.py
+++ server/chat/routing.py
@@ -1,5 +1,4 @@
 # chat/routing.py
-# from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.sessions import SessionMiddlewareStack
 from django.urls import re_path
@@ -7,22 +6,15 @@ from django.urls import re_path
 from . import consumers
 from .middleware import JWTAuthMiddleware
 
-# from .middleware import JWTAuthMiddleware
-
 websocket_urlpatterns = [
     re_path(r"ws/events/$", consumers.UserEventsConsumer.as_asgi()),
 ]
 
-
+# only for automatic tests
 application = ProtocolTypeRouter({
     "websocket": SessionMiddlewareStack(  # Ajout du middleware de session
         JWTAuthMiddleware(
-            URLRouter(websocket_urlpatterns)
-        )
-    )
+            URLRouter(websocket_urlpatterns),
+        ),
+    ),
 })
-# application = ProtocolTypeRouter({
-#     "websocket": JWTAuthMiddleware(
-#         URLRouter(websocket_urlpatterns)
-#     )
-# })
