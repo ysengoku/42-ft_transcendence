bc3f3f49f2b0159d55a3c07d5d987670476ff0bf
yusengok <ysengoku.tourisme@gmail.com>
2025-04-29 17:01:04 +0200

    minor: add listeners to tournament menu (on working)

--- front/app/__mock__/functions/mockTournamentListData.js
+++ front/app/__mock__/functions/mockTournamentListData.js
@@ -55,7 +55,7 @@ export async function mockTournamentList() {
     name: 'Valor Clash',
     date: '2025-06-10T15:00:00Z',
     status: 'ongoing',
-    required_participants: 16,
+    required_participants: 8,
     creator: {
       alias: 'BladeRunner',
       user: {
@@ -66,7 +66,88 @@ export async function mockTournamentList() {
       is_online: true,
       }
     },
-    participants: [],
+    participants: [
+      {
+        alias: 'WhiskeyDuke',
+        user: {
+          username: 'GeorgeLucas',
+          nickname: 'The Legend',
+          avatar: '/__mock__/img/sample-pic2.png',
+          elo: 1500,
+          is_online: true,
+        }
+        },
+        {
+        alias: 'LoneStar',
+        user: {
+          username: 'Alex',
+          nickname: 'Jedi Master',
+          avatar: '/__mock__/img/sample-pic3.png',
+          elo: 1500,
+          is_online: false,
+        }
+        },
+        {
+        alias: 'ShadowFang',
+        user: {
+          username: 'ShadowFang',
+          nickname: 'The Phantom',
+          avatar: '/__mock__/img/sample-pic1.jpg',
+          elo: 1500,
+          is_online: true,
+        }
+        },
+        {
+          alias: 'SpeedFreak',
+          user: {
+            username: 'Tommy',
+            nickname: 'Speedster',
+            avatar: '/__mock__/img/sample-pic2.png',
+            elo: 1550,
+            is_online: false,
+          }
+          },
+          {
+            alias: 'QuickSilver',
+            user: {
+              username: 'Sam',
+              nickname: 'Blitz',
+              avatar: '/__mock__/img/sample-pic2.png',
+              elo: 1490,
+              is_online: true,
+            }
+          },
+          {
+            alias: 'SilentStorm',
+            user: {
+            username: 'Morgan',
+            nickname: 'The Whisper',
+            avatar: '/__mock__/img/sample-pic1.jpg',
+            elo: 1450,
+            is_online: false,
+            }
+          },
+          {
+            alias: 'LoneStar',
+            user: {
+              username: 'Sammy',
+              nickname: 'Blitz',
+              avatar: '/__mock__/img/sample-pic2.png',
+              elo: 1490,
+              is_online: true,
+            }
+          },
+          {
+            alias: 'StormBreaker',
+            user: {
+            username: 'Morganv',
+            nickname: 'The Whisper',
+            avatar: '/__mock__/img/sample-pic1.jpg',
+            elo: 1450,
+            is_online: false,
+            }
+          },
+    ],
     winner: null,
     },
     {
@@ -95,7 +176,37 @@ export async function mockTournamentList() {
         elo: 1550,
         is_online: false,
       }
-      }
+      },
+      {
+        alias: 'QuickSilver',
+        user: {
+          username: 'Sam',
+          nickname: 'Blitz',
+          avatar: '/__mock__/img/sample-pic2.png',
+          elo: 1490,
+          is_online: true,
+        }
+      },
+      {
+        alias: 'SilentStorm',
+        user: {
+        username: 'Morgan',
+        nickname: 'The Whisper',
+        avatar: '/__mock__/img/sample-pic1.jpg',
+        elo: 1450,
+        is_online: false,
+        }
+      },
+      {
+        alias: 'ShadowFang',
+        user: {
+          username: 'ShadowFang',
+          nickname: 'The Phantom',
+          avatar: '/__mock__/img/sample-pic1.jpg',
+          elo: 1500,
+          is_online: true,
+        }
+      }, 
     ],
     winner: {
       alias: 'SpeedFreak',
@@ -105,7 +216,8 @@ export async function mockTournamentList() {
       avatar: '/__mock__/img/sample-pic2.png',
       elo: 1550,
       is_online: false,
-      }
+      },
+      
     },
     },
     {
@@ -113,7 +225,7 @@ export async function mockTournamentList() {
     name: 'Gladiator\'s Arena',
     date: '2025-05-15T20:00:00Z',
     status: 'lobby',
-    required_participants: 32,
+    required_participants: 4,
     creator: {
       alias: 'WarHammer',
       user: {
@@ -162,7 +274,7 @@ export async function mockTournamentList() {
     name: 'Nexus Battle',
     date: '2025-08-20T14:00:00Z',
     status: 'lobby',
-    required_participants: 16,
+    required_participants: 8,
     creator: {
       alias: 'DarkKnight',
       user: {
--- front/app/src/js/components/pages/tournament/TournamentMenu.js
+++ front/app/src/js/components/pages/tournament/TournamentMenu.js
@@ -1,52 +1,123 @@
+import { Modal } from 'bootstrap';
 import './components/index.js';
 
 export class TournamentMenu extends HTMLElement {
   #state = {
+    newTournament: null,
+    tounamentRegistration: null,
   };
 
   constructor() {
     super();
+
+    this.showCreateNewTournamentForm = this.showCreateNewTournamentForm.bind(this);
+    this.showTournamentDetail = this.showTournamentDetail.bind(this);
+    this.toggleRequiredParticipants = this.toggleRequiredParticipants.bind(this);
   }
 
   connectedCallback() {
     this.render();
-  
-    this.createNewTournament = this.createNewTournament.bind(this);
-    this.registerForTournament = this.registerForTournament.bind(this);
   }
 
   disconnectedCallback() {
-    this.createTournamentButton?.removeEventListener('click', this.createNewTournament);
-    this.list?.removeEventListener('register-tournament', this.registerForTournament);
+    this.createTournamentButton?.removeEventListener('click', this.showCreateNewTournamentForm);
+    this.list?.removeEventListener('register-tournament', this.showTournamentDetail);
+    this.noOpenTournaments?.removeEventListener('click', this.showCreateNewTournamentForm);
   }
 
+  /* ------------------------------------------------------------------------ */
+  /*      Render                                                              */
+  /* ------------------------------------------------------------------------ */
   render() {
     this.innerHTML = this.template();
 
     this.createTournamentButton = this.querySelector('#create-tournament-button');
     this.list = this.querySelector('tournament-list');
+    
+    this.createTournamentButton.addEventListener('click', this.showCreateNewTournamentForm);
+    this.list.addEventListener('click', this.showTournamentDetail);
 
-    this.createTournamentButton.addEventListener('click', this.createNewTournament);
-    this.list.addEventListener('click', this.registerForTournament);
+    const modalTemplate = document.createElement('template');
+    modalTemplate.innerHTML = this.modalTemplate();
+    this.modalComponent = modalTemplate.content.querySelector('.modal');
+    document.body.appendChild(this.modalComponent);
+    this.modal = new Modal(this.modalComponent);
+    this.modalBody = this.modalComponent.querySelector('.modal-body');
+    this.modalFooter = this.modalComponent.querySelector('.modal-footer');
+
+    window.requestAnimationFrame(() => {
+      this.noOpenTournaments = document.getElementById('no-open-tournaments');
+      this.noOpenTournaments?.addEventListener('click', this.showCreateNewTournamentForm);
+    });
   }
 
-  createNewTournament() {
+  tournamentDetail = {
+    lobby: () => {
+      this.modalBody.innerHTML = this.registerForTournamentForm();
+      const modalTitle = this.modalBody.querySelector('.modal-title');
+      const modalTournamentStatus = this.modalBody.querySelector('.modal-tournament-status');
+      modalTitle.textContent = this.tournamentName;
+      modalTournamentStatus.textContent = 'Open for entries';
+  
+      const confirmButton = this.modalFooter.querySelector('.confirm-button');
+      confirmButton.textContent = 'Register';
+    },
+    ongoing: () => {
+    },
+    finished: () => {
+    },
+    cancelled: () => {
+    }
+  };
+
+  /* ------------------------------------------------------------------------ */
+  /*      New tournament                                                      */
+  /* ------------------------------------------------------------------------ */
+  showCreateNewTournamentForm() {
     console.log('Create new tournament');
+    this.modalBody.innerHTML = '';
+
+    this.modalBody.innerHTML = this.createTournamentForm();
+    const confirmButton = this.modalFooter.querySelector('.confirm-button');
+    confirmButton.textContent = 'Create';
+
+    this.tounamentNameInput = this.modalBody.querySelector('#tournament-name');
+    this.requiredParticipant4 = this.modalBody.querySelector('#required-participants-4');
+    this.requiredParticipant8 = this.modalBody.querySelector('#required-participants-8');
+
+    this.requiredParticipant4.addEventListener('click', this.toggleRequiredParticipants);
+    this.requiredParticipant8.addEventListener('click', this.toggleRequiredParticipants);
+
+    this.modal.show();
   }
 
-  registerForTournament(event) {
-    console.log('Register for tournament');
+  toggleRequiredParticipants(event) {
+    const selectedValue = event.target.value;
+    console.log('Selected value:', selectedValue);
+  }
+
+  /* ------------------------------------------------------------------------ */
+  /*      Existing tournaments                                                */
+  /* ------------------------------------------------------------------------ */
+  showTournamentDetail(event) {
     const listItem = event.target.closest('li[tournament-id]');
-    if (!listItem) {
+    if (!listItem || !listItem.hasAttribute('tournament-id')) {
       return;
     }
-    const tournamentId = listItem.getAttribute('tournament-id');
-    const tournamentName = listItem.getAttribute('tournament-name');
+    this.tournamentId = listItem.getAttribute('tournament-id');
+    this.tournamentName = listItem.getAttribute('tournament-name');
     const tournamentStatus = listItem.getAttribute('tournament-status');
-
-    console.log('Register for tournament', tournamentId, tournamentName, tournamentStatus);
+    if (!this.tournamentId || !this.tournamentName || !tournamentStatus) {
+      return;
+    }
+    this.modalBody.innerHTML = '';
+    this.tournamentDetail[tournamentStatus]();
+    this.modal.show();
   }
 
+  /* ------------------------------------------------------------------------ */
+  /*      Template & style                                                    */
+  /* ------------------------------------------------------------------------ */
   template() {
     return `
     <div class="container">
@@ -72,6 +143,80 @@ export class TournamentMenu extends HTMLElement {
       </div>
     </div>
 		`;
+  }
+
+  modalTemplate() {
+    return `
+    <div class="modal fade mt-5" tabindex="-1" aria-hidden="true">
+      <div class="modal-dialog pt-4">
+        <div class="modal-content">
+          <div class="modal-header border-0">
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body"></div>
+          <div class="modal-footer border-0 my-2">
+            <button type="button" class="cancel-button btn" data-bs-dismiss="modal">Cancel</button>
+            <button type="button" class="confirm-button btn fw-bolder fs-5" data-bs-dismiss="modal"></button>
+          </div>
+        <div>
+      </div>
+    </div>
+    `;  
+  }
+
+  createTournamentForm() {
+    return `
+    <style>
+    .btn-outline-create-tournament {
+      border: 1px solid rgba(var(--bs-body-color-rgb), 0.4);
+      color: rgba(var(--bs-body-color-rgb), 0.6);
+    }
+    .btn-check:checked + .btn-outline-create-tournament,
+    .btn-check:active + .btn-outline-create-tournament,
+    .btn-outline-create-tournament.active {
+      color: var(--pm-primary-100);
+      font-weight: 600;
+      background-color: var(--pm-primary-500);
+      border-color: var(--pm-primary-500);
+    }
+    </style>
+    <div class="d-flex flex-column align-items-center px-4">
+      <h2 class="modal-title text-center pb-4">Create a tournament</h2>
+      <div id="create-tournament-form" class="d-flex flex-column w-100 gap-2">
+        <div class="mb-3">
+          <label for="tournament-name" class="form-label">Tournament name</label>
+          <input type="text" class="form-control" id="tournament-name" placeholder="Tournament name" minlength="3" maxlength="50" required>
+          <div class="invalid-feedback" id="tournament-name-feedback"></div>
+        </div>
+        <div class="d-flex flex-column mb-3">
+          <label class="mb-2">Number of participants</label>
+          <div class="btn-group" role="group">
+            <input type="radio" class="btn-check" name="requiredParticipants" id="required-participants-4" value="4" autocomplete="off" checked>
+            <label class="btn btn-outline-create-tournament py-0" for="required-participants-4">4</label>
+
+            <input type="radio" class="btn-check" name="requiredParticipants" id="required-participants-8" value="8" autocomplete="off">
+            <label class="btn btn-outline-create-tournament py-0" for="required-participants-8">8</label>
+          </div>
+        </div>
+      </div>
+    </div>
+    `;
+  }
+
+  registerForTournamentForm() {
+    return `
+    <div class="d-flex flex-column align-items-center px-4">
+      <h2 class="modal-title text-center"></h2>
+      <p class="modal-tournament-status text-center"></p>
+      <div id="tournament-register-form" class="d-flex flex-column w-100 gap-2">
+        <div class="mb-3">
+          <label for="tournament-alias" class="form-label">Tournament name</label>
+          <input type="text" class="form-control" id="tournament-alias" placeholder="Enter your alias" minlength="1" maxlength="12" required>
+          <div class="invalid-feedback" id="tournament-alias-feedback"></div>
+        </div>
+      </div>
+    </div>
+    `;
   }
 }
 
