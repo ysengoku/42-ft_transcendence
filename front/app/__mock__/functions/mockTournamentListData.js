41982a96d75495af8c80d1dfef278bd8cb479044
ysengoku <ysengoku.tourisme@gmail.com>
2025-05-03 00:11:09 +0200

    fix: add text overflow handling to tournament title, refactor tournament menu and sub-components, update mock data

--- front/app/__mock__/functions/mockTournamentListData.js
+++ front/app/__mock__/functions/mockTournamentListData.js
@@ -3,9 +3,8 @@ export async function mockTournamentList() {
     {
       tournament_id: 'kagerhnpewn16461gdsga',
       tournament_name: 'Peacemakers Cup',
-      date: '2025-05-01T12:00:00Z',
+      date: null,
       status: 'lobby',
-      required_participants: 8,
       creator: {
         username: 'Pedro',
         nickname: 'Pedro',
@@ -13,46 +12,14 @@ export async function mockTournamentList() {
         elo: 1500,
         is_online: true,
       },
-      participants: [
-        {
-          alias: 'WhiskeyDuke',
-          user: {
-            username: 'GeorgeLucas',
-            nickname: 'The Legend',
-            avatar: '/__mock__/img/sample-pic2.png',
-            elo: 1500,
-            is_online: true,
-          }
-        },
-        {
-          alias: 'LoneStar',
-          user: {
-            username: 'Alex',
-            nickname: 'Jedi Master',
-            avatar: '/__mock__/img/sample-pic3.png',
-            elo: 1500,
-            is_online: false,
-          }
-        },
-        {
-          alias: 'ShadowFang',
-          user: {
-            username: 'ShadowFang',
-            nickname: 'The Phantom',
-            avatar: '/__mock__/img/sample-pic1.jpg',
-            elo: 1500,
-            is_online: true,
-          }
-        }
-      ],
-      winner: null,
+      required_participants: 8,
+      participants_count: 3,
     },
     {
       tournament_id: 'bqwerty123451234qwe',
       tournament_name: 'Valor Clash',
-      date: '2025-06-10T15:00:00Z',
+      date: '2025-05-10T15:00:00Z',
       status: 'ongoing',
-      required_participants: 8,
       creator: {
         username: 'SarahConnor',
         nickname: 'Time Traveler',
@@ -60,96 +27,14 @@ export async function mockTournamentList() {
         elo: 1600,
         is_online: true,
       },
-      participants: [
-        {
-          alias: 'WhiskeyDuke',
-          user: {
-            username: 'GeorgeLucas',
-            nickname: 'The Legend',
-            avatar: '/__mock__/img/sample-pic2.png',
-            elo: 1500,
-            is_online: true,
-          }
-        },
-        {
-          alias: 'LoneStar',
-          user: {
-            username: 'Alex',
-            nickname: 'Jedi Master',
-            avatar: '/__mock__/img/sample-pic3.png',
-            elo: 1500,
-            is_online: false,
-          }
-        },
-        {
-          alias: 'ShadowFang',
-          user: {
-            username: 'ShadowFang',
-            nickname: 'The Phantom',
-            avatar: '/__mock__/img/sample-pic1.jpg',
-            elo: 1500,
-            is_online: true,
-          }
-        },
-        {
-          alias: 'SpeedFreak',
-          user: {
-            username: 'Tommy',
-            nickname: 'Speedster',
-            avatar: '/__mock__/img/sample-pic2.png',
-            elo: 1550,
-            is_online: false,
-          }
-        },
-        {
-          alias: 'QuickSilver',
-          user: {
-            username: 'Sam',
-            nickname: 'Blitz',
-            avatar: '/__mock__/img/sample-pic2.png',
-            elo: 1490,
-            is_online: true,
-          }
-        },
-        {
-          alias: 'SilentStorm',
-          user: {
-            username: 'Morgan',
-            nickname: 'The Whisper',
-            avatar: '/__mock__/img/sample-pic1.jpg',
-            elo: 1450,
-            is_online: false,
-          }
-        },
-        {
-          alias: 'LoneStar',
-          user: {
-            username: 'Sammy',
-            nickname: 'Blitz',
-            avatar: '/__mock__/img/sample-pic2.png',
-            elo: 1490,
-            is_online: true,
-          }
-        },
-        {
-          alias: 'StormBreaker',
-          user: {
-            username: 'Morganv',
-            nickname: 'The Whisper',
-            avatar: '/__mock__/img/sample-pic1.jpg',
-            elo: 1450,
-            is_online: false,
-          }
-        },
-      ],
-      winner: null,
+      required_participants: 8,
+      participants_count: 8,
     },
     {
       tournament_id: 'cultimate78904567abcdef',
       tournament_name: 'Ultimate Throwdown',
       date: '2025-04-20T18:00:00Z',
       status: 'finished',
-      required_participants: 4,
       creator: {
         username: 'Morgan',
         nickname: 'The Whisper',
@@ -157,51 +42,11 @@ export async function mockTournamentList() {
         elo: 1450,
         is_online: false,
       },
-      participants: [
-        {
-          alias: 'SpeedFreak',
-          user: {
-            username: 'Tommy',
-            nickname: 'Speedster',
-            avatar: '/__mock__/img/sample-pic2.png',
-            elo: 1550,
-            is_online: false,
-          }
-        },
-        {
-          alias: 'QuickSilver',
-          user: {
-            username: 'Sam',
-            nickname: 'Blitz',
-            avatar: '/__mock__/img/sample-pic2.png',
-            elo: 1490,
-            is_online: true,
-          }
-        },
-        {
-          alias: 'SilentStorm',
-          user: {
-            username: 'Morgan',
-            nickname: 'The Whisper',
-            avatar: '/__mock__/img/sample-pic1.jpg',
-            elo: 1450,
-            is_online: false,
-          }
-        },
-        {
-          alias: 'ShadowFang',
-          user: {
-            username: 'ShadowFang',
-            nickname: 'The Phantom',
-            avatar: '/__mock__/img/sample-pic1.jpg',
-            elo: 1500,
-            is_online: true,
-          }
-        },
-      ],
+      required_participants: 4,
+      participants_count: 4,
       winner: {
         alias: 'SpeedFreak',
-        user: {
+          user: {
           username: 'Tommy',
           nickname: 'Speedster',
           avatar: '/__mock__/img/sample-pic2.png',
@@ -213,9 +58,8 @@ export async function mockTournamentList() {
     {
       tournament_id: 'dgladiator123asdasd',
       tournament_name: "Gladiator's Arena",
-      date: '2025-05-15T20:00:00Z',
+      date: null,
       status: 'lobby',
-      required_participants: 4,
       creator: {
         username: 'Olivia',
         nickname: 'The Hammer',
@@ -223,15 +67,14 @@ export async function mockTournamentList() {
         elo: 1700,
         is_online: true,
       },
-      participants: [],
-      winner: null,
+      required_participants: 4,
+      participants_count: 0,
     },
     {
       tournament_id: 'esniper45678zxcvbnm',
       tournament_name: 'Sniper Showdown',
-      date: '2025-07-01T10:00:00Z',
+      date: null,
       status: 'lobby',
-      required_participants: 8,
       creator: {
         username: 'Daniel',
         nickname: 'Eagle Eye',
@@ -239,26 +82,14 @@ export async function mockTournamentList() {
         elo: 1520,
         is_online: false,
       },
-      participants: [
-        {
-          alias: 'QuickSilver',
-          user: {
-            username: 'Sam',
-            nickname: 'Blitz',
-            avatar: '/__mock__/img/sample-pic2.png',
-            elo: 1490,
-            is_online: true,
-          }
-        }
-      ],
-      winner: null,
+      required_participants: 8,
+      participants_count: 2,
     },
     {
       tournament_id: 'fnexus99999poiuytrewq',
       tournament_name: 'Nexus Battle',
-      date: '2025-08-20T14:00:00Z',
+      date: null,
       status: 'lobby',
-      required_participants: 8,
       creator: {
         username: 'Bruce',
         nickname: 'The Bat',
@@ -266,8 +97,8 @@ export async function mockTournamentList() {
         elo: 1800,
         is_online: true,
       },
-      participants: [],
-      winner: null,
+      required_participants: 4,
+      participants_count: 0,
     },
   ];
   return { items: data, count: data.length };
