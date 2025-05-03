6f2a0a332faad8b5e72a72c1ef212b4728d82473
emuminov <jseldardw@gmail.com>
2025-05-03 19:12:28 +0200

    fix: added async_to_sync wraps around django channels group methods, fixed bad variable name, added group_discard to disconnect on matchmaking, added better cleanup with row lock

--- server/pong/consumers/matchmaking.py
+++ server/pong/consumers/matchmaking.py
@@ -25,10 +25,32 @@ PLAYERS_REQUIRED = 2
 
 
 class MatchmakingConsumer(WebsocketConsumer):
+    def _with_db_lock_find_valid_game_room(self):
+        """
+        Filter game rooms according to requirements, without locking (due to annotations in .for_valid_game_room).
+        If room was found, use it's id to get an actually locked room.
+        Revalidate it.
+        """
+        candidate_room = GameRoom.objects.for_valid_game_room().first()
+        if candidate_room:
+            locked_candidate_room = (
+                GameRoom.objects.select_for_update().filter(id=candidate_room.id, status=GameRoom.PENDING).first()
+            )
+            if locked_candidate_room and locked_candidate_room.players.count() < PLAYERS_REQUIRED:
+                return locked_candidate_room
+        return None
+
     def connect(self):
+        """
+        On connection, join existing pending game room with less than two players, or create a new one.
+        Pending game rooms always have at least one player.
+        To avoid race condition, locks the GameRoom and related objects with `.select_for_update()`.
+        Unfortunately, annotated objects cannot be locked, so `for_valid_game_room`, which uses them to find the
+        conforming room. The room is the re-fetched so to say, and revalidated.
+        """
         self.user = self.scope.get("user")
+        self.game_room = None
         if not self.user:
-            self.game_room = None
             self.close()
             return
 
@@ -36,36 +58,54 @@ class MatchmakingConsumer(WebsocketConsumer):
 
         # transaction.atomic and .select_for_update are needed to prevent possible race condition
         with transaction.atomic():
-            self.game_room = GameRoom.objects.select_for_update().for_valid_game_room().first()
-            if not self.game_room:
-                self.game_room = GameRoom.objects.create(players=[self.user.profile])
-                logger.info("[Matchmaking.connect]: game room {%s} was created", self.game_room)
-            else:
+            self.game_room = self._with_db_lock_find_valid_game_room()
+
+            if self.game_room:
                 GameRoomPlayer.objects.create(game_room=self.game_room, profile=self.user.profile)
-                self.game_room.players.add(self.user.profile)
                 logger.info(
                     "[Matchmaking.connect]: game room {%s} added profile {%s}",
                     self.game_room,
                     self.user.profile,
                 )
+            else:
+                self.game_room = GameRoom.objects.create()
+                GameRoomPlayer.objects.create(game_room=self.game_room, profile=self.user.profile)
+                logger.info("[Matchmaking.connect]: game room {%s} was created", self.game_room)
 
-            self.group_name = f"matchmaking_{self.game_room.id}"
-            async_to_sync(self.channel_layer.group_add)(self.group_name, self.channel_name)
+            self.matchmaking_group_name = f"matchmaking_{self.game_room.id}"
+            async_to_sync(self.channel_layer.group_add)(self.matchmaking_group_name, self.channel_name)
 
             if self.game_room.players.count() == PLAYERS_REQUIRED:
                 logger.info("[Matchmaking.connect]: game room {%s} both players were found")
                 self.game_room.close()
-                async_to_sync(self.channel_layer.group_send)(self.group_name, {"type": "matchmaking_players_found"})
+                async_to_sync(self.channel_layer.group_send)(
+                    self.matchmaking_group_name, {"type": "matchmaking_players_found"},
+                )
 
     def disconnect(self, code: int):
+        async_to_sync(self.channel_layer.group_discard)(self.matchmaking_group_name, self.channel_name)
         if not self.game_room:
             return
 
-        self.game_room.players.remove(self.user.profile)
-        logger.info("[Matchmaking.disconnect]: game room {%s} removed player {%s}", self.game_room, self.user.profile)
-        if self.game_room.players.count() < 1:
-            self.game_room.close()
-            logger.info("[Matchmaking.disconnect]: game room {%s} closed", self.game_room)
+        normal_close_code = 1000
+        if code == normal_close_code:
+            logger.info(
+                "[Matchmaking.disconnect]: players were found for game room {%s}, connection is closed normally",
+                self.game_room,
+            )
+            return
+
+        with transaction.atomic():
+            room_to_clean = GameRoom.objects.select_for_update().filter(id=self.game_room.id).first()
+            room_to_clean.players.remove(self.user.profile)
+            logger.info(
+                "[Matchmaking.disconnect]: game room {%s} removed player {%s}",
+                room_to_clean,
+                self.user.profile,
+            )
+            if self.game_room.players.count() < 1:
+                room_to_clean.close()
+                logger.info("[Matchmaking.disconnect]: game room {%s} closed", room_to_clean)
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
@@ -80,7 +120,8 @@ class MatchmakingConsumer(WebsocketConsumer):
                 self.close()
 
     def matchmaking_players_found(self, event):
-        opponent = self.game_room.players.exclude(user=self.user.profile).first()
+        opponent = self.game_room.players.exclude(user=self.user).first()
+        logger.info("[Matchmaking.players_found]: {%s} vs {%s}", self.user.profile, opponent)
         self.game_room.status = GameRoom.ONGOING
         self.game_room.save()
         self.send(
@@ -94,3 +135,4 @@ class MatchmakingConsumer(WebsocketConsumer):
                 },
             ),
         )
+        self.close(1000)
