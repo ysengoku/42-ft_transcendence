c76e45a01d1026130b1abd52095e8470993afbbb
emuminov <jseldardw@gmail.com>
2025-05-04 17:13:10 +0200

    fix: fixed import issues related to file renaming

--- server/pong/consumers/__init__.py
+++ server/pong/consumers/__init__.py
@@ -1,5 +1,5 @@
-from .game import GameConsumer
 from .game_room import GameRoomConsumer
+from .game_worker import GameConsumer
 from .matchmaking import MatchmakingConsumer
 
 __all__ = ["GameConsumer", "GameRoomConsumer", "MatchmakingConsumer"]
