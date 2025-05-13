919240140d4b4902553bbb9e50e0131b5385c1b7
yusengok <ysengoku.tourisme@gmail.com>
2025-05-13 15:03:44 +0200

    feat: add tournament page and lobby status content

--- front/app/src/js/components/pages/tournament/components/index.js
+++ front/app/src/js/components/pages/tournament/components/index.js
@@ -1,5 +1,19 @@
 import { TournamentList } from './TournamentList';
 import { TournamentCreation } from './TournamentCreation';
 import { TournamentRegistration } from './TournamentRegistration';
+import { TournamentWaiting } from './TournamentWaiting';
+import { TournamentRoundStart } from './TournamentRoundStart';
+import { TournamentRoundWaiting } from './TournamentRoundWaiting';
+import { TournamentRoundFinished } from './TournamentRoundFinished';
+import { TournamentCanceled } from './TournamentCanceled';
 
-export { TournamentList, TournamentCreation, TournamentRegistration };
+export {
+	TournamentList,
+	TournamentCreation,
+	TournamentRegistration,
+	TournamentWaiting,
+	TournamentRoundStart,
+	TournamentRoundWaiting,
+	TournamentRoundFinished,
+	TournamentCanceled,
+};
