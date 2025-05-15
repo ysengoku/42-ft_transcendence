b559aadea1425aefd419b020876c9d28153a5c5a
melodie <melodie.bernard123@yahoo.fr>
2025-05-15 18:02:05 +0200

    minor: trying to fix the tournament, added something to count the rounds in signals.py, need to test this

--- server/tournaments/schemas.py
+++ server/tournaments/schemas.py
@@ -1,16 +1,17 @@
 from datetime import datetime
 from uuid import UUID
 
-from ninja import Schema
+from ninja import ModelSchema, Schema
 
 from common.schemas import ProfileMinimalSchema
+from tournaments.models import Tournament
 
 
 class RoundSchema(Schema):
-    tournament: 'TournamentSchema'
+    tournament: "TournamentSchema"
     tournament_id: UUID
     number: int
-    brackets: list['BracketSchema']
+    brackets: list["BracketSchema"]
     status: str
 
 
@@ -33,16 +34,32 @@ class BracketSchema(Schema):
     score_p2: int
 
 
-class TournamentSchema(Schema):
-    id: UUID
-    name: str
-    status: str
+class TournamentSchema(ModelSchema):
     creator: ProfileMinimalSchema
     winner: ParticipantSchema | None
-    date: datetime
     rounds: list[RoundSchema]
     participants: list[ParticipantSchema]
-    required_participants: int
+
+    class Config:
+        model = Tournament
+        model_fields = ["id", "name", "status",
+                        "date", "required_participants"]
+
+    @staticmethod
+    def resolve_creator(obj: Tournament):
+        return obj.creator.profile.to_profile_minimal_schema()
+
+    @staticmethod
+    def resolve_rounds(obj: Tournament):
+        return obj.rounds.all().prefetch_related("brackets")
+
+    @staticmethod
+    def resolve_participants(obj: Tournament):
+        return obj.participants.all().select_related("user__profile")
+
+    @staticmethod
+    def resolve_winner(obj: Tournament):
+        return obj.winner
 
 
 TournamentSchema.update_forward_refs()
