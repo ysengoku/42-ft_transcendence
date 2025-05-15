05889e9c0add90115551647d73cacdc1d067b68f
yusengok <ysengoku.tourisme@gmail.com>
2025-05-15 16:11:59 +0200

    feat: create tounament overview (still on working)

--- front/app/__mock__/functions/mockTournamentDetail.js
+++ front/app/__mock__/functions/mockTournamentDetail.js
@@ -59,7 +59,7 @@ export async function mockTournamentDetail(id) {
                   is_online: false
                 },
                 alias: 'MidnightRider',
-                status: 'won',
+                status: 'winner',
               },
               participant2: {
                 user: {
@@ -76,12 +76,12 @@ export async function mockTournamentDetail(id) {
                 user: {
                   username: 'user1',
                   nickname: 'MidnightRider',
-                  avatar: '/__mock__/img/sample-pic3.png',
+                  avatar: '/__mock__/img/sample-pic2.png',
                   elo: 1500,
                   is_online: false
                 },
                 alias: 'MidnightRider',
-                status: 'won',
+                status: 'winner',
               },
               round: 1,
               status: 'finished',
@@ -104,7 +104,7 @@ export async function mockTournamentDetail(id) {
                 user: {
                   username: 'user4',
                   nickname: 'Player Four',
-                  avatar: '/__mock__/img/sample-pic3.png',
+                  avatar: '/__mock__/img/sample-pic2.png',
                   elo: 1300,
                   is_online: false
                 },
@@ -127,7 +127,7 @@ export async function mockTournamentDetail(id) {
                   is_online: true
                 },
                 alias: 'DirtyHarry',
-                status: 'won',
+                status: 'winner',
               },
               participant2: {
                 user: {
@@ -166,13 +166,13 @@ export async function mockTournamentDetail(id) {
                   is_online: true,
                 },
                 alias: 'Gunslinger',
-                status: 'won',
+                status: 'winner',
               },
               participant2: {
                 user: {
                   username: 'user8',
                   nickname: 'Player Eight',
-                  avatar: '/__mock__/img/sample-pic3.png',
+                  avatar: '/__mock__/img/sample-pic2.png',
                   elo: 1100,
                   is_online: false,
                 },
@@ -202,13 +202,13 @@ export async function mockTournamentDetail(id) {
           alias: 'MidnightRider', status: 'active', round: 2 },
         { user: { username: 'user2', nickname: 'Player Two', avatar: '/__mock__/img/sample-pic3.png', elo: 1400, is_online: true },
           alias: 'Lone Cactus', status: 'eliminated', round: 1 },
-        { user: { username: 'user3', nickname: 'Player Three', avatar: '/__mock__/img/sample-pic3.png', elo: 1450, is_online: true },
+        { user: { username: 'user3', nickname: 'Player Three', avatar: '/__mock__/img/sample-pic2.png', elo: 1450, is_online: true },
           alias: 'LazyFox', status: 'active', round: 2 },
         { user: { username: 'user4', nickname: 'Player Four', avatar: '/__mock__/img/sample-pic3.png', elo: 1300, is_online: false },
           alias: 'DuskDevil', status: 'eliminated', round: 1 },
         { user: { username: 'user5', nickname: 'Player Five', avatar: '/__mock__/img/sample-pic3.png', elo: 1350, is_online: true },
           alias: 'DirtyHarry', status: 'active', round: 2 },
-        { user: { username: 'user6', nickname: 'Player Six', avatar: '/__mock__/img/sample-pic3.png', elo: 1200, is_online: true },
+        { user: { username: 'user6', nickname: 'Player Six', avatar: '/__mock__/img/sample-pic2.png', elo: 1200, is_online: true },
           alias: 'CactusJack', status: 'eliminated', round: 1 },
         { user: { username: 'user7', nickname: 'Player Seven', avatar: '/__mock__/img/sample-pic3.png', elo: 1250, is_online: true },
           alias: 'Gunslinger', status: 'active', round: 2 },
@@ -224,7 +224,7 @@ export async function mockTournamentDetail(id) {
       creator: {
         username: 'pedro',
         nickname: 'Pedro',
-        avatar: '/__mock__/img/sample-pic3.png',
+        avatar: '/__mock__/img/sample-pic2.png',
         elo: 2000,
         is_online: true
       },
@@ -237,7 +237,7 @@ export async function mockTournamentDetail(id) {
         is_online: false
         },
         alias: 'MidnightRider',
-        status: 'won',
+        status: 'winner',
         round: 3
       },
       date: '2025-05-09T11:43:42.045Z',
@@ -257,7 +257,7 @@ export async function mockTournamentDetail(id) {
                   is_online: false
                 },
                 alias: 'MidnightRider',
-                status: 'won',
+                status: 'winner',
               },
               participant2: {
                 user: {
@@ -279,7 +279,7 @@ export async function mockTournamentDetail(id) {
                   is_online: false
                 },
                 alias: 'MidnightRider',
-                status: 'won',
+                status: 'winner',
               },
               round: 1,
               status: 'finished',
@@ -292,12 +292,12 @@ export async function mockTournamentDetail(id) {
                 user: {
                   username: 'user3',
                   nickname: 'Player Three',
-                  avatar: '/__mock__/img/sample-pic3.png',
+                  avatar: '/__mock__/img/sample-pic2.png',
                   elo: 1450,
                   is_online: true
                 },
                 alias: 'LazyFox',
-                status: 'won',
+                status: 'winner',
               },
               participant2: {
                 user: {
@@ -314,12 +314,12 @@ export async function mockTournamentDetail(id) {
                 user: {
                   username: 'user3',
                   nickname: 'Player Three',
-                  avatar: '/__mock__/img/sample-pic3.png',
+                  avatar: '/__mock__/img/sample-pic2.png',
                   elo: 1450,
                   is_online: true
                 },
                 alias: 'LazyFox',
-                status: 'won',
+                status: 'winner',
               },
               round: 1,
               status: 'finished',
@@ -332,18 +332,18 @@ export async function mockTournamentDetail(id) {
                 user: {
                 username: 'user5',
                   nickname: 'Player Five',
-                  avatar: '/__mock__/img/sample-pic3.png',
+                  avatar: '/__mock__/img/sample-pic1.jpg',
                   elo: 1350,
                   is_online: true
                 },
                 alias: 'DirtyHarry',
-                status: 'won',
+                status: 'winner',
               },
               participant2: {
                 user: {
                   username: 'user6',
                   nickname: 'Player Six',
-                  avatar: '/__mock__/img/sample-pic3.png',
+                  avatar: '/__mock__/img/sample-pic2.png',
                   elo: 1200,
                   is_online: true
                 },
@@ -371,12 +371,12 @@ export async function mockTournamentDetail(id) {
                 user: {
                   username: 'user7',
                   nickname: 'Player Seven',
-                  avatar: '/__mock__/img/sample-pic3.png',
+                  avatar: '/__mock__/img/sample-pic2.png',
                   elo: 1250,
                   is_online: true,
                 },
                 alias: 'Gunslinger',
-                status: 'won',
+                status: 'winner',
               },
               participant2: {
                 user: {
@@ -393,7 +393,7 @@ export async function mockTournamentDetail(id) {
                 user: {
                   username: 'user7',
                   nickname: 'Player Seven',
-                  avatar: '/__mock__/img/sample-pic3.png',
+                  avatar: '/__mock__/img/sample-pic2.png',
                   elo: 1250,
                   is_online: true
                 },
@@ -421,13 +421,13 @@ export async function mockTournamentDetail(id) {
                   is_online: false
                 },
                 alias: 'MidnightRider',
-                status: 'won',
+                status: 'winner',
               },
               participant2: {
                 user: {
                   username: 'user3',
                   nickname: 'Player Three',
-                  avatar: '/__mock__/img/sample-pic3.png',
+                  avatar: '/__mock__/img/sample-pic2.png',
                   elo: 1450,
                   is_online: true
                 },
@@ -443,7 +443,7 @@ export async function mockTournamentDetail(id) {
                   is_online: false
                 },
                 alias: 'MidnightRider',
-                status: 'won',
+                status: 'winner',
               },
               round: 2,
               status: 'finished',
@@ -461,13 +461,13 @@ export async function mockTournamentDetail(id) {
                   is_online: true,
                 },
                 alias: 'DirtyHarry',
-                status: 'won',
+                status: 'winner',
               },
               participant2: {
                 user: {
                   username: 'user7',
                   nickname: 'Player Seven',
-                  avatar: '/__mock__/img/sample-pic3.png',
+                  avatar: '/__mock__/img/sample-pic1.jpg',
                   elo: 1250,
                   is_online: true,
                 },
@@ -478,12 +478,12 @@ export async function mockTournamentDetail(id) {
                 user: {
                   username: 'user5',
                   nickname: 'Player Five',
-                  avatar: '/__mock__/img/sample-pic3.png',
+                  avatar: '/__mock__/img/sample-pic2.png',
                   elo: 1350,
                   is_online: true,
                 },
                 alias: 'DirtyHarry',
-                status: 'won',
+                status: 'winner',
               },
               round: 2,
               status: 'finished',
@@ -507,7 +507,7 @@ export async function mockTournamentDetail(id) {
                   is_online: false,
                 },
                 alias: 'MidnightRider',
-                status: 'won',
+                status: 'winner',
             },
             participant2: {
               user: {
@@ -524,12 +524,12 @@ export async function mockTournamentDetail(id) {
               user: {
                 username: 'user1',
                 nickname: 'MidnightRider',
-                avatar: '/__mock__/img/sample-pic3.png',
+                avatar: '/__mock__/img/sample-pic2.png',
                 elo: 1500,
                 is_online: false,
               },
               alias: 'MidnightRider',
-              status: 'won',
+              status: 'winner',
             },
             round: 3,
             status: 'finished',
