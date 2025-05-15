9eda4155fcf4fd1a0be353e1c1bd7fd8c4646388
melodie <melodie.bernard123@yahoo.fr>
2025-05-15 22:59:04 +0200

    minor: working on tournaments, trying to get the get_tournament by id to work but it doesnt

--- server/tournaments/schemas.py
+++ server/tournaments/schemas.py
@@ -4,41 +4,51 @@ from uuid import UUID
 from ninja import ModelSchema, Schema
 
 from common.schemas import ProfileMinimalSchema
-from tournaments.models import Tournament
+from tournaments.models import Bracket, Participant, Round, Tournament
 
 
-class RoundSchema(Schema):
-    tournament: "TournamentSchema"
-    tournament_id: UUID
-    number: int
+class RoundSchema(ModelSchema):
+    # tournament: "TournamentSchema"
+    # tournament_id: UUID
+    # number: int
     brackets: list["BracketSchema"]
-    status: str
+
+    class Config:
+        model = Round
+        model_fields = ["number", "status"]
+
+    # status: str
 
 
-class ParticipantSchema(Schema):
-    tournament_id: UUID
+class ParticipantSchema(ModelSchema):
+    # tournament_id: UUID
     user: ProfileMinimalSchema
-    alias: str
-    status: str
-    round: int
+
+    class Config:
+        model = Participant
+        model_fields = ["alias", "status", "current_round"]
+
+    # alias: str
+    # status:
+    #     str
+    # round: int
 
 
-class BracketSchema(Schema):
+class BracketSchema(ModelSchema):
     game_id: UUID
     participant1: ParticipantSchema
     participant2: ParticipantSchema
     winner: ParticipantSchema | None
-    round: int
-    status: str
-    score_p1: int
-    score_p2: int
+
+    class Config:
+        model = Bracket
+        model_fields = ["round", "status", "score_p1", "score_p2"]
 
 
 class TournamentSchema(ModelSchema):
     creator: ProfileMinimalSchema
-    winner: ParticipantSchema | None
-    rounds: list[RoundSchema]
-    participants: list[ParticipantSchema]
+    participants: list["ParticipantSchema"]
+    rounds: list["RoundSchema"]
 
     class Config:
         model = Tournament
@@ -46,21 +56,26 @@ class TournamentSchema(ModelSchema):
                         "date", "required_participants"]
 
     @staticmethod
-    def resolve_creator(obj: Tournament):
-        return obj.creator.profile.to_profile_minimal_schema()
+    def resolve_creator(obj: Tournament) -> dict:
+        return ProfileMinimalSchema.from_orm(obj.creator.profile.user).dict()
 
     @staticmethod
-    def resolve_rounds(obj: Tournament):
-        return obj.rounds.all().prefetch_related("brackets")
+    def resolve_rounds(obj: Tournament) -> list:
+        return [
+            RoundSchema.from_orm(r).dict()
+            for r in obj.rounds.all().prefetch_related("brackets")
+        ]
 
     @staticmethod
-    def resolve_participants(obj: Tournament):
-        return obj.participants.all().select_related("user__profile")
+    def resolve_participants(obj: Tournament) -> list:
+        return [
+            ParticipantSchema.from_orm(p).dict()
+            for p in obj.participants.all().select_related("user")
+        ]
 
     @staticmethod
     def resolve_winner(obj: Tournament):
         return obj.winner
 
-
-TournamentSchema.update_forward_refs()
-RoundSchema.update_forward_refs()
+        # TournamentSchema.update_forward_refs()
+        # RoundSchema.update_forward_refs()
