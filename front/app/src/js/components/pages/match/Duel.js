709c8de0a2908d2c5bde8f8a1ea78e292cdaad5c
Melodie Bernard (from school) <melodie.bernard123@yahoo.fr>
2025-05-27 16:08:58 +0200

    Revert "Merge remote-tracking branch 'origin/dev-front' into game_invitation"
    This reverts commit 92df66a58e652e4ce83f27f3e69a39b2509cdc1c, reversing
    changes made to 127ac75fb347919e6e28a70d1b5350aa1da966b8.

--- front/app/src/js/components/pages/match/Duel.js
+++ front/app/src/js/components/pages/match/Duel.js
@@ -114,8 +114,6 @@ export class Duel extends HTMLElement {
     } else {
       this.animation.classList.add('d-none');
       this.timer.classList.add('d-none');
-      router.removeBeforeunloadCallback();
-      window.removeEventListener('beforeunload', this.confirmLeavePage);
     }
     if (this.#state.status === 'inviting') {
       this.cancelButton?.addEventListener('click', this.cancelInvitation);
@@ -169,8 +167,8 @@ export class Duel extends HTMLElement {
     if (data.username !== this.#state.opponent.username) {
       return;
     }
-    // router.removeBeforeunloadCallback();
-    // window.removeEventListener('beforeunload', this.confirmLeavePage);
+    router.removeBeforeunloadCallback();
+    window.removeEventListener('beforeunload', this.confirmLeavePage);
     this.#state.status = 'declined';
     this.renderContent();
   }
@@ -179,8 +177,8 @@ export class Duel extends HTMLElement {
     devLog('Canceling game invitation');
     const message = { action: 'cancel_game_invite', data: { username: this.#state.opponent.username } };
     socketManager.sendMessage('livechat', message);
-    // router.removeBeforeunloadCallback();
-    // window.removeEventListener('beforeunload', this.confirmLeavePage);
+    router.removeBeforeunloadCallback();
+    window.removeEventListener('beforeunload', this.confirmLeavePage);
     this.#state.status = 'canceled';
     this.renderContent();
   }
