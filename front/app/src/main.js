3d6a2e93c0c95c571eab02b9a79d5df5d69cd9a1
yusengok <ysengoku.tourisme@gmail.com>
2025-05-27 11:20:15 +0200

    enhance: update game_id and tournament_id check on butonn click in Home

--- front/app/src/main.js
+++ front/app/src/main.js
@@ -14,11 +14,13 @@ document.documentElement.setAttribute('data-bs-theme', theme);
 const observer = new MutationObserver(() => {
   document.documentElement.getAttribute('data-bs-theme') === 'light' ? (
     document.getElementById('stars') ? document.body.removeChild(stars) : null,
-    document.body.style.backgroundImage = `linear-gradient(rgba(170,79,236, 0.8) 0%, rgba(236,79,84, 0.8) 50%, rgba(236,79,84, 0.8) 100%)`,
+    document.body.style.backgroundImage =
+      'linear-gradient(rgba(170,79,236, 0.8) 0%, rgba(236,79,84, 0.8) 50%, rgba(236,79,84, 0.8) 100%)',
     createClouds()
   ) : (
     document.getElementById('cloud') ? document.body.removeChild(cloud) : null,
-    document.body.style.backgroundImage = `linear-gradient(rgb(23, 18, 40) 0%, rgb(62, 52, 97) 16%, rgb(95, 83, 138) 40%, #6670A2 100%)`,
+    document.body.style.backgroundImage =
+      'linear-gradient(rgb(23, 18, 40) 0%, rgb(62, 52, 97) 16%, rgb(95, 83, 138) 40%, #6670A2 100%)',
     createStars()
   );
 });
