623d0ead2122984e3a8e4845641fc91bafe4c341
ysengoku <ysengoku.tourisme@gmail.com>
2025-05-30 23:11:24 +0200

    feat: add game instruction to duel menu and local game menu

--- front/app/src/js/components/pages/match/components/index.js
+++ front/app/src/js/components/pages/match/components/index.js
@@ -1,5 +1,6 @@
 import { GameOptions } from './GameOptions';
 import { DuelPreview } from './DuelPreview';
 import { ConfirmationModal } from './ConfirmationModal';
+import { GameInstruction } from './GameInstruction';
 
-export { GameOptions, DuelPreview, ConfirmationModal };
+export { GameOptions, DuelPreview, ConfirmationModal, GameInstruction };
