c923ad01706fc1f4b8195f7f4b259c12583fae45
yusengok <ysengoku.tourisme@gmail.com>
2025-05-30 09:38:38 +0200

    test: update prettier and eslint --fix config

--- front/app/eslint.config.js
+++ front/app/eslint.config.js
@@ -1,9 +1,21 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import js from '@eslint/js';
 import google from 'eslint-config-google';
+import prettierPlugin from 'eslint-plugin-prettier';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const filteredGoogleRules = { ...google.rules };
 delete filteredGoogleRules['valid-jsdoc'];
 delete filteredGoogleRules['require-jsdoc'];
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: 'eslint:recommended',
+});
 
 export default [
   {
@@ -18,16 +30,21 @@ export default [
         CustomEvent: 'readonly',
       },
     },
+    plugins: {
+      prettier: prettierPlugin,
+    },
     rules: {
       ...filteredGoogleRules,
       'max-len': ['error', { code: 120, ignoreTemplateLiterals: true }],
       'no-tabs': 'off',
-      'semi': ['error', 'always'],
+      semi: ['error', 'always'],
       'object-curly-spacing': ['error', 'always'],
-	    'quotes': ['error', 'single'],
-	    'indent': ['error', 2],
-      'camelcase': ['error', { properties: 'never' }],
+      quotes: ['error', 'single'],
+      indent: ['error', 2],
+      camelcase: ['error', { properties: 'never' }],
       'no-undef': 'off',
+      'prettier/prettier': 'error',
     },
   },
+  ...compat.extends('prettier'),
 ];
