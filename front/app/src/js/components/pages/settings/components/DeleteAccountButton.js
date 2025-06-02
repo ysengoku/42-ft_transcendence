8820acea84f41ee26ed2c41a4b9cc6a5d8d084b6
yusengok <ysengoku.tourisme@gmail.com>
2025-06-02 12:13:29 +0200

    fix: replace router.navigate by redirect

--- front/app/src/js/components/pages/settings/components/DeleteAccountButton.js
+++ front/app/src/js/components/pages/settings/components/DeleteAccountButton.js
@@ -46,7 +46,7 @@ export class DeleteAccountButton extends HTMLElement {
     /* eslint-disable-next-line new-cap */
     const response = await apiRequest('DELETE', API_ENDPOINTS.USER_DELETE(this.#state.username), null, false, true);
     if (response.success) {
-      router.navigate('/account-deleted');
+      router.redirect('/account-deleted');
     } else if (response.status !== 401) {
       console.error(response.msg);
       showAlertMessage(ALERT_TYPE.ERROR, response.msg);
