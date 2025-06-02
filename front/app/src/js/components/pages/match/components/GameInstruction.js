43ac84625c70fb2527a25bc600fe709ff0e475ce
yusengok <ysengoku.tourisme@gmail.com>
2025-06-02 12:26:13 +0200

    style: update colors for game instructions

--- front/app/src/js/components/pages/match/components/GameInstruction.js
+++ front/app/src/js/components/pages/match/components/GameInstruction.js
@@ -14,7 +14,7 @@ export class GameInstruction extends HTMLElement {
   template() {
     return `
     <div class="ps-4 pe-3 py-4" id="instruction-wrapper">
-      <h6 class="fs-5 fw-bolder m-0 mb-2">How to play?</h6>
+      <h6 class="fs-5 m-0 mb-2">How to play?</h6>
       <div>
         Hit the ball past your opponent to score.<br>
         Move your paddle with <kbd>W</kbd> <kbd>A</kbd> <kbd>S</kbd> <kbd>D</kbd> or&nbsp;
@@ -31,15 +31,15 @@ export class GameInstruction extends HTMLElement {
     return `
     <style>
     #instruction-wrapper {
-      background-color: rgba(var(--bs-body-bg-rgb), 0.5);
-      border-radius: 0.2rem;
+      background-color: rgba(var(--bs-body-bg-rgb), 0.3);
       color: rgba(var(--bs-body-color-rgb), 0.8);
+      border-radius: 0.2rem;
     }
     kbd {
       display: inline-block;
       border-radius: 0.2rem;
       background-color: rgba(var(--bs-body-color-rgb), 0.8);
-      color: rgba(var(--bs-body-bg-rgb), 0.8);
+      color: rgba(var(--bs-body-bg-rgb), 0.6);
       font-family: "Helvetica Neue", sans-serif;
       font-weight: 600;
       font-size: .5rem;
