1bea32e56f78b9f71081b527858b84e18bcf3b30
ysengoku <ysengoku.tourisme@gmail.com>
2025-06-08 22:47:58 +0200

    fix: fix rendering issue for 'tournament starting' and 'waiting for next round' status, add mock data function for test

---
+++ front/app/src/js/components/pages/tournament/tournamentStatus.js
@@ -0,0 +1,34 @@
+export const UI_STATUS = {
+  PENDING: 'pending',
+  ROUND_STARTING: 'roundStarting',
+  BRACKET_ONGOING: 'bracketOngoing',
+  WAITING_NEXT_ROUND: 'waitingNextRound',
+  ROUND_FINISHED: 'roundFinished',
+  FINISHED: 'finished',
+};
+
+export const TOURNAMENT_STATUS = {
+  PENDING: 'pending',
+  ONGOING: 'ongoing',
+  FINISHED: 'finished',
+  CANCELED: 'canceled',
+};
+
+export const ROUND_STATUS = {
+  STARTING: 'starting',
+  ONGOING: 'ongoing',
+  FINISHED: 'finished',
+};
+
+export const BRACKET_STATUS = {
+  STARTING: 'starting',
+  ONGOING: 'ongoing',
+  FINISHED: 'finished',
+};
+
+export const PARTICIPANT_STATUS = {
+  STARTING: 'starting',
+  QUALIFIED: 'qualified',
+  ELIMINATED: 'eliminated',
+  WINNER: 'winner',
+};
