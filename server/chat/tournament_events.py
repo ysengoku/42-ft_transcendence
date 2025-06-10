c78dae949b6849cc25bdf22f732d9d0c70a7586b
Melodie Bernard (from school) <melodie.bernard123@yahoo.fr>
2025-06-10 17:24:04 +0200

    feat: invitation for tournament is sent to every one except the creator. Must verify if the function that put the notification to close works and put a query in notification to prevent from showing closed tournaments invitation

---
+++ server/chat/tournament_events.py
@@ -0,0 +1,126 @@
+import json
+import logging
+from datetime import datetime
+
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+
+from tournaments.models import Tournament
+from users.models import Profile
+
+from .chat_utils import ChatUtils
+from .models import Notification, TournamentInvitation
+
+logger = logging.getLogger("server")
+
+
+class TournamentEvent:
+    def __init__(self, consumer):
+        self.consumer = consumer
+
+    @classmethod
+    def send_tournament_notification(cls, tournament_id, alias):
+        tournament_id = tournament_id
+        # TODO:Verify tournament_id (is it UUID)
+        if tournament_id is None:
+            logger.warning("Wrong tournament_id send by %s", self.consumer.user.username)
+            self.close()
+            return
+        try:
+            tournament = Tournament.objects.get(id=tournament_id)
+            tournament_name = tournament.name
+            tournament_creator = tournament.creator
+        except Tournament.DoesNotExist:
+            logger.warning("This tournament does not exist")
+            return
+
+        all_profiles = Profile.objects.exclude(pk=tournament_creator.pk)
+ 
+        channel_layer = get_channel_layer()
+
+        for profile in all_profiles:
+            invitation = TournamentInvitation.objects.create(
+                sender=tournament_creator,
+                recipient=profile,
+                tournament_id = str(tournament_id),
+                tournament_name=tournament_name,
+                alias = alias,
+                status=TournamentInvitation.OPEN,
+            )
+            tournament_creator.refresh_from_db()
+            notification = Notification.objects.action_send_tournament_invite(
+                receiver=profile,
+                sender=tournament_creator,
+                notification_data={
+                    "tournament_id": str(tournament_id),
+                    "tournament_name": tournament_name,
+                    "alias": alias,
+                    "invitation_id": str(invitation.id),
+                },
+            )
+            notification_data = notification.data.copy()
+            logger.warning(notification_data)
+            if "date" in notification_data and isinstance(notification_data["date"], datetime):
+                notification_data["date"] = notification_data["date"].isoformat()
+            async_to_sync(channel_layer.group_send)(
+                f"user_{profile.user.id}",
+                {
+                    "type": "chat_message",
+                    "message": json.dumps(
+                        {
+                            "action": "tournament_invite",
+                            "data": notification_data,
+                        },
+                    ),
+                },
+            )
+
+    @classmethod
+    def close_tournament_invitations(cls, tournament_id):
+        TournamentInvitation.objects.filter(
+            tournament_id=tournament_id,
+            status=TournamentInvitation.OPEN,
+        ).update(status=TournamentInvitation.CLOSED)
+
+        notifications = Notification.objects.filter(
+            data__tournament_id=str(tournament.id),
+            action=Notification.NEW_TOURNAMENT,
+        )
+
+        channel_layer = get_channel_layer()
+        for notif in notifications:
+            notif.data["status"] = TournamentInvitation.CLOSED
+            notif.save(update_fields=["data"])
+
+            async_to_sync(channel_layer.group_send)(
+                f"user_{notif.receiver.user.id}",
+                {
+                    "type": "chat_message",
+                    "message": json.dumps({
+                        "action": "tournament_invitation_closed",
+                        "data": notif.data | {"id": str(notif.id)},
+                    }),
+                },
+            )
+
+    def handle_new_tournament(self, data):
+        tournament_id = data["data"].get["tournament_id"]
+        tournament_name = data["data"].get["tournament_name"]
+        organizer_id = data["data"].get["organizer_id"]
+
+        organizer = Profile.objects.get(id=organizer_id)
+
+        # send notification to concerned users
+        notification_data = ChatUtils.get_user_data(organizer)
+        notification_data.update(
+            {"id": tournament_id, "tournament_name": tournament_name},
+        )
+
+        self.consumer.send(
+            text_data=json.dumps(
+                {
+                    "action": "new_tournament",
+                    "data": notification_data,
+                },
+            ),
+        )
