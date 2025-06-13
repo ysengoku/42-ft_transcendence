d40e60b1d79f94b48280d9058ad70ddd31cf8c3d
yusengok <ysengoku.tourisme@gmail.com>
2025-06-13 10:27:49 +0200

    feat: handle matchmaking ws closing with 3003, update game_invite notification rendering

--- front/app/src/js/components/navbar/components/NotificationsListItem.js
+++ front/app/src/js/components/navbar/components/NotificationsListItem.js
@@ -48,7 +48,8 @@ export class NotificationsListItem extends HTMLElement {
   render() {
     this.innerHTML = this.template();
 
-    this.querySelector('.notifications-list-avatar').src = this.#state.data.avatar;
+    const avatar = this.querySelector('.notifications-list-avatar');
+    avatar.src = this.#state.data.avatar;
     this.querySelector('.notification-time').textContent = getRelativeTime(this.#state.data.date);
 
     this.content = this.querySelector('.notification-content');
@@ -70,7 +71,8 @@ export class NotificationsListItem extends HTMLElement {
           this.buttonWrapper.appendChild(this.acceptButton);
           this.buttonWrapper.appendChild(this.declineButton);
         } else {
-          this.content.textContent = this.message.pendingGameInvitation(this.#state.data.nickname);
+          avatar.src = this.#state.data.invitee.avatar;
+          this.content.textContent = this.message.pendingGameInvitation(this.#state.data.invitee.nickname);
           this.cancelInviteButton = document.createElement('button');
           this.cancelInviteButton.textContent = 'Cancel';
           this.cancelInviteButton.addEventListener('click', this.cancelDuelInvitation);
