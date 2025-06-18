23c91739d63a9a03c8f0062a491bab3cbc7aa8f8
Melodie Bernard (from school) <melodie.bernard123@yahoo.fr>
2025-06-18 14:39:44 +0200

    fix: missing :

--- server/tournaments/consumers.py
+++ server/tournaments/consumers.py
@@ -293,7 +293,7 @@ class TournamentConsumer(WebsocketConsumer):
         if bracket.winner.alias == p1_alias:
             bracket.score_p1 = bracket.winners_score
             bracket.score_p2 = bracket.losers_score
-        else
+        else:
             bracket.score_p1 = bracket.losers_score
             bracket.score_p2 = bracket.winners_score
         bracket.save(update_fields=["score_p1", "score_p2"])
