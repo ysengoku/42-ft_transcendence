f03c24903cd629bf72edb33f93238d645ece4f8c
emuminov <jseldardw@gmail.com>
2025-06-21 17:56:23 +0200

    feat: tournament_id from the SelfSchema does not count users eliminated from a tournament anymore refactor: moved SelfSchema resolvers to the self endpoint

--- server/users/tests/test_profile_model.py
+++ server/users/tests/test_profile_model.py
@@ -13,33 +13,32 @@ class ProfileModelTests(TestCase):
     def setUp(self):
         self.user: User = User.objects.create_user("TestUser", email="user0@gmail.com", password="123")
 
-    def test_can_participate_in_game_when_not_active_in_games(self):
+    def test_get_active_game_participation_when_not_active_in_games(self):
         self.assertEqual(
-            self.user.profile.can_participate_in_game(),
+            all(x is None for x in self.user.profile.get_active_game_participation()),
             True,
             "User should be able to participate in games when they are not active in matchmaking, tournaments or "
             "invitations",
         )
 
-    def test_can_participate_in_game_when_active_in_matchmaking(self):
+    def test_get_active_game_participation_when_active_in_matchmaking(self):
         game_room: GameRoom = GameRoom.objects.create(settings=get_default_game_room_settings())
         GameRoomPlayer.objects.create(
             game_room=game_room,
             profile=self.user.profile,
         )
-        self.assertEqual(
-            self.user.profile.can_participate_in_game(),
-            False,
+        print(next((x for x in self.user.profile.get_active_game_participation() if isinstance(x, GameRoom)), None))
+        self.assertIsNotNone(
+            next((x for x in self.user.profile.get_active_game_participation() if isinstance(x, GameRoom)), None),
             "User should not be able to participate in games when they are already active in matchmaking",
         )
         game_room.close()
-        self.assertEqual(
-            self.user.profile.can_participate_in_game(),
-            True,
+        self.assertIsNone(
+            next((x for x in self.user.profile.get_active_game_participation() if isinstance(x, GameRoom)), None),
             "User should be able to participate in games after they finished the game",
         )
 
-    def test_can_participate_in_game_when_active_in_tournament(self):
+    def test_get_active_game_participation_when_active_in_tournament(self):
         tournament: Tournament = Tournament.objects.validate_and_create(
             tournament_name="foobar",
             creator=self.user.profile,
@@ -47,16 +46,14 @@ class ProfileModelTests(TestCase):
             alias="barfoo",
             settings=get_default_game_room_settings(),
         )
-        self.assertEqual(
-            self.user.profile.can_participate_in_game(),
-            False,
+        self.assertIsNotNone(
+            next((x for x in self.user.profile.get_active_game_participation() if isinstance(x, Tournament)), None),
             "User should not be able to participate in games when they are already active in a tournament",
         )
         tournament.status = Tournament.FINISHED
         tournament.save()
-        self.assertEqual(
-            self.user.profile.can_participate_in_game(),
-            True,
+        self.assertIsNone(
+            next((x for x in self.user.profile.get_active_game_participation() if isinstance(x, Tournament)), None),
             "User should be able to participate in games after finishing a tournament",
         )
 
@@ -67,24 +64,21 @@ class ProfileModelTests(TestCase):
             recipient=user2.profile,
             options=get_default_game_room_settings(),
         )
-        self.assertEqual(
-            self.user.profile.can_participate_in_game(),
-            False,
+        self.assertIsNotNone(
+            next((x for x in self.user.profile.get_active_game_participation() if isinstance(x, GameInvitation)), None),
             "User should not be able to participate in games after they invited someone",
         )
 
         game_invitation.status = GameInvitation.CANCELLED
         game_invitation.save()
-        self.assertEqual(
-            self.user.profile.can_participate_in_game(),
-            True,
+        self.assertIsNone(
+            next((x for x in self.user.profile.get_active_game_participation() if isinstance(x, GameInvitation)), None),
             "User should be able to participate in games after cancelling invite",
         )
 
         game_invitation.status = GameInvitation.ACCEPTED
         game_invitation.save()
-        self.assertEqual(
-            self.user.profile.can_participate_in_game(),
-            True,
+        self.assertIsNone(
+            next((x for x in self.user.profile.get_active_game_participation() if isinstance(x, GameInvitation)), None),
             "User should be able to participate in games after their invite was accepted",
         )
