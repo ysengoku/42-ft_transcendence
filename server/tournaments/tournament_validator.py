a68486b97776ba7361e7b85d3ca4fed3465e4797
Melodie Bernard (from school) <melodie.bernard123@yahoo.fr>
2025-06-24 14:17:03 +0200

    feat: tournament can be played towards the end and display results in finished tournaments. To do : close connexion from eliminated participants

--- server/tournaments/tournament_validator.py
+++ server/tournaments/tournament_validator.py
@@ -10,11 +10,9 @@ class Validator:
         required_fields = {
             "new_registration": ["alias", "avatar"],
             "last_registration": ["alias", "avatar"],
+            "user_left": ["alias"],
             "tournament_message": ["alias", "avatar"],
-            "start_round": ["id"],  #
-            "round_end": ["id"],  #
-            "tournament_game_finished": ["id"],  # the bracket id
-            "match_result": ["id", "chat_id"],
+            "tournament_game_finished": ["bracket_id"],
         }
 
         if action in required_fields:
@@ -32,9 +30,9 @@ class Validator:
         expected_types = {
             "new_registration": {"alias": str, "avatar": str},
             "last_registration": {"alias": str, "avatar": str},
+            "user_left": {"alias": str},
             "tournament_message": {"alias": str, "avatar": str},
-            "start_round": {"round_number": int},
-            "match_result": {"round_number": int, "result": int, "tournament_id": str},
+            "tournament_game_finished": {"bracket_id": str},
         }
 
         uuid_fields = {
@@ -52,76 +50,4 @@ class Validator:
                         type(value).__name__,
                     )
                     return False
-
-        # UUID verification
-        if action in uuid_fields:
-            for field in uuid_fields[action]:
-                value = data.get(field)
-                if value and not Validator.is_valid_uuid(value):
-                    logger.warning("Invalid UUID format for '%s', the value is %s", field, value)
-                    return False
-
         return True
-
-    @staticmethod
-    def is_valid_uuid(value) -> bool:
-        try:
-            UUID(str(value))
-            return True
-        except ValueError:
-            return False
-
-    @staticmethod
-    def check_str_option(options, option, dict_options) -> bool:
-        if option is None:
-            return True
-        if not isinstance(option, str) or option not in dict_options:
-            logger.warning("%s must be one of %s and currently is %s", options, dict_options, option)
-            return False
-        return True
-
-    @staticmethod
-    def check_bool_option(options, option) -> bool:
-        if option is not None and not isinstance(option, bool):
-            logger.warning("%s must be a boolean", options)
-            return False
-        return True
-
-    @staticmethod
-    def check_int_option(options, option, val_min, val_max) -> bool:
-        if option is None:
-            return True
-        if not isinstance(option, int) or not (val_min <= option <= val_max):
-            logger.warning("%s must be an int between %d and %d", options, val_min, val_max)
-            return False
-        return True
-
-    @staticmethod
-    def validate_options(options) -> bool:
-        if not isinstance(options, dict):
-            logger.warning("Invalid type for 'options'")
-            return False
-        ranked = "ranked"
-        game_speed = "game_speed"
-        score_to_win = "score_to_win"
-        time_limit = "time_limit"
-        cool_mode = "cool_mode"
-        schema = {ranked, game_speed, score_to_win, time_limit, cool_mode}
-        for field in schema:
-            if field not in options:
-                continue
-            if options.get(field) is None:
-                logger.warning("Field [{%s}] is None for action game_invite", field)
-                return False
-
-        allowed_game_speeds = {"slow", "medium", "fast"}
-        min_score, max_score = 3, 20
-        min_time, max_time = 1, 5
-
-        return (
-            Validator.check_str_option(game_speed, options.get(game_speed), allowed_game_speeds)
-            and Validator.check_bool_option(ranked, options.get(ranked))
-            and Validator.check_bool_option(cool_mode, options.get(cool_mode))
-            and Validator.check_int_option(score_to_win, options.get(score_to_win), min_score, max_score)
-            and Validator.check_int_option(time_limit, options.get(time_limit), min_time, max_time)
-        )
