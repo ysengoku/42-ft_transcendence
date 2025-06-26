60516bf66edce141e5c7f7b747bc4f8cabdee5ab
yusengok <ysengoku.tourisme@gmail.com>
2025-06-26 13:46:44 +0200

    enhance: validate game options gotten from local storage and set default values if invalid, style: norm eslint

---
+++ front/app/src/js/components/pages/match/utils/gameOptions.js
@@ -0,0 +1,79 @@
+import { DEFAULT_GAME_OPTIONS } from '@env';
+
+const defaultOptionValue = {
+  score_to_win: DEFAULT_GAME_OPTIONS.score_to_win || 5,
+  game_speed: DEFAULT_GAME_OPTIONS.game_speed || 'medium',
+  time_limit: DEFAULT_GAME_OPTIONS.time_limit || 3,
+  ranked: DEFAULT_GAME_OPTIONS.ranked || false,
+  cool_mode: DEFAULT_GAME_OPTIONS.cool_mode || false,
+};
+
+const range = {
+  minScoreToWin: 3,
+  maxScoreToWin: 20,
+  minTimeLimit: 1,
+  maxTimeLimit: 5,
+};
+
+const gameOptionsSchema = {
+  cool_mode: { type: 'boolean' },
+  ranked: { type: 'boolean' },
+  game_speed: {
+    type: 'enum',
+    values: ['slow', 'medium', 'fast', 'any'],
+  },
+  score_to_win: {
+    type: 'number',
+    min: range.minScoreToWin,
+    max: range.maxScoreToWin,
+  },
+  time_limit: {
+    type: 'number',
+    min: range.minTimeLimit,
+    max: range.maxTimeLimit,
+  },
+};
+
+export function getOptionsFromLocalStorage() {
+  const storedOptions = localStorage.getItem('gameOptions');
+  let options = null;
+  if (storedOptions) {
+    try {
+      options = JSON.parse(storedOptions);
+    } catch {
+      console.error('Invalid format of Game options');
+      options = null;
+    }
+  }
+  if (!options) {
+    return null;
+  }
+  return validateStoredOptions(options);
+}
+
+function validateStoredOptions(options = {}) {
+  const validatedOtions = {};
+  for (const [key, rule] of Object.entries(gameOptionsSchema)) {
+    if (options[key] === undefined || options[key] === null) {
+      validatedOtions[key] = defaultOptionValue[key];
+    } else if (validateOption(options[key], rule)) {
+      validatedOtions[key] = options[key];
+    } else {
+      validatedOtions[key] = defaultOptionValue[key];
+    }
+  }
+  return validatedOtions;
+}
+
+function validateOption(raw, rule) {
+  switch (rule.type) {
+    case 'boolean':
+      return typeof raw === 'boolean' || raw === 'any';
+    case 'enum':
+      return rule.values.includes(raw);
+    case 'number':
+      return raw === 'any' || (typeof raw === 'number' && raw >= rule.min && raw <= rule.max);
+    default:
+      console.error(`Unknown validation rule type: ${rule.type}`);
+  }
+}
