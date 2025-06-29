1ab189a260c47f7ed7639973b32128d8501216e6
emuminov <jseldardw@gmail.com>
2025-06-29 19:49:38 +0200

    deps: removed silk as one of the dependencies

--- server/server/settings.py
+++ server/server/settings.py
@@ -154,7 +154,6 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "silk.middleware.SilkyMiddleware",
 ]
 
 ROOT_URLCONF = "server.urls"
