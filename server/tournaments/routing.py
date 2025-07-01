ab11a33dcf0d23ec98b3fe3a0b73fefb83aeb22f
Melodie Bernard (from school) <melodie.bernard123@yahoo.fr>
2025-07-01 16:43:46 +0200

    feat: added a little worker in tournament

--- server/tournaments/routing.py
+++ server/tournaments/routing.py
@@ -1,9 +1,11 @@
 from django.urls import re_path
+# from django.urls import path
 
 from . import consumers
 
 websocket_urlpatterns = [
     re_path(r"ws/tournament/$", consumers.TournamentConsumer.as_asgi()),
-    re_path(r"ws/tournament/(?P<tournament_id>[^/]+)",
-            consumers.TournamentConsumer.as_asgi()),
+    re_path(r"ws/tournament/(?P<tournament_id>[^/]+)", consumers.TournamentConsumer.as_asgi()),
+    # path("ws/tournament/", consumers.TournamentConsumer.as_asgi()),
+    # path("ws/tournament/<uuid:tournament_id>/", consumers.TournamentConsumer.as_asgi()),
 ]
