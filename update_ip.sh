ddb9cb99f26517e8abb8fe6aee698ca62aea2465
emuminov <jseldardw@gmail.com>
2025-07-16 18:35:32 +0200

    refactor: cleaned up Makefile, removed server_entrypoint.sh typo: fixed typo in update_ip.sh

--- update_ip.sh
+++ update_ip.sh
@@ -6,7 +6,7 @@ if [ ! -f .env ]; then
 fi
 HOST_IP=$(ip route get 8.8.8.8 | awk '{print $7; exit}');
 if ! grep -qE "^HOST_IP=" .env; then
-    printf "\n# IP Of the host machine\nHOST_IP=$HOST_IP" >> .env
+    printf "\n# IP of the host machine\nHOST_IP=$HOST_IP" >> .env
     echo "Host IP $HOST_IP was added to the .env"
 else
     sed -i -E "s/^HOST_IP=.*/HOST_IP=$HOST_IP/" .env
