fe98252841b82554c2139308fb88083a4cbad19e
Boussard Fanny <boussard.fanny@gmail.com>
2025-07-27 01:51:39 +0200

    fixed - sign up problem fixed

--- server/users/router/endpoints/auth.py
+++ server/users/router/endpoints/auth.py
@@ -107,11 +107,8 @@ def signup(request: HttpRequest, data: SignUpSchema):
         email=data.email,
         password=data.password,
     )
-    user.save()
 
-    response = create_json_response_with_tokens(user, user.profile.to_profile_minimal_schema())
-    response.status_code = 201
-    return response
+    return create_json_response_with_tokens(user, user.profile.to_profile_minimal_schema())
 
 
 @auth_router.post(
