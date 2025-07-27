567381d002f866abe1305bfa53a0b425b3f15da5
emuminov <jseldardw@gmail.com>
2025-07-27 16:16:56 +0200

    refactor: moved PongCloseCodes to common/ and renamed to CloseCodes, deleted server/chat/templates

---
+++ server/common/close_codes.py
@@ -0,0 +1,14 @@
+class CloseCodes:
+    """
+    Shared enum between different consumers for definition the unified close codes.
+    Normal closure     -> connection did its task normally and closed normally.
+    Cancelled          -> connection was canceled by the server.
+    Illegal connection -> user who is not authorized tried to connect.
+    Bad data           -> server received ill-formed data.
+    """
+
+    NORMAL_CLOSURE = 3000
+    CANCELLED = 3001
+    ILLEGAL_CONNECTION = 3002
+    ALREADY_IN_GAME = 3003
+    BAD_DATA = 3100
