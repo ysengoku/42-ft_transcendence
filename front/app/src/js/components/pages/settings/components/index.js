c462f1d12025bb0a2741a0da173342117288d92e
yusengok <ysengoku.tourisme@gmail.com>
2025-08-19 16:02:18 +0200

    feat: disable username change in settings, add notice for username in signup page

--- front/app/src/js/components/pages/settings/components/index.js
+++ front/app/src/js/components/pages/settings/components/index.js
@@ -1,8 +1,8 @@
-import { UserIdentityUpdate } from './UserIdentityUpdate.js';
+import { NicknameUpdate } from './NicknameUpdate.js';
 import { EmailUpdate } from './EmailUpdate.js';
 import { PasswordUpdate } from './PasswordUpdate.js';
 import { AvatarUpload } from './AvatarUpload.js';
 import { AvatarUploadModal } from './AvatarUploadModal.js';
 import { MfaEnableUpdate } from './MfaEnableUpdate.js';
 
-export { UserIdentityUpdate, EmailUpdate, PasswordUpdate, AvatarUpload, AvatarUploadModal, MfaEnableUpdate };
+export { NicknameUpdate, EmailUpdate, PasswordUpdate, AvatarUpload, AvatarUploadModal, MfaEnableUpdate };
