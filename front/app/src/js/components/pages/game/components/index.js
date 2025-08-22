513918ea398743788657145b1b36191935db1b7b
ysengoku <ysengoku.tourisme@gmail.com>
2025-08-22 15:12:26 +0200

    feat: update GameOverlay to adjust both single player and multiplayer

--- front/app/src/js/components/pages/game/components/index.js
+++ front/app/src/js/components/pages/game/components/index.js
@@ -1,6 +1,6 @@
-import { GameOverlay } from './GameOverlay';
+import { GameOverlay, OVERLAY_TYPE } from './GameOverlay';
 import { GameBuffIcon } from './GameBuffIcon';
 import { GameLifePoint } from './GameLifePoints';
 import { GameTimer } from './GameTimer';
 
-export { GameOverlay, GameBuffIcon, GameLifePoint, GameTimer };
+export { GameOverlay, OVERLAY_TYPE, GameBuffIcon, GameLifePoint, GameTimer };
