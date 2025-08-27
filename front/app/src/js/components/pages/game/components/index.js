7f180d11e08466f8fccb5c97ff6e46caa8209dee
yusengok <ysengoku.tourisme@gmail.com>
2025-08-27 14:12:11 +0200

    feat: add acore to game ui

--- front/app/src/js/components/pages/game/components/index.js
+++ front/app/src/js/components/pages/game/components/index.js
@@ -1,6 +1,7 @@
 import { GameOverlay, OVERLAY_TYPE } from './GameOverlay';
 import { GameBuffIcon } from './GameBuffIcon';
+import { GameScore } from './GameScore';
 import { GameLifePoint } from './GameLifePoints';
 import { GameTimer } from './GameTimer';
 
-export { GameOverlay, OVERLAY_TYPE, GameBuffIcon, GameLifePoint, GameTimer };
+export { GameOverlay, OVERLAY_TYPE, GameBuffIcon, GameScore, GameLifePoint, GameTimer };
