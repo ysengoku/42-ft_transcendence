80c3c9df3ed07236e69b3d4efa08ac421661a206
ysengoku <ysengoku.tourisme@gmail.com>
2025-09-15 18:36:26 +0200

    enhance: use MutationObserver with timeout instaed of requestAnimationFrame to prevent potentiel infinite loop, replace customEvent with direct handler call for game_found

--- front/app/src/js/sockets/matchMakingSocket.js
+++ front/app/src/js/sockets/matchMakingSocket.js
@@ -1,9 +1,22 @@
 import { socketManager } from './socket';
+import { router } from '@router';
 
 // Socket registration for matchmaking
 socketManager.addSocket('matchmaking', {
   game_found: (data) => {
-    const customEvent = new CustomEvent('gameFound', { detail: data, bubbles: true });
-    document.dispatchEvent(customEvent);
+    if (window.location.pathname === '/duel') {
+      const duelPageElement = document.querySelector('duel-page');
+      if (duelPageElement) {
+        duelPageElement.handleGameFound(data);
+      }
+    } else {
+      router.navigate('/duel', { status: 'matchmaking' });
+      requestAnimationFrame(() => {
+        const duelPageElement = document.querySelector('duel-page');
+        if (duelPageElement) {
+          duelPageElement.handleGameFound(data);
+        }
+      });
+    }
   },
 });
