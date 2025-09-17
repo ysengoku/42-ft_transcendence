eabc30baf5c33a59c4fd891581e0155802944192
ysengoku <ysengoku.tourisme@gmail.com>
2025-09-17 10:36:56 +0200

    feat: add Firefox support for range input styles, verify that -webkit-line-clamp with display: -webkit-box already works in Firefox

--- front/app/src/js/components/pages/profile/components/DuelHistory.js
+++ front/app/src/js/components/pages/profile/components/DuelHistory.js
@@ -280,8 +280,8 @@ export class UserDuelHistory extends HTMLElement {
         overflow: hidden;
         text-overflow: ellipsis;
         word-break: break-all;
-        display: -webkit-box;
-        -webkit-line-clamp: 1;
+        display: -webkit-box; /* Compatible for Firefox */
+        -webkit-line-clamp: 1; /* Compatible for Firefox by combining with display: -webkit-box; */
         -webkit-box-orient: vertical;
       }
       .result-badge {
